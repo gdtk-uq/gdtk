54c0e55b59cbdb0a50ef7872961e929eced853f6
Kyle Damm <k.damm@uq.edu.au>
2015-10-16 09:54:10 +1000

    kinetics: Added alpha-QSS method for chemical kinetics update.
    Also, during testing found a bug in the contolling timestep
    algorithm that allowed successively smaller timesteps to
    be saved and used on the next update. The code could work itself
    into taking exceedingly small timesteps. This bug is fixed by
    using a dtSave variable to record the last "good" timestep.

--- src/kinetics/chemistry_update.d
+++ src/kinetics/chemistry_update.d
@@ -2,7 +2,7 @@
  * This module provides routines to update a gas phase
  * chemistry system.
  *
- * Author: Rowan G.
+ * Author: Rowan G. and Kyle D.
  */
 
 module kinetics.chemistry_update;
@@ -43,7 +43,8 @@ final class ReactionUpdateScheme {
 	rmech = createReactionMechanism(L, gmodel);
 	lua_close(L);
 	// Just hard code selection of RKF for present.
-	cstep = new RKFStep(gmodel, rmech, 1.0e-3);
+	//cstep = new RKFStep(gmodel, rmech, 1.0e-3);
+	cstep = new AlphaQssStep(gmodel, rmech);
 	tightTempCoupling = false;
 	maxSubcycles = MAX_SUBCYCLES;
 	maxAttempts = MAX_STEP_ATTEMPTS;
@@ -86,7 +87,9 @@ int update_chemistry(GasState Q, double tInterval, ref double dtSuggest,
 	h = rmech.estimateStepSize(conc0);
     else
 	h = dtSuggest;
+    double dtSave = h;
     // 2. Now do the interesting stuff, increment species change
+    //writeln("THIS IS THE FIRST dt = ", h);
     int cycle = 0;
     int attempt = 0;
     for ( ; cycle < maxSubcycles; ++cycle ) {
@@ -96,14 +99,14 @@ int update_chemistry(GasState Q, double tInterval, ref double dtSuggest,
 	 * want to store the fractional step of (tInterval - t)
 	 * that is taken as the last step.
 	 */
-	dtSuggest = h;
+	dtSave = h;
 	h = min(h, tInterval - t);
-	attempt = 0;
-	for ( ; attempt < maxAttempts; ++attempt ) {
+	attempt= 0;
+      	for ( ; attempt < maxAttempts; ++attempt ) {
 	    if ( cstep(conc0, h, concOut, dtSuggest) == ResultOfStep.success ) {
 		/* We succesfully took a step of size h
 		 * so increment the total time.
-		 */
+		 */       
 		t += h;
 		foreach ( i; 0..conc0.length ) conc0[i] = concOut[i];
 		/* We can now make some decision about how to
@@ -112,17 +115,29 @@ int update_chemistry(GasState Q, double tInterval, ref double dtSuggest,
 		 * that it's sensible. For example, we won't
 		 * increase by more than 10% (or INCREASE_PERCENT).
 		 * We'll also balk if the ODE step wants to reduce
-		 * the stepsize by 50% (or DECREASE_PERCENT).
-		 * In that case, we'll set the new
-		 * timestep to 50% of what it was and keep
-		 * on going. Our reasoning being that we were
-		 * successful on the previous step and things
-		 * shouldn't have changed by that much.
+		 * the stepsize on a successful step. This is because
+		 * if the step was successful there shouldn't be any
+		 * need (stability wise or accuracy related) to warrant 
+		 * a reduction. Thus if the step is successful and
+		 * the dtSuggest comes back lower, let's just set
+		 * h as the original value for the successful step.
 		 */
 		double hMax = h*(1.0 + DT_INCREASE_PERCENT/100.0);
-		double hMin = h*(1.0 - DT_DECREASE_PERCENT/100.0);
-		h = min(dtSuggest, hMax);
-		h = max(h, hMin);
+		if (dtSuggest > h) {
+		    /* It's possible that dtSuggest is less than h.
+		     * When that occurs, we've made the decision to ignore
+		     * the new timestep suggestion supplied by the ODE step.
+		     * Our reasoning is that we'd like push for an aggressive
+		     * timestep size. If that fails, then a later part of
+		     * the timestepping algorithm will catch that and reduce
+		     * the timestep.
+		     *
+		     * It's also possible that the suggested timestep
+		     * is much larger than what we just used. For that
+		     * case, we cap it at hMax. That's the following line.
+		     */
+		    h = min(dtSuggest, hMax);
+		}
 		break;
 	    }
 	    else { // in the case of failure...
@@ -184,6 +199,7 @@ int update_chemistry(GasState Q, double tInterval, ref double dtSuggest,
     // else all is well, so update GasState Q and leave.
     gmodel.conc2massf(concOut, Q);
     gmodel.update_thermo_from_rhoe(Q);
+    dtSuggest = dtSave;
     return 0;
 }
 
@@ -338,7 +354,179 @@ private:
     double[] _yTmp, _yErr;
     double[] _k1, _k2, _k3, _k4, _k5, _k6;
 }
+
+
+/++
+ + The Alpha-QSS method, specialised to work
+ + with a chemical kinetic ODE.
+ + 
+ + author: K.Damm 2015
+ + adapted from R.Gollan's eilmer3 implementation
+ +
+ + References:
+ + Mott, D. (1999)
+ + Integrates a system of chemical kinetics ODEs which contains both
+ + stiff and non-stiff equations. It is proven to be A-stable and has
+ + second order accuracy. The method is single-step self starting,
+ + thus has little start-up costs.
+ +
+ + S.R. Qureshi, R. Prosser (2007)
+ + Delta term added into Mott algorithm to prevent excessively small
+ + concentrations forcing the solver to take miniscule steps.
+ + Default delta = 10.0e-10.
+ +/
+
+class AlphaQssStep : ChemODEStep {
+public:
+  this(in GasModel gmodel, ReactionMechanism rmech)
+    {
+	super(rmech);
+	// Allocate working arrays
+	_ndim = gmodel.n_species;
+	_yp.length = _ndim;
+	_yp0.length = _ndim;
+	_yc.length = _ndim;
+	_q0.length = _ndim;
+	_L0.length = _ndim;
+	_p0.length = _ndim;
+	_qtilda.length = _ndim;
+	_pbar.length = _ndim;
+	_qp.length = _ndim;
+        _Lp.length = _ndim;
+	_pp.length = _ndim;
+	_alpha.length = _ndim;
+	_alphabar.length = _ndim;
+    }
+
+    override ResultOfStep opCall(double[] y0, double h, double[] yOut, ref double hSuggest)
+    {
+	// 0. Alpha-QSS Constant parameters (currently hard-coded)
+        static immutable double _EPS1 = 0.001;
+        static immutable double _EPS2 = _EPS1/2.0;
+        static immutable int _MAX_ITER = 10;
+        static immutable double _ZERO_EPS = 1.0e-50;
+	static immutable double _delta = 1.0e-10;
+	
+	// 1. Predictor Step
+	_rmech.eval_split_rates(y0, _q0, _L0);
+	p_on_y(_L0, y0, _p0);
+        alpha_compute(_p0, _alpha, h);
+	update_conc(_yp, y0, _q0, _p0, _alpha, h);
+	// put aside the first predictor values for
+	// later use in the convergence test
+	foreach ( i; 0.._ndim )
+	  _yp0[i] = _yp[i];
+
+	// 2. Corrector Step
+	foreach ( corr; 0.._MAX_ITER) {
+	    _rmech.eval_split_rates(_yp, _qp, _Lp);
+	    p_on_y(_Lp, _yp, _pp);
+	    alpha_compute(_pp, _alphabar, h);
+	    foreach ( i; 0.._ndim ) {
+		_qtilda[i] = _alphabar[i]*_qp[i] + (1-_alphabar[i])*_q0[i];
+		_pbar[i] = 0.5*(_p0[i]+_pp[i]);
+	    }
+
+	    // actual corrector step
+	    update_conc(_yc, y0, _qtilda, _pbar, _alphabar, h);
+
+	    // test if converged or not
+	    bool converged = test_converged(_yc, _yp0, h);
+
+	    if ( converged ) {
+		// the solver has converged, let's set the current
+		// corrector to the yOut vector and suggest a new
+		// time step.
+		foreach ( i; 0.._ndim ) {
+		    yOut[i] = _yc[i];
+		}
+		hSuggest = step_suggest(h, _yc, _yp0);
+		return ResultOfStep.success;
+	    }
+	    // if we haven't converged yet make the corrector the new predictor and try again
+	    foreach ( i; 0.._ndim ) {
+		_yp[i] = _yc[i];
+	    }
+	}
+	// if we have failed the convergence test too many times let's start again with a smaller h
+	// This suggested h will be passed through generic step size  check on outer loop
+        hSuggest = step_suggest(h, _yc, _yp0);
+	return ResultOfStep.failure;
+    }
     
+private:
+    int _ndim;
+    double[] _yTmp, _yp, _yp0,  _yc, _q0, _L0, _p0, _pp, _qtilda, _pbar, _qp, _Lp, _alpha, _alphabar;
+    immutable double _EPS1 = 0.001;
+    immutable double _EPS2 = _EPS1/2.0;
+    immutable int _MAX_ITER = 10;
+    immutable double _ZERO_EPS = 1.0e-50;
+    immutable double _delta = 1.0e-10;
+    
+    // Private functions.
+    void p_on_y(double[] L, double[] y, double[] p_y) {
+	foreach( i; 0.._ndim) 
+	    p_y[i] = L[i] / (y[i] + _ZERO_EPS);
+	return;
+    }
+
+    void alpha_compute(double[] p, double[] alpha, double h) {
+        foreach ( i; 0.._ndim ) {
+	    double r = 1.0/(p[i]*h+_ZERO_EPS);  // ZERO_EPS prevents division by 0 
+	    alpha[i] = (180.0*r*r*r+60.0*r*r+11.0*r+1.0)/(360.0*r*r*r+60.0*r*r+12.0*r+1.0);
+	}
+    }
+
+    void update_conc(double[] yTmp, double[] y0, double[] q, double[] p, double[] alpha, double h) {
+        foreach ( i; 0.._ndim )
+	    yTmp[i] = y0[i] + (h*(q[i]-p[i]*y0[i]))/(1.0+alpha[i]*h*p[i]);
+    }
+
+    bool test_converged(in double[] yc, in double[] yp, double h) {
+	int flag = 0;
+	double test = 0.0;
+	foreach ( i; 0.._ndim ) {
+	    if ( yc[i] < _ZERO_EPS )
+		continue;
+	    test = fabs(yc[i] - yp[i]);
+	    // +delta from Qureshi and Prosser (2007)
+	    if ( test >= (_EPS1 * (yc[i] + _delta)) )
+	      ++flag;
+	}
+	if ( flag == 0 )
+	    return true;
+	else
+	    return false;
+    }
+
+    double step_suggest(double h, double[] yc, double[] yp)
+    {
+	double test = 0.0;
+	double sigma = 0.0;
+	double h_new = 0.0;
+
+	foreach (i; 0.._ndim) {
+	    if ( yc[i] < _ZERO_EPS )
+		continue;
+	    test = fabs( yc[i] - yp[i]) / (_EPS2*(yc[i]+ _delta));
+	    if ( test > sigma )
+		sigma = test;
+	}
+
+	if ( sigma <= 0.0 ) {
+	    h_new = h;
+	}
+	else {
+	    double x = sigma;
+	    x = x - 0.5*(x*x - sigma)/x;
+	    x = x - 0.5*(x*x - sigma)/x;
+	    x = x - 0.5*(x*x - sigma)/x;
+	    h_new = h * ( (1.0 / x) + 0.005 );
+	}
+    return h_new;
+    }
+}
+
 unittest
 {
     import std.stdio;
@@ -376,3 +564,5 @@ unittest
     gmodel.massf2conc(gd, conc);
     assert(approxEqual(7.14215400423, conc[2]), failedUnitTest());
 }
+
+
