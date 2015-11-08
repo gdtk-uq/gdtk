968fec1b4346835a2cea3dd1de9bb3574a727871
Rowan J. Gollan <r . gollan at uq . edu . au>
2015-11-08 21:37:56 +1000

    eilmer/examples: Some detailed commenting added to the connection test.
    I find the code to exercise all 144 connections of 3D blocks can
    get confusing, particularly when one comes back to the code after
    some time. I tried to document my thinking now while the case is
    still fresh in my head.

--- examples/eilmer/3D/connection-test/connection-test.lua
+++ examples/eilmer/3D/connection-test/connection-test.lua
@@ -1,14 +1,101 @@
--- read the case and find the vertex pairs
+-- Author: Rowan J. Gollan
+-- Date: 2015-11-08
+--[[
+
+This script is a template script that can
+be used as part of a testing routine to
+exercise every possible connection in 3D
+of right-handed blocks in a structured grid
+code. The script is specialised by reading
+in a particular connection to test in a
+"case.txt" file.
+
+This connection test script requires some explanation,
+mostly so I'll remember how it works in the future.
+In this preamble to the file, I'll document main idea
+of how this test works. The comments that appear
+later in the file will attempt to described the
+implementatation.
+
+Test case overview
+------------------
+The test involves two blocks placed side-by-side
+with a face in common providing a connection.
+The initial gas state in both blocks varies
+spatially such that there is a non-uniform
+distribution of pressure and density across
+the connecting face. This non-uniformity has
+no symmetry across the connceting plane.
+This is important for the testing
+(to be explained shortly).
+The arrangement of the blocks is shown here
+(except that face [BCGF] of the left block
+physically coincides with face [ADHE] of
+the right block):
+
+      H------------G          H------------G
+     /|           /|         /|           /|
+    / |          / |        / |          / |
+   D--+---------C  |       D--+---------C  |
+   |  | LFT BLK |  |       |  |RGHT BLK |  |
+   |  |         |  |       |  |         |  |
+   |  E---------+--F       |  E---------+--F
+   | /          | /        | /          | /
+   |/           |/         |/           |/
+   A------------B          A------------B
+
+The test is run by letting the flow solver
+iterate the state of the system forward
+in time by one timestep. The non-uniform
+nature of the flow properties gives a
+unique flow field after one time step.
+We build a reference case of how that
+flow state should look after one step.
+We will then swap the arrangement of the block
+connections and test that the flow state
+after one step matches our reference case.
+If if doesn't, it will be a strong indication
+that a copying error has occurred during
+the exchange routines for that particular
+block connection type.
+
+Implementation of the test
+--------------------------
+The "trick" to this test is that the left
+and right blocks are always located in the
+same physical position, no matter which connection
+is being tested. By keeping the blocks in the
+same physical position, it means that the expected
+state of the system after one step is always the
+same in physical space. This simplifies the
+extraction of the data. We can use the "--probe"
+option of the post-processor to probe data at
+the same physical location, and then check that
+the output matches our reference case.
+
+Most of the complexity in the script below is about
+rotating logical orientations of the blocks so that
+we can test all of the connections. When reading the
+script, keep in mind that the alphabetic labels
+always refer to the physical locations of block
+corners as shown in the figure above. The numeric labels
+refer to the logical labels used internally in the code
+to designate the corners of 3D block.
+
+--]]
+
+-- Read the case 
 file = io.open("case.txt", "r")
 faceA = tostring(file:read("*line"))
 faceB = tostring(file:read("*line"))
 orientation = tonumber(file:read("*number"))
 file:close()
 
+-- Now find the connecting vertex pairs by looking
+-- up the tabled 'vtxPairs3D' proviede in 'blk_conn.lua'
 vtxPairs = vtxPairs3D[faceA..faceB..orientation]
 
-print("faceA= ", faceA, " faceB= ", faceB, " ornt= ", orientation)
-
+-- Proceed with some general simulation setup
 config.title = "3D Connection tester"
 config.dimensions = 3
 
@@ -23,6 +110,9 @@ p_base = 1.0e5
 T_base = 348.3
 
 -- Set up the physical aspects of the test
+-- The next two functions provide the initial state of
+-- the gas in the LFT and RGHT cubes. This initial state
+-- varies in 3D space.
 function lftCubeFill(x, y, z)
    p_inc_factor = x * (5*y^2) * (10*z^3)
    p = p_base*(1.0 + p_inc_factor)
@@ -62,9 +152,21 @@ function cube(origin, length)
    return {A=A, B=B, C=C, D=D, E=E, F=F, G=G, H=H}
 end
 
--- build cubes (left and right)
+-- Build cubes (left and right)
+-- We store the cube corners based on their
+-- physical location (alphabetic labels) 
+-- in 'cL' (corners of left cube)
+-- and 'cR' (corners of right cube)
 cL = cube(Vector3:new{0.0, 0.0, 0.0}, 1.0)
 cR = cube(Vector3:new{1.0, 0.0, 0.0}, 1.0)
+
+-- Now we consider the logical arrangement of the corners (vertices) 
+-- depending on which face of the left block is being used in the
+-- connection faceA.
+-- lftVtxs: stores the corners in the correct order (0, 1, 2, ...)
+--          such that the correct face coincides with face [BCGF] of the
+--          left block
+-- lftFace: store the index locations associated with the corners [BCGF]
 local lftVtxs
 local lftFace
 if faceA == north then
@@ -87,13 +189,31 @@ elseif faceA == bottom then
    lftFace = {B=0, C=3, F=1, G=2}
 end
 
+-- On a given face, there is always a set of corners on the
+-- opposite face across the block. These sets of opposites
+-- occur in the pairs: east/west, north/south, and top/bottom.
+-- We store these connecting sets for later use. 
+
 ewPair = {[0]=1, [1]=0, [3]=2, [2]=3, [7]=6, [6]=7, [4]=5, [5]=4}
 nsPair = {[3]=0, [0]=3, [2]=1, [1]=2, [6]=5, [5]=6, [7]=4, [4]=7}
 tbPair = {[4]=0, [0]=4, [5]=1, [1]=5, [6]=2, [2]=6, [7]=3, [3]=7}
 
--- find the vertices on the connecting face of the right cube
--- We could condense this next bit of code, but it gets hard
--- to understand later
+-- Our left face is made of corners: B, C, F and G.
+-- From the figure above, we know the connections to the right
+-- face:
+--       B (of left) +--+ A (of right)
+--       C (of left) +--+ D (of right)
+--       F (of left) +--+ E (of right)
+--       G (of left) +--+ H (of right)
+-- Now we already have a list of all pairs of vertices involved
+-- in the connection. So we'll look through the list of vertices
+-- and find the numeric value that matches with corner B of
+-- the left face. When we've found that, we know that the matching
+-- vertex is corner B of the right face. Continuing in that manner
+-- for corners C, F and G of the left face, we can find the numeric
+-- labels for the matching corners D, E and H on the right face.
+-- (It would be possible to condense the code that does this search
+--  but it gets very obsucre, very quickly.)
 local rghtPts = {}
 pB = lftFace.B
 for _,vp in ipairs(vtxPairs) do
@@ -112,8 +232,12 @@ for _,vp in ipairs(vtxPairs) do
    if pG == vp[1] then rghtPts.H = vp[2] end
 end
 
-for k,v in pairs(rghtPts) do print(k,v) end
-
+-- Now we get to use the connecting sets we used above.
+-- Since we know the numeric points at locations 
+-- A, E, H and D on the right face, AND we know what
+-- type of connecting face it is, then we can instantly
+-- fill out the opposing corners of the right cube
+-- (B, F, G, C) using the connection mapping. 
 if faceB == north or faceB == south then
    rghtPts.B = nsPair[rghtPts.A]
    rghtPts.F = nsPair[rghtPts.E]
@@ -131,11 +255,17 @@ elseif faceB == top or faceB == bottom then
    rghtPts.C = tbPair[rghtPts.D]
 end
 
+-- We need to list the right points in order (0, 1, 2, etc. but
+-- counting from 1 [hence i+1]).
+-- So we basically build a reverse map of what we've collected
+-- in rghtPts.
 rghtVtxs = {}
 for cnr,i in pairs(rghtPts) do
    rghtVtxs[i+1] = cR[cnr]
 end
 
+-- In the last step, we build the left and right blocks from
+-- our collections of vertices.
 lftGrid = StructuredGrid:new{pvolume=TFIVolume:new{vertices=lftVtxs},
 			     niv=nn+1, njv=nn+1, nkv=nn+1}
 lftBlk = SBlock:new{grid=lftGrid, fillCondition=lftCubeFill}
