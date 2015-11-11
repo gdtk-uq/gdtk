401d7053ab0a82e463d2bf346695f87d1dd46360
Peter Jacobs <peterj@mech.uq.edu.au>
2015-11-12 07:58:52 +1000

    nm: fix indentation and move a comment; no functional change.

--- src/nm/bracketing.d
+++ src/nm/bracketing.d
@@ -23,24 +23,25 @@ int bracket(alias f)(ref double x1, ref double x2,
     if ( is(typeof(f(0.0)) == double) || is(typeof(f(0.0)) == float) )
 {
     if ( x1 == x2 ) {
-    throw new Exception("Bad initial range given to bracket.");
+	throw new Exception("Bad initial range given to bracket.");
     }
     double f1 = f(x1);
     double f2 = f(x2);
     for ( int i = 0; i < max_try; ++i ) {
-        if ( f1*f2 < 0.0 ) return 0; // we have success
-        if ( abs(f1) < abs(f2) ) {
-            x1 += factor * (x1 - x2);
-            x1 = max(x1_min, x1);//prevent the bracket from being expanded beyond a specified domain
-            f1 = f(x1);
-        } else {
-            x2 += factor * (x2 - x1);
-            f2 = f(x2);
-    }
+	if ( f1*f2 < 0.0 ) return 0; // we have success
+	if ( abs(f1) < abs(f2) ) {
+	    x1 += factor * (x1 - x2);
+	    //prevent the bracket from being expanded beyond a specified domain
+	    x1 = max(x1_min, x1);
+	    f1 = f(x1);
+	} else {
+	    x2 += factor * (x2 - x1);
+	    f2 = f(x2);
+	}
     }
     // If we leave the loop here, we were unsuccessful.
     return -1;
- } // end bracket()
+} // end bracket()
 
 version(bracketing_test) {
     import util.msg_service;
