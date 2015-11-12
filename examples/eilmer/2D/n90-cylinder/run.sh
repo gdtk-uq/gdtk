ca438674bfa7ff6c27a610a388a8397bdabb7385
Peter Jacobs <peterj@mech.uq.edu.au>
2015-11-12 18:12:43 +1000

    eilmer: run the n90 case with max 4 cpus to get a useful speedup in the tests.

--- examples/eilmer/2D/n90-cylinder/run.sh
+++ examples/eilmer/2D/n90-cylinder/run.sh
@@ -3,5 +3,5 @@
 prep-gas nitrogen-2sp.inp nitrogen-2sp.lua
 prep-chem nitrogen-2sp.lua nitrogen-2sp-2r.lua e4-chem.lua
 e4shared --prep --job=n90
-e4shared --run --job=n90 --verbosity=1 --max-cpus=1
+e4shared --run --job=n90 --verbosity=1 --max-cpus=4
 e4shared --post --job=n90 --tindx-plot=all --vtk-xml
