60418df53c47a2fe2c82353ac1a0959a95d9485a
Peter Jacobs <peterj@mech.uq.edu.au>
2015-12-07 21:59:18 +1000

    eilmer: cone20-usg has only one block, presently.

--- examples/eilmer/2D/cone20-usg/run.sh
+++ examples/eilmer/2D/cone20-usg/run.sh
@@ -2,5 +2,5 @@
 # run.sh
 prep-gas ideal-air.inp ideal-air-gas-model.lua
 e4shared --prep --job=cone20
-e4shared --run --job=cone20 --verbosity=1 --max-cpus=2
+e4shared --run --job=cone20 --verbosity=1 --max-cpus=1
 e4shared --post --job=cone20 --vtk-xml --add-vars="mach,pitot,total-p,total-h"
