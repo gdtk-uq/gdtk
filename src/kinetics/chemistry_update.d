d9fd82b263af04a5523044221ab083b84a10352e
Rowan J. Gollan <r.gollan@uq.edu.au>
2016-02-19 19:47:20 +1000

    kinetics: Added extra commentary about DT_INCREASE_PERCENT...
    ... to help myself out. I had to stop and think a little when
    I looked at that variable.

--- src/kinetics/chemistry_update.d
+++ src/kinetics/chemistry_update.d
@@ -19,6 +19,9 @@ import kinetics.reaction_mechanism;
 
 immutable double DT_INCREASE_PERCENT = 10.0; // allowable percentage increase on succesful step
 immutable double DT_DECREASE_PERCENT = 50.0; // allowable percentage decrease on succesful step
+                                             // Yes, you read that write. Sometimes a step is succesful
+                                             // but the timestep selection algorithm will suggest
+                                             // a reduction. We limit that reduction to no more than 50%.
 immutable double DT_REDUCTION_FACTOR = 2.0; // factor by which to reduce timestep
                                             // after a failed attempt
 immutable double H_MIN = 1.0e-15; // Minimum allowable step size
