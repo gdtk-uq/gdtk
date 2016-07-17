bb54012763b3cc111537c9dce624e24014280ac0
Kyle Damm <kyle.damm@uqconnect.edu.au>
2016-07-18 08:27:23 +1000

    examples: removed old examples files.
    Removed the old example cases for cyl50, laminar-
    flat-plate and hakkinen.

--- examples/eilmer/2D/cyl50/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# run.sh
-prep-gas ideal-air.inp ideal-air-gas-model.lua
-e4shared --prep --job=cyl50
-e4shared --run --job=cyl50 --verbosity=1 --max-cpus=4
-e4shared --post --job=cyl50 --tindx-plot=all --vtk-xml --add-vars="mach,pitot"
--- examples/eilmer/2D/laminar-flat-plate/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# run.sh
-prep-gas ideal-air.inp ideal-air-gas-model.lua
-e4shared --prep --job=lam_flat_plate_march
-e4shared --run --job=lam_flat_plate_march --verbosity=1 --max-cpus=4
-e4shared --post --job=lam_flat_plate_march --tindx-plot=all --vtk-xml --add-vars="mach,pitot"
