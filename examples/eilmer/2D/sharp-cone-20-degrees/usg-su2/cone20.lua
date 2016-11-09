7118e0cfd777859b3ee7946799f3ce01ea8eebaa
Peter Jacobs <peterj@mech.uq.edu.au>
2016-11-09 10:45:01 +0100

    Eilmer: updates the sharp-cone SU2 unstructured-grid simulation.
    This version makes use of the 4-block arrangement prepared by Kyle.
    It runs nicely on 4 cores of my little HP computer.

--- examples/eilmer/2D/sharp-cone-20-degrees/usg-su2/cone20.lua
+++ examples/eilmer/2D/sharp-cone-20-degrees/usg-su2/cone20.lua
@@ -1,42 +1,33 @@
 -- cone20.lua
 -- Unstructured Grid Example -- for use with Eilmer4
--- PJ & RG
--- 2015-11-08 -- adapted from cone20-simple
+-- 2015-11-08  PeterJ, RowanG, KyleD
 
-job_title = "Mach 1.5 flow over a 20 degree cone -- Unstructured Grid."
-print(job_title)
-
--- We can set individual attributes of the global data object.
+config.title = "Mach 1.5 flow over a 20 degree cone -- Unstructured Grid."
+print(config.title)
 config.dimensions = 2
-config.title = job_title
 config.axisymmetric = true
 
 nsp, nmodes, gm = setGasModel('ideal-air-gas-model.lua')
 print("GasModel set to ideal air. nsp= ", nsp, " nmodes= ", nmodes)
-initial = FlowState:new{p=5955.0, T=304.0, velx=0.0, vely=0.0}
-inflow = FlowState:new{p=95.84e3, T=1103.0, velx=1000.0, vely=0.0}
-
--- Demo: Verify Mach number of inflow.
-Q = GasState:new{gm}
-Q.p = 95.84e3
-Q.T = {1103.0}
-print("T", Q.T[1])
-Q.massf = {air=1.0}
-gm:updateSoundSpeed(Q)
-print("Sound speed= ", Q.a)
-print("Inflow Mach number= ", 1000.0/Q.a)
+initial = FlowState:new{p=5955.0, T=304.0}
+inflow = FlowState:new{p=95.84e3, T=1103.0, velx=1000.0}
 
 -- Define the flow domain using an imported grid.
-mygrid = UnstructuredGrid:new{filename="cone20.su2", fmt="su2text"}
-myblk = UBlock:new{grid=mygrid, fillCondition=initial}
--- There are 8 boundaries within the single unstructured-grid
--- These are essentially the same as the two-block arrangement of the other examples,
--- so we need to join the interior boundaries.
--- Unspecified boundaries will be SlipWallBC.
-myblk.bcList[6] = InFlowBC_Supersonic:new{flowCondition=inflow, label="inflow-boundary"} -- WEST0
-myblk.bcList[7] = ExchangeBC_MappedCell:new{} -- WEST1
-myblk.bcList[0] = ExchangeBC_MappedCell:new{} -- EAST0
-myblk.bcList[1] = OutFlowBC_Simple:new{label="outflow-boundary"} -- EAST1
+grids = {}
+for i=0,3 do
+   fileName = string.format("cone20_grid%d.su2", i)
+   grids[i] = UnstructuredGrid:new{filename=fileName, fmt="su2text"}
+end
+my_bcDict = {INFLOW=InFlowBC_Supersonic:new{flowCondition=inflow},
+	     OUTFLOW=OutFlowBC_Simple:new{},
+	     SLIP_WALL=WallBC_WithSlip:new{},
+	     INTERIOR=ExchangeBC_MappedCell:new{list_mapped_cells=true},
+	     CONE_SURFACE=WallBC_WithSlip:new{}
+	    }
+blks = {}
+for i=0,3 do
+   blks[i] = UBlock:new{grid=grids[i], fillCondition=inflow, bcDict=my_bcDict}
+end
 
 -- Do a little more setting of global data.
 config.max_time = 5.0e-3  -- seconds
--- examples/eilmer/2D/sharp-cone-20-degrees/usg-su2/run.sh
+++ examples/eilmer/2D/sharp-cone-20-degrees/usg-su2/run.sh
@@ -3,6 +3,6 @@
 prep-gas ideal-air.inp ideal-air-gas-model.lua
 e4shared --prep --job=cone20
 # e4shared --post --job=cone20 --tindx-plot=0 --vtk-xml
-e4shared --run --job=cone20 --verbosity=1 --max-cpus=2
+e4shared --run --job=cone20 --verbosity=1 --max-cpus=4
 e4shared --post --job=cone20 --add-vars="mach,pitot,total-p,total-h" --vtk-xml \
          --extract-streamline="0.1,0.11,0.0" --output-file="streamline.dat"
