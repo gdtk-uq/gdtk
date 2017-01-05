de4595af10d75795af20d80ffa11b999f5c254d0
Kyle Damm <kyle.damm@uqconnect.edu.au>
2017-01-05 14:33:40 +1000

    examples: Added cone20 usg metis example.
    Added in yet another cone20 example, this time we are testing the unstructured grid partitioner which
    utilises Metis.

---
+++ examples/eilmer/2D/sharp-cone-20-degrees/usg-metis/cone20.lua
@@ -0,0 +1,57 @@
+-- cone20.lua
+-- Unstructured Grid Example -- for use with Eilmer4
+-- PJ & RG
+-- 2015-11-08 -- adapted from cone20-simple
+
+job_title = "Mach 1.5 flow over a 20 degree cone -- Unstructured Grid."
+print(job_title)
+
+-- We can set individual attributes of the global data object.
+config.dimensions = 2
+config.title = job_title
+config.axisymmetric = true
+
+nsp, nmodes, gm = setGasModel('ideal-air-gas-model.lua')
+print("GasModel set to ideal air. nsp= ", nsp, " nmodes= ", nmodes)
+initial = FlowState:new{p=5955.0, T=304.0, velx=0.0, vely=0.0}
+inflow = FlowState:new{p=95.84e3, T=1103.0, velx=1000.0, vely=0.0}
+
+-- Demo: Verify Mach number of inflow.
+Q = inflow:toTable()
+print("T=", Q.T, "density=", Q.rho, "sound speed= ", Q.a)
+print("inflow Mach number=", 1000.0/Q.a)
+print("dynamic pressure q=", 1/2*Q.rho*1.0e6)
+
+-- Define the flow domain using an imported grid
+nblocks = 4
+grids = {}
+for i=0,nblocks-1 do
+   fileName = string.format("block_%d_cone20.su2", i)
+   grids[i] = UnstructuredGrid:new{filename=fileName, fmt="su2text", scale=1.0}
+end
+
+my_bcDict = {INFLOW=InFlowBC_Supersonic:new{flowCondition=inflow, label="inflow-boundary"},
+	     OUTFLOW=OutFlowBC_Simple:new{label="outflow-boundary"},
+	     SLIP_WALL=WallBC_WithSlip:new{},
+	     METIS_INTERIOR=ExchangeBC_MappedCell:new{mapped_cells_from_file=true}}
+
+blks = {}
+for i=0,nblocks-1 do
+   blks[i] = UBlock:new{grid=grids[i], fillCondition=inflow, bcDict=my_bcDict}
+end
+
+-- There are 8 boundaries within the single unstructured-grid
+-- These are essentially the same as the two-block arrangement of the other examples,
+-- so we need to join the interior boundaries.
+-- Unspecified boundaries will be SlipWallBC.
+
+-- Do a little more setting of global data.
+config.max_time = 5.0e-3  -- seconds
+config.max_step = 3000
+config.dt_init = 1.0e-6
+config.cfl_value = 0.5
+config.dt_plot = 1.5e-3
+config.dt_history = 10.0e-5
+
+setHistoryPoint{x=1.0, y=0.2} -- nose of cone
+setHistoryPoint{x=0.201, y=0.001} -- base of cone
---
+++ examples/eilmer/2D/sharp-cone-20-degrees/usg-metis/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# run.sh
+ugrid_partition cone20.su2 mapped_cells 4 2 
+prep-gas ideal-air.inp ideal-air-gas-model.lua
+e4shared --prep --job=cone20
+e4shared --post --job=cone20 --tindx-plot=last --vtk-xml
+e4shared --run --job=cone20 --verbosity=1 --max-cpus=4
+e4shared --post --job=cone20 --add-vars="mach,pitot,total-p,total-h" --vtk-xml \
+         --extract-streamline="0.1,0.11,0.0" --output-file="streamline.dat"
