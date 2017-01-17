410dbbd7aab95def98f2384d0930c7d7c7b89151
Kyle Damm <kyle.damm@uqconnect.edu.au>
2017-01-17 10:06:23 +1000

    eilmer-examples: Removed old shock fitting example directory.

--- examples/eilmer/2D/cylinder-shockfitting/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# run.sh
-prep-gas ideal-air.inp ideal-air-gas-model.lua
-e4shared --prep --job=cyl-sf
-e4shared --run --job=cyl-sf --verbosity=1 --max-cpus=4
-e4shared --post --job=cyl-sf --tindx-plot=all --vtk-xml
