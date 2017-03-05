5426a572982c42f95326b8c684f15fe2b5befa5b
Peter Jacobs <peterj@mech.uq.edu.au>
2017-03-05 22:22:18 +1000

    Gas: a bit of progress on cea_gas.d.
    Sorted the design of the set of methods.

--- src/gas/cea_gas.d
+++ src/gas/cea_gas.d
@@ -28,6 +28,22 @@ import util.lua;
 import util.lua_service;
 import core.stdc.stdlib : exit;
 
+void runCEAProgram(string jobName, bool checkTableHeader=true)
+{
+    string inpFile = jobName ~ ".inp";
+    string outFile = jobName ~ ".out";
+    string pltFile = jobName ~ ".plt";
+    if (!exists(inpFile)) {
+	throw new Exception(format("CEAGas cannot find %s", inpFile));
+    }
+    if (exists(outFile)) { remove(outFile); }
+    if (exists(pltFile)) { remove(pltFile); }
+    //
+    // [TODO] run cea as a subprocess and send the jobName to it.
+    //
+    // [TODO] scan the outFile looking for "THERMODYNAMIC PROPERTIES".
+} // end runCEAProgram()
+
 class CEAGas: GasModel {
 public:
 
@@ -46,13 +62,32 @@ public:
 	//
 	create_species_reverse_lookup();
 	//
+	// Make sure that all of the pieces are in place to run CEA calculations.
+	//
 	_cea_exe_path = environment.get("CEA_EXE_PATH", "~/e3bin/cea2");
 	_cea_cases_path = environment.get("CEA_CASES_PATH", "~/e3bin/cea-cases");
 	string ceaExeFile = expandTilde(_cea_exe_path);
-	writeln("ceaExeFile=", ceaExeFile);
+	// writeln("ceaExeFile=", ceaExeFile); // DEBUG
 	if (!exists(ceaExeFile)) {
 	    throw new Exception("Cannot find cea2 exe file.");
 	}
+	string ceaThermoFile = buildNormalizedPath(expandTilde(_cea_cases_path),
+						   "thermo.inp");
+	// writeln("ceaThermoFile=", ceaThermoFile); // DEBUG
+	if (!exists(ceaThermoFile)) {
+	    throw new Exception("Cannot find cea2 thermo.inp file.");
+	}
+	auto copy_thermo = executeShell("cp " ~ ceaThermoFile ~ " .");
+	string ceaTransFile = buildNormalizedPath(expandTilde(_cea_cases_path),
+						  "trans.inp");
+	// writeln("ceaTransFile=", ceaTransFile); // DEBUG
+	if (!exists(ceaTransFile)) {
+	    throw new Exception("Cannot find cea2 trans.inp file.");
+	}
+	auto copy_trans = executeShell("cp " ~ ceaTransFile ~ " .");
+	//
+	runCEAProgram("thermo");
+	runCEAProgram("trans");
     } // end constructor
     
     this(lua_State *L) {
@@ -119,89 +154,74 @@ public:
 	return to!string(repr);
     }
 
+    // All of the update functions call up CEA behind the scene
+    // to do the real calculations.
+
     override void update_thermo_from_pT(GasState Q) const 
     {
-	// [TODO] replace
-	Q.rho = Q.p/(Q.Ttr*_Rgas);
-	Q.u = _Cv*Q.Ttr;
+	callCEA(Q, 0.0, 0.0, "pT", false);
     }
     override void update_thermo_from_rhoe(GasState Q) const
     {
-	// [TODO] replace
-	Q.Ttr = Q.u/_Cv;
-	Q.p = Q.rho*_Rgas*Q.Ttr;
+	callCEA(Q, 0.0, 0.0, "rhoe", false);
     }
     override void update_thermo_from_rhoT(GasState Q) const
     {
-	// [TODO] replace
-	Q.p = Q.rho*_Rgas*Q.Ttr;
-	Q.u = _Cv*Q.Ttr;
+	callCEA(Q, 0.0, 0.0, "rhoT", false);
     }
     override void update_thermo_from_rhop(GasState Q) const
     {
-	// [TODO] replace
-	Q.Ttr = Q.p/(Q.rho*_Rgas);
-	Q.u = _Cv*Q.Ttr;
+	callCEA(Q, 0.0, 0.0, "rhop", false);
     }
     
     override void update_thermo_from_ps(GasState Q, double s) const
     {
-	// [TODO] replace
-	Q.Ttr = _T1 * exp((1.0/_Cp)*((s - _s1) + _Rgas * log(Q.p/_p1)));
-	update_thermo_from_pT(Q);
+	callCEA(Q, 0.0, s, "ps", false);
     }
     override void update_thermo_from_hs(GasState Q, double h, double s) const
     {
-	// [TODO] replace
-	Q.Ttr = h / _Cp;
-	Q.p = _p1 * exp((1.0/_Rgas)*(_s1 - s + _Cp*log(Q.Ttr/_T1)));
-	update_thermo_from_pT(Q);
+	callCEA(Q, h, 0.0, "hs", false);
     }
     override void update_sound_speed(GasState Q) const
     {
-	// [TODO] replace
-	Q.a = sqrt(_gamma*_Rgas*Q.Ttr);
+	callCEA(Q, 0.0, 0.0, "sound_speed", false);
     }
     override void update_trans_coeffs(GasState Q)
     {
-	// [TODO] replace
-	// need to do something, or not
+	callCEA(Q, 0.0, 0.0, "pT", true);
     }
+
+    // The following functions return the most-recently-computed values.
+    // It is assumed that you have previously called an update function
+    // so that the values sitting in the CEAGas object are relevant.
+
     override double dudT_const_v(in GasState Q) const
     {
-	// [TODO] replace
 	return _Cv;
     }
     override double dhdT_const_p(in GasState Q) const
     {
-	// [TODO] replace
 	return _Cp;
     }
     override double dpdrho_const_T(in GasState Q) const
     {
-	// [TODO] replace
-	double R = gas_constant(Q);
-	return R*Q.Ttr;
+	return _Rgas * Q.Ttr;
     }
     override double gas_constant(in GasState Q) const
     {
-	// [TODO] replace
 	return _Rgas;
     }
     override double internal_energy(in GasState Q) const
     {
-	// [TODO] replace
 	return Q.u;
     }
     override double enthalpy(in GasState Q) const
     {
-	// [TODO] replace
 	return Q.u + Q.p/Q.rho;
     }
     override double entropy(in GasState Q) const
     {
-	// [TODO] replace
-	return _s1 + _Cp * log(Q.Ttr/_T1) - _Rgas * log(Q.p/_p1);
+	return _s;
     }
 
 private:
@@ -214,20 +234,58 @@ private:
     bool _withIons;
     string _cea_exe_path; // expect to find cea2 executable file here
     string _cea_cases_path; // expect to find thermo.lib and trans.lib
-    
-    // [TODO] replace
-    // Thermodynamic constants
+
+    // Thermodynamic parameters that can be filled in by running CEA.
     double _Rgas; // J/kg/K
-    double _gamma;   // ratio of specific heats
+    double _gamma; // ratio of specific heats
     double _Cv; // J/kg/K
     double _Cp; // J/kg/K
-    // Reference values for entropy
-    double _s1;  // J/kg/K
-    double _T1;  // K
-    double _p1;  // Pa
-
-    // [TODO] private helper functions to write and scan cea2 data files
-    // and to run the cea2 program.
+    double _s;  // J/kg/K
+    
+    void callCEA(GasState Q, double h, double s,
+		 string problemType="pT", bool transProps=true) const
+    {
+	// Write input file for CEA that is specific to the problemType.
+	// [TODO]
+	switch (problemType) {
+	case "pT":
+	    Q.rho = Q.p/(Q.Ttr*_Rgas);
+	    Q.u = _Cv*Q.Ttr;
+	    break;
+	case "rhoe":
+	    Q.Ttr = Q.u/_Cv;
+	    Q.p = Q.rho*_Rgas*Q.Ttr;
+	    break;
+	case "rhoT":
+	    Q.p = Q.rho*_Rgas*Q.Ttr;
+	    Q.u = _Cv*Q.Ttr;
+	    break;
+	case "rhop":
+	    Q.Ttr = Q.p/(Q.rho*_Rgas);
+	    Q.u = _Cv*Q.Ttr;
+	    break;
+	case "ps":
+	    // Q.Ttr = _T1 * exp((1.0/_Cp)*((s - _s1) + _Rgas * log(Q.p/_p1)));
+	    update_thermo_from_pT(Q);
+	    break;
+ 	case "hs":
+	    Q.Ttr = h / _Cp;
+	    // Q.p = _p1 * exp((1.0/_Rgas)*(_s1 - s + _Cp*log(Q.Ttr/_T1)));
+	    update_thermo_from_pT(Q);
+	    break;
+	case "sound_speed":
+	    Q.a = sqrt(_gamma*_Rgas*Q.Ttr);
+	    break;
+	default: 
+	    throw new Exception("Unknown problemType for CEA.");
+	}
+	//
+	// Actually run the CEA program
+	//
+	// Scan the output file for all of the bits that we need.
+	// [TODO]
+	//
+    } // end callCEA()
 } // end class CEAGgas
 
 version(cea_gas_test) {
