02caa1285e710633fcfba6a8519e9cb94e0c7e23
Peter Jacobs <peterj@mech.uq.edu.au>
2017-07-01 14:24:53 +1000

    Geom: add SlabVolume and WedgeVolume classes to build 3D volumes from a surface.
    These are likely to be useful for preparing essentially 2D meshes for use with
    OpenFoam (which accepts only 3D meshes).

--- src/geom/volume.d
+++ src/geom/volume.d
@@ -173,10 +173,12 @@ public:
 
     override Vector3 opCall(double r, double s, double t) const
     // Locate a point within the volume by blended linear interpolation.
-    // @param r: interpolation parameter i-direction west-->east, 0.0<=r<=1.0
-    // @param s: interpolation parameter j-direction south-->north, 0.0<=s<=1.0 
-    // @param t: interpolation parameter k-direction bottom-->top, 0.0<=t<=1.0
-    // @returns: a Vector3 value for the point.
+    // Input:
+    //     r: interpolation parameter i-direction west-->east, 0.0<=r<=1.0
+    //     s: interpolation parameter j-direction south-->north, 0.0<=s<=1.0 
+    //     t: interpolation parameter k-direction bottom-->top, 0.0<=t<=1.0
+    // Returns:
+    //     a Vector3 value for the point.
     {
 	Vector3 pW = faces[Face.west](s,t); 
 	Vector3 pE = faces[Face.east](s,t);
@@ -236,9 +238,11 @@ unittest {
 
 class SweptSurfaceVolume : ParametricVolume {
 public:
-    ParametricSurface face0123;
-    Path edge04;
-    Vector3[8] p;
+    ParametricSurface face0123; // The bottom surface.
+    Path edge04; // The path along which points from the bottom surface will be swept.
+    // Note that the line edge04(0.0) location anchors the p0 corner of the volume.
+    // Effectively, the bottom face will be moved to that location.
+    Vector3[8] p; // Corner points for the defined volume.
 
     this(const ParametricSurface face0123, const Path edge04)
     {
@@ -268,10 +272,12 @@ public:
 
     override Vector3 opCall(double r, double s, double t) const
     // Locate a point within the volume by blended linear interpolation.
-    // @param r: interpolation parameter i-direction west-->east, 0.0<=r<=1.0
-    // @param s: interpolation parameter j-direction south-->north, 0.0<=s<=1.0 
-    // @param t: interpolation parameter k-direction bottom-->top, 0.0<=t<=1.0
-    // @returns: a Vector3 value for the point.
+    // Input:
+    //     r: interpolation parameter i-direction west-->east, 0.0<=r<=1.0
+    //     s: interpolation parameter j-direction south-->north, 0.0<=s<=1.0 
+    //     t: interpolation parameter k-direction bottom-->top, 0.0<=t<=1.0
+    // Returns:
+    //     a Vector3 value for the point.
     {
 	Vector3 p_rst = edge04(t) + face0123(r, s) - face0123(0.0, 0.0);
 	return p_rst; 
@@ -285,8 +291,164 @@ public:
     } // end toString
 } // end SweptSurfaceVolume
 
+unittest {
+    Vector3[8] p;
+    p[0] = Vector3(0.0, 0.1, 0.0);
+    p[1] = Vector3(1.0, 0.1, 0.0);
+    p[2] = Vector3(1.0, 1.1, 0.0);
+    p[3] = Vector3(0.0, 1.1, 0.0);
+
+    p[4] = Vector3(0.0, 0.1, 3.0);
+    p[5] = Vector3(1.0, 0.1, 3.0);
+    p[6] = Vector3(1.0, 1.1, 3.0);
+    p[7] = Vector3(0.0, 1.1, 3.0);
+
+    ParametricSurface my_face = new CoonsPatch(p[0], p[1], p[2], p[3]);
+    auto my_box = new SweptSurfaceVolume(my_face, new Line(p[0],p[4]));
+    auto d = my_box(0.1, 0.1, 0.5);
+    assert(approxEqualVectors(d, Vector3(0.1, 0.2, 1.5)), "SweptSurfaceVolume my_box");
+} // end unittest
+
+
+class SlabVolume : SweptSurfaceVolume {
+public:
+    Vector3 dz; // slab thickness
+
+    this(const ParametricSurface face0123, const Vector3 dz)
+    // The simplest use case is that face0123 surface is in the x,y-plane and
+    // the thickness vector is in the z-direction, however, the volume can be
+    // made with more general orientation and the thickness vector just needs
+    // to be somewhat orthogonal, so that the volume does not collapse.
+    // The face0123 surface will form the bottom surface of the volume.
+    {
+	this.dz = dz;
+	Vector3 p0 = face0123(0.0,0.0);
+	super(face0123, new Line(p0, p0+dz));
+    }
+    
+    this(ref const(SlabVolume) other)
+    {
+	dz = other.dz;
+	Vector3 p0 = face0123(0.0,0.0);
+	super(other.face0123, new Line(p0, p0+dz));
+    }
+
+    override SlabVolume dup() const
+    {
+	return new SlabVolume(this.face0123, this.dz);
+    }
+
+    override string toString() const
+    {
+	string repr = "SlabVolume(face0123=" ~ to!string(face0123);
+	repr ~= ", dz=" ~ to!string(dz) ~ ")";
+	return repr;
+    } // end toString
+} // end SlabVolume
+
+unittest {
+    Vector3[8] p;
+    p[0] = Vector3(0.0, 0.1, 0.0);
+    p[1] = Vector3(1.0, 0.1, 0.0);
+    p[2] = Vector3(1.0, 1.1, 0.0);
+    p[3] = Vector3(0.0, 1.1, 0.0);
+
+    p[4] = Vector3(0.0, 0.1, 3.0);
+    p[5] = Vector3(1.0, 0.1, 3.0);
+    p[6] = Vector3(1.0, 1.1, 3.0);
+    p[7] = Vector3(0.0, 1.1, 3.0);
+
+    ParametricSurface my_face = new CoonsPatch(p[0], p[1], p[2], p[3]);
+    auto my_box = new SlabVolume(my_face, p[4]-p[0]);
+    auto d = my_box(0.1, 0.1, 0.5);
+    assert(approxEqualVectors(d, Vector3(0.1, 0.2, 1.5)), "SlabVolume my_box");
+} // end unittest
+
+
+class WedgeVolume : ParametricVolume {
+public:
+    ParametricSurface face0123; // The bottom surface.
+    double dtheta; // The angle through which points from the bottom surface will be swept.
+    Vector3[8] p; // Corner points for the defined volume.
+
+    this(const ParametricSurface face0123, double dtheta)
+    {
+	this.face0123 = face0123.dup();
+	this.dtheta = dtheta;
+	p[0] = face0123(0.0, 0.0);
+	p[1] = face0123(1.0, 0.0);
+	p[2] = face0123(1.0, 1.0);
+	p[3] = face0123(0.0, 1.0);
+	p[4] = sweep_through_arc(p[0], dtheta);
+	p[5] = sweep_through_arc(p[1], dtheta);
+	p[6] = sweep_through_arc(p[2], dtheta);
+	p[7] = sweep_through_arc(p[3], dtheta);
+    }
+    
+    this(ref const(WedgeVolume) other)
+    {
+	face0123 = other.face0123.dup();
+	dtheta = other.dtheta;
+	foreach(i; 0 .. 8) this.p[i] = other.p[i].dup();
+    }
+
+    override WedgeVolume dup() const
+    {
+	return new WedgeVolume(this.face0123, this.dtheta);
+    }
+
+    override Vector3 opCall(double r, double s, double t) const
+    // Locate a point within the volume by first interpolating on
+    // the bottom surface and then sweeping about the x-axis.
+    // Input:
+    //     r: interpolation parameter i-direction west-->east, 0.0<=r<=1.0
+    //     s: interpolation parameter j-direction south-->north, 0.0<=s<=1.0 
+    //     t: interpolation parameter k-direction bottom-->top, 0.0<=t<=1.0
+    // Returns:
+    //     a Vector3 value for the point.
+    {
+	Vector3 p_rs = face0123(r, s);
+	return sweep_through_arc(p_rs, t*dtheta); 
+    } // end opCall
+
+    override string toString() const
+    {
+	string repr = "WedgeVolume(face0123=" ~ to!string(face0123);
+	repr ~= ", dtheta=" ~ to!string(dtheta) ~ ")";
+	return repr;
+    } // end toString
+
+private:
+    Vector3 sweep_through_arc(const Vector3 p0, double theta) const
+    {
+	// We want to rotate the point about the x-axis, according to the right-hand rule.
+	// Angles are measured from the y-axis, positive as we swing around toward the z-axis.
+	// Refer to PJ's workbook page 36, 2017-07-01
+	double r = sqrt((p0.y)^^2 + (p0.z)^^2);
+	double theta0 = atan2(p0.z, p0.y);
+	double theta1 = theta0+theta;
+	return Vector3(p0.x, r*cos(theta1), r*sin(theta1));
+    } // end sweep_through_arc()
+} // end WedgeVolume
+
+unittest {
+    Vector3[8] p;
+    p[0] = Vector3(0.0, 0.1, 0.0);
+    p[1] = Vector3(1.0, 0.1, 0.0);
+    p[2] = Vector3(1.0, 1.1, 0.0);
+    p[3] = Vector3(0.0, 1.1, 0.0);
+
+    ParametricSurface my_face = new CoonsPatch(p[0], p[1], p[2], p[3]);
+    auto my_box = new WedgeVolume(my_face, 0.1);
+    auto d = my_box(0.1, 0.1, 0.5);
+    writeln("my_box=", my_box, " d=", d);
+    // expect p0.x=0.1 p0.y=0.2 and have set t=0.5, dtheta=0.1
+    assert(approxEqualVectors(d, Vector3(0.1, 0.2*cos(0.05), 0.2*sin(0.05))), "WedgeVolume my_box");
+} // end unittest
+
 
 class MeshVolume : ParametricVolume {
+    // TODO some day...
 } // end class MeshVolume
 
 unittest {
