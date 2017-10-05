2baa1fa61eff144af6bd0fcd1ec4b50681462162
Rowan J. Gollan <r.gollan@uq.edu.au>
2017-10-05 16:20:11 +1000

    gas: Vibrationally specific nitrogen thermo routines implemented.
    This commit is a copy of Katrina Sklavos' work on a
    vibrationally specific model for diatomic nitrogen.

--- src/gas/vib_specific_nitrogen.d
+++ src/gas/vib_specific_nitrogen.d
@@ -40,10 +40,12 @@ class VibSpecificNitrogen: GasModel {
 public:
     this()
     {
-	_n_species = 1;
+	_n_species = N_VIB_LEVELS;
 	_n_modes = N_VIB_LEVELS;
-	_species_names.length = 1;
-	_species_names[0] = "N2";
+	_species_names.length = _n_species;
+	foreach (isp; 0 .. _n_species) {
+            _species_names[isp] = format("N2-vib-%d", isp);
+        }
 	create_species_reverse_lookup();
     }
 
@@ -56,29 +58,38 @@ public:
 
     override void update_thermo_from_pT(GasState Q) const 
     {
-	// [TODO:KS]
-	// Fill in Q.rho and Q.u and Q.u_modes
-	// Q.rho = ....;
-	// Q.u = ....;
-	// foreach (imode; 0 .. _n_modes) {
-	//     Q.u_modes = ....;
-	// }
+	Q.rho = Q.p/(_R_N2*Q.Ttr);
+	
+	foreach (imode; 0 .. _n_modes) {
+	     Q.u_modes[imode] = (Avogadro_number/_M_N2) * Q.massf[imode] * vib_energy(imode);
+	}
+	Q.u = 2.5 * _R_N2 * Q.Ttr;
+	
     }
     override void update_thermo_from_rhou(GasState Q) const
     {
-	// [TODO:KS]
-	// Fill in Q.p, Q.Ttr and Q.T_modes
+	Q.Ttr = 0.4 * Q.u * (1/_R_N2);
+	Q.p = Q.rho * _R_N2 * Q.Ttr;
+	Q.T_modes[0] = compute_Tvib(Q, 200.0, 1000.0, 1e-4);
+
     }
     override void update_thermo_from_rhoT(GasState Q) const
     {
-	// [TODO:KS]
-	// Fill in Q.p, Q.u and Q.u_modes 
+	Q.p = Q.rho * _R_N2 * Q.Ttr;
+	Q.u = 2.5 * _R_N2 * Q.Ttr;
+	
+	foreach (imode; 0 .. _n_modes) {
+	     Q.u_modes[imode] = (Avogadro_number/_M_N2) * Q.massf[imode] * vib_energy(imode);
+	}
 
     }
     override void update_thermo_from_rhop(GasState Q) const
     {
-	// [TODO:KS]
-	// Fill in Q.Ttr, Q.u
+	Q.Ttr = 0.4 * Q.u * (1/_R_N2);
+	Q.u = 2.5 * _R_N2 * Q.Ttr;
+	foreach (imode; 0 .. _n_modes) {
+	     Q.u_modes[imode] = (Avogadro_number/_M_N2) * Q.massf[imode] * vib_energy(imode);
+	}
     }
     
     override void update_thermo_from_ps(GasState Q, double s) const
@@ -94,8 +105,7 @@ public:
     }
     override void update_sound_speed(GasState Q) const
     {
-	// [TODO:KS]
-	// Fill in Q.a
+	Q.a = (_gamma * _R_N2 * Q.Ttr)^^0.5; 
     }
     override void update_trans_coeffs(GasState Q)
     {
@@ -105,7 +115,7 @@ public:
     }
     override double dudT_const_v(in GasState Q) const
     {
-	throw new Error("ERROR: VibSpecificNitrogen:dudT_const_v NOT IMPLEMENTED.");
+        return _R_N2/(_gamma - 1.0);
     }
     override double dhdT_const_p(in GasState Q) const
     {
@@ -134,8 +144,74 @@ public:
 
 private:
     double _R_N2 = 296.805; // gas constant for N2
-    // [TODO:KS]
-    // You may declare any extra data you need in this area.
+    double _M_N2 = 0.0280134;
+    double _gamma = 7./5.; // ratio of specific heats.
+    double kB = Boltzmann_constant;
+    double vib_energy(int i) const 
+    {
+        int I = i+1;
+        double w_e = 235857;
+        double we_xe = 1432.4;
+        double we_ye = -0.226;
+        double h = 6.626e-34;
+        double c = 2.998e8;
+        double e = h*c * (w_e*(I-0.5) - we_xe*(I-0.5)^^2 + we_ye*(I-0.5)^^3);
+        return e;
+     }
+     
+     double boltzmann_eq(double Tf1) const
+     {
+        double summ = 0;
+        
+        foreach(ei; 0 .. N_VIB_LEVELS) {
+             summ += exp(-vib_energy(ei) / (kB*Tf1));
+        }        
+        double e1 = vib_energy(0);
+        double temp_func = (exp(-e1/(kB*Tf1)) / summ);
+        return temp_func;   
+     }
+     
+     double compute_Tvib(GasState Q, double x0, double x1, double tol) const
+     {
+        //this method (secant) is used to compute Tf1
+        double init_x0 = x0;
+        double init_x1 = x1;
+        double fx0 = boltzmann_eq(x0) - Q.massf[0];
+        double fx1 = boltzmann_eq(x1) - Q.massf[0];
+        double max_it = 100; 
+        
+        foreach(n; 0 .. max_it) {
+            if (abs(fx1) < tol) {return x1;}
+        
+            double x2 = ((x0*fx1) - (x1*fx0)) / (fx1 - fx0);
+            x0 = x1;
+            x1 = x2;
+            fx0 = fx1;
+            fx1 = boltzmann_eq(x2) - Q.massf[0];
+        } //end foreach
+        return x1;
+     }
+     
+    
+     /**
+     //BELOW FUNCTION ACTUALLY BELONGS IN THERMOCHEMICALREACTOR CLASS
+     // Indices need checking
+     double F_coeff(int i, double Temp) const
+     {
+        double I = i + 1;
+        double ft = 10e-6 * Avogadro_number * exp(-3.24093 - (140.69597/Temp^^0.2));
+        double delta_t = 0.26679 - (6.99237e5 * Temp) + (4.70073e-9 * Temp^^2);
+        double f = (I-1) * ft * exp((I-2)*delta_t);
+        return f;     
+     }   
+     
+     double B_coeff(int i, double Temp) const
+     {
+        double B = F_coeff(i,Temp) * exp(-(vib_energy(i) - vib_energy(i-1)) / (kB * Temp));
+        // F_coeff already uses i+1
+        return B;
+     } 
+     **/
 
 } // end class VibSpecificNitrogen
 
@@ -145,20 +221,39 @@ version(vib_specific_nitrogen_test) {
 
     int main() {
 	auto gm = new VibSpecificNitrogen();
-	auto Q = new GasState(1, N_VIB_LEVELS);
+	auto Q = new GasState(N_VIB_LEVELS, N_VIB_LEVELS);
 	Q.p = 1.0e5;
 	Q.Ttr = 300.0;
 	foreach (imode; 0 .. gm.n_modes()) {
 	    Q.T_modes[imode] = 300.0;
 	}
-	Q.massf[0] = 1.0;
+	Q.massf[] = 1.0/10;
 
-	// [TODO:KS]
-	// Test update_thermo_from_pT
+        double R_N2 = 296.805; // gas constant for N2
+        double M_N2 = 0.0280134;
+        double gamma = 7./5.; // ratio of specific heats.
+
 	gm.update_thermo_from_pT(Q);
-	// assert(approxEqual(Q.rho, ...., 1.0e-6), failedUnitTest());
+	double my_rho = 1.0e5 / (R_N2 * 300.0);
+	assert(approxEqual(Q.rho, my_rho, 1.0e-6), failedUnitTest());
 	
-	// and other tests.
+	double my_u = 2.5 * R_N2 * 300.0;
+	assert(approxEqual(Q.u, my_u, 1.0e-6), failedUnitTest());
+	
+	//double my_u_modes = (Avogadro_number/M_N2) * 1.0 * vib_energy(1); //using mode 1 for test (massf = 1.0 here)
+	//assert(approxEqual(Q.u_modes[0], my_u_modes, 1.0e-6), failedUnitTest());
+	
+	gm.update_trans_coeffs(Q);
+	assert(approxEqual(Q.mu, 0.0, 1.0e-6), failedUnitTest());
+	assert(approxEqual(Q.k, 0.0, 1.0e-6), failedUnitTest());
+	
+	gm.update_sound_speed(Q);
+	double my_a = (gamma * R_N2 * 300.0)^^0.5;
+	assert(approxEqual(Q.a, my_a, 1.0e-6), failedUnitTest());
+	//assert(approxEqual(1.0,2.0,1.0e-6), failedUnitTest()); //fail on purpose for check
+	
 	return 0;
     }
 }
+
+
