776e42921da53b8511e8ac16a5306ed5cf6efb35
Peter Jacobs <peterj@mech.uq.edu.au>
2017-11-26 18:45:40 +1000

    Geometry refactor: a file missing from the previous commit.

---
+++ src/geom/luawrap/package.d
@@ -0,0 +1,13 @@
+module geom.luawrap;
+
+public import geom.luawrap.luageom;
+public import geom.luawrap.luagpath;
+public import geom.luawrap.luasurface;
+public import geom.luawrap.luavolume;
+
+public import geom.luawrap.luagrid;
+public import geom.luawrap.luasgrid;
+public import geom.luawrap.luausgrid;
+
+public import geom.luawrap.luasketch;
+public import geom.luawrap.luaunifunction;
