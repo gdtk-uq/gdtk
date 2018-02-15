30d9f904081c411325dce377237e083b7f938e3b
Rowan J. Gollan <r.gollan@uq.edu.au>
2018-02-15 11:54:14 +1000

    examples/eilmer: Updated gridpro-import with Fabian Zander's partial 3D sphere.

--- examples/eilmer/3D/gridpro-import/run-test.sh
+++ examples/eilmer/3D/gridpro-import/run-test.sh
@@ -2,8 +2,8 @@
 
 prep-gas ideal-air.inp ideal-air.lua
 e4shared --job=test-import --prep
-e4shared --job=test-import --post --vtk-xml
+mpirun -np 5 e4mpi --job=test-import --run
+e4shared --job=test-import --post --vtk-xml --tindx-plot=all
 
-echo "Grid may be viewed in paraview. Expected output is 3 blocks."
+echo "Grid may be viewed in paraview. Expected output is 5 blocks."
 echo "> paraview plot/test-import.pvd"
-
--- examples/eilmer/3D/gridpro-import/test-import.lua
+++ examples/eilmer/3D/gridpro-import/test-import.lua
@@ -1,15 +1,25 @@
 -- Author: Rowan J. Gollan
 -- Date: 2016-04-22
+--
+-- History:
+--   2018-02-14 : Use Fabian Zander's example with BC labels.
 
 gproGrid = "blk.tmp"
+gproConn = "blk.tmp.conn"
 gproPty = "blk.tmp.pty"
 
 config.title = "Gridpro importer test"
 config.dimensions = 3
 
+config.max_time = 2.0e-4
+config.max_step = 10000000
+config.dt_init = 1.0e-9
+config.dt_plot = config.max_time/10.0
+
 -- Set up a dummy flow condition for the test
 setGasModel('ideal-air.lua')
-inflow = FlowState:new{p=1.0e5, T=300, velx=100.0}
+initial = FlowState:new{p=1.0e4, T=300, velx=0.0}
+inflow = FlowState:new{p=1.0e5, T=300, velx=-1000.0}
 
 -- Read in GridPro grids.
 -- The GridPro grid file contains all the blocks in one file.
@@ -21,12 +31,17 @@ grids = importGridproGrid(gproGrid, scale)
 -- Now we attach the grids to blocks
 blks = {}
 for i,g in ipairs(grids) do
-   blks[#blks+1] = FluidBlock:new{grid=g, initialState=inflow}
+   blks[#blks+1] = FluidBlock:new{grid=g, initialState=initial}
 end
 
 print("Number of blocks constructed: ", #blks)
 
--- [TODO]: Apply BCs function not yet available.
+applyGridproConnectivity(gproConn, blks)
 
+bcMap = {surface_4=WallBC_WithSlip:new{},
+   	 surface_1=WallBC_NoSlip_FixedT:new{Twall=30000},
+   	 surface_2=InFlowBC_Supersonic:new{flowState=inflow},
+   	 surface_3=OutFlowBC_Simple:new{},
+   	 surface_5=WallBC_WithSlip:new{}}
 
-
+applyGridproBoundaryConditions(gproPty, blks, bcMap)
