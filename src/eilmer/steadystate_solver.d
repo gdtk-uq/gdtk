4af84c084b17a89b459835385b56195a7cfa9889
Kyle Damm <kyle.damm@uqconnect.edu.au>
2018-03-02 17:18:59 +0900

    eilmer: Fixed flux calculator naming bug in steady-state solver.
    Updated the evalRHS() method in steadystate_solver.d to follow the new
    adaptive flux calculator naming format introduced in r1537.

--- src/eilmer/steadystate_solver.d
+++ src/eilmer/steadystate_solver.d
@@ -895,7 +895,8 @@ void evalRHS(double pseudoSimTime, int ftl)
     // We don't want to switch between flux calculator application while
     // doing the Frechet derivative, so we'll only search for shock points
     // at ftl = 0, which is when the F(U) evaluation is made.
-    if ( ftl == 0 && GlobalConfig.flux_calculator == FluxCalculator.adaptive) {
+    if ( ftl == 0 &&
+         (GlobalConfig.flux_calculator == FluxCalculator.adaptive_efm_ausmdv || FluxCalculator.adaptive_hlle_ausmdv) ) {
         foreach (blk; parallel(localFluidBlocks,1)) {
             blk.detect_shock_points();
         }
