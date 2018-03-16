5c03511805d43ad45f2a50dec24af97ea590c8e7
Kyle Damm <kyle.damm@uqconnect.edu.au>
2018-03-16 15:03:46 +0900

    eilmer: added optdriver.d -- the optimsation driver for adjoint optimisation framework
    The optimisation driver packages together Eilmer's steady-state solver (e4sss), and Eilmer's
    shape sensitivity calculator (e4ssc) for coupling with the open-source optimsation package
    DAKOTA (https://dakota.sandia.gov/).
    
    This first commit of the optimisation driver (e4opt) only reads the DAKOTA input script (this commit also adds in the
    compile instructions to make e4opt).

---
+++ src/eilmer/optdriver.d
@@ -0,0 +1,86 @@
+/** optdriver.d
+ * 
+ * Eilmer4 optimisation driver code.
+ *
+ * This is the core driver for performing CFD-based optimisation with Eilmer4. 
+ *
+ * It is written to interface with the open-source DAKOTA optimisation software,
+ * found here, https://dakota.sandia.gov/
+ *
+ * Author: Kyle D.
+ * Date: 2018-03-16
+ *
+**/
+
+import core.stdc.stdlib : exit;
+import core.memory;
+import std.stdio;
+import std.file;
+import std.format;
+import std.conv;
+import std.parallelism;
+import std.algorithm;
+import std.getopt;
+import std.string;
+import std.array;
+import std.math;
+import std.datetime;
+import std.process;
+
+void main(string[] args) {
+    writeln("Eilmer optimisation driver:");
+    writeln("Revision: PUT_REVISION_STRING_HERE");
+
+    // some internal settings
+    bool computeObjFns = false;
+    bool computeObjGrads = false;
+    string runFlowSolverCmd = "run_flow_solver.sh";
+    string runGradSolverCmd = "run_shape_sensitivity_calc.sh";
+    int nfns; // number of functions (objective/constraints)
+    int nrsps; // number of expected responses
+    int ndvars; // number of design variables
+    double[] dvars; // design variable values
+    double[] gvars; // gradients of design variables
+    string dakotaInFileName = "params.in";
+    string dakotaResultFileName = "results.out";
+    
+    // read DAKOTA input file
+    assert(exists(dakotaInFileName), "DAKOTA input file not present");
+    auto f = File(dakotaInFileName, "r");
+    // first line holds ndvars
+    {
+        auto lineContent = f.readln().strip();
+        auto tokens = lineContent.split();
+        ndvars = to!int(tokens[0]);
+    }
+    // the following ndvar lines holds the design variable values
+    foreach ( i; 0..ndvars) {
+        auto lineContent = f.readln().strip();
+        auto tokens = lineContent.split();
+        dvars ~= to!double(tokens[0]);
+    }
+    // the following line holds nfns
+    {
+        auto lineContent = f.readln().strip();
+        auto tokens = lineContent.split();
+        nfns = to!int(tokens[0]);
+    }
+    // the following nfns lines holds the number of expected response data for each fns
+    // NB. for now I assume that each variable will asks for the same number of response data (so just read first variable)
+    // also, I assume this value will either be:
+    //          1          (return the ojective function evaluation), or
+    //          nvars      (return objective function gradients), or
+    //          1 + nvars  ( return objective function evaluation, and gradients)
+    {
+        auto lineContent = f.readln().strip();
+        auto tokens = lineContent.split();
+        nrsps = to!int(tokens[0]);
+        if ( nrsps == 1) { computeObjFns = true; computeObjGrads = false; }
+        else if ( nrsps == ndvars) { computeObjFns = false; computeObjGrads = true; }
+        else if ( nrsps == 1+ndvars) { computeObjFns = true; computeObjGrads = true; }
+        else assert(0, "No requested response data in DAKOTA input file");
+    }
+    // finished reading DAKOTA input file
+    
+    writeln(computeObjFns, ", ",  computeObjGrads, ", ", dvars);
+}
