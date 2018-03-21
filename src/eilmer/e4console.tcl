a694bcc12121a1cb65ea7f950618563da0896850
Peter Jacobs <peterj@mech.uq.edu.au>
2018-03-21 11:30:00 +1000

    Eilmer: update e4console to use themed widgets instead of Bwidget widgets.
    Doing this catch-up, we get one fewer dependency, simpler code and a nicer GUI.
    What's not to like about that?

--- src/eilmer/e4console.tcl
+++ src/eilmer/e4console.tcl
@@ -16,7 +16,7 @@
 
 proc showAboutBox {} {
     tk_messageBox -type ok -title "About e4console.tcl" \
-	-message "e4console V0.1\nP.J. 19-Mar-2018"
+	-message "e4console V0.2\nP.J. 21-Mar-2018"
 }; # end proc showAboutBox
 
 proc showGeneralHelp {} {
@@ -25,8 +25,6 @@ proc showGeneralHelp {} {
 }; # end proc showGeneralHelp
 
 # -----------------------------------------------------------------------
-
-package require BWidget
 
 # When looking at the OS name, we are expecting values such as
 # "GNU/Linux" or "Cygwin"
@@ -38,6 +36,10 @@ if {[string equal $tcl_platform(platform) "windows"] && \
 	![string equal $::my_platform "Cygwin"] } {
     # we are on a plain Win32 system (without Cygwin)
     console show
+}
+if { [string equal $tcl_platform(platform) "unix"] } {
+    # Something nicer than default, I think.
+    ttk::style theme use clam
 }
 set ::debugFlag 1
 # Get the directory of the script and assume that the
@@ -71,7 +73,7 @@ set optionsPost(tindxPlot) "all"
 set optionsPost(outputFormat) "vtk-xml"
 set optionsPost(addMach) 0
 set optionsPost(addPitotP) 0
-set optionePost(addTotalP) 0
+set optionsPost(addTotalP) 0
 set optionsPost(extras) ""
 
 set logFont {courier 12}
@@ -163,10 +165,10 @@ proc runPost {} {
 	append addVarsStr "mach,"
     }
     if { $::optionsPost(addPitotP) } {
-	append cmd "pitot-p,"
+	append addVarsStr "pitot,"
     }
     if { $::optionsPost(addTotalP) } {
-	append cmd "total-p,"
+	append addVarsStr "total-p,"
     }
     if [string length $addVarsStr] {
         append cmd " --add-vars=\"$addVarsStr\""
@@ -255,7 +257,7 @@ proc postMainMenu {} {
    # m2 is a second-level menu
    set m0 [menu .menubar]
    . config -menu $m0
-   wm title . "e4console: Eilmer4 supervisor program."
+   wm title . "Eilmer4 Job Supervisor"
 
    set m1 [menu $m0.file -tearoff 0]
    $m0 add cascade -label File -menu $m1
@@ -297,50 +299,51 @@ proc postMainMenu {} {
 
 proc initializeDisplay {} {
     postMainMenu
-
+    # Items that make up the main window...
     #
-    # A line indicating the current jobName and extension.
+    # (1) A line indicating the current jobName and extension.
     #
-    set jobNameFrame [LabelFrame .fn -text "Job Name:" -side left -borderwidth 3 -relief flat]
-    set jobNameEntry [entry [$jobNameFrame getframe].jne -textvariable ::jobName -width 50]
+    set jobNameFrame [ttk::labelframe .jnf -text "Job indentity: " -labelanchor nw]
+    pack [ttk::label $jobNameFrame.nl -text "Name:"] -side left
+    set jobNameEntry [ttk::entry $jobNameFrame.jne -textvariable ::jobName -width 50]
     bind $jobNameEntry <Return> { readScriptFile }
     pack $jobNameEntry -side left -expand 1 -fill x
-    set extLabel [label [$jobNameFrame getframe].el -text "Script extension:"]
-    pack $extLabel -side left
-    set extEntry [entry [$jobNameFrame getframe].ee -textvariable ::scriptExt -width 20 ]
+    pack [ttk::label $jobNameFrame.el -text "extension:"] -side left
+    set extEntry [ttk::entry $jobNameFrame.ee -textvariable ::scriptExt -width 20 ]
     pack $extEntry -side left -expand 1 -fill x
-    pack $jobNameFrame -side top -expand 1 -fill both
+    pack $jobNameFrame -side top -expand 1 -fill both -pady 5
     #
-    # A scrolling text window for the content of the user's script.
+    # (2) A scrolling text window for the content of the user's script.
     #
-    set scriptFrame [LabelFrame .tf0 -text "Input script:" -side top]
-    set ::scriptTextWidget [text [$scriptFrame getframe].t -height 20 -width 70 \
+    set scriptFrame [ttk::labelframe .tf0 -text "Input script:" -labelanchor nw]
+    set ::scriptTextWidget [text $scriptFrame.t -height 20 -width 70 \
 				-font $::logFont -wrap char \
-		       -yscrollcommand [list [$scriptFrame getframe].vsb set] ]
-    set scriptScrollBar [scrollbar [$scriptFrame getframe].vsb -orient vertical \
+		       -yscrollcommand [list $scriptFrame.vsb set] ]
+    set scriptScrollBar [ttk::scrollbar $scriptFrame.vsb -orient vertical \
 			     -command {$::scriptTextWidget yview} ]
     pack $::scriptTextWidget -side left -expand 1 -fill both
     pack $scriptScrollBar -side left -fill y
-    pack $scriptFrame -side top -expand 1 -fill both
+    pack $scriptFrame -side top -expand 1 -fill both -pady 5
     #
-    # A scrolling text window for the text output from running the programs.
+    # (3) A scrolling text window for the text output from running the programs.
     #
-    set textFrame [LabelFrame .tf -text "Log of output:" -side top]
-    set ::logTextWidget [text [$textFrame getframe].t -height 10 -width 70 \
+    set textFrame [ttk::labelframe .tf -text "Log of output:" -labelanchor nw]
+    set ::logTextWidget [text $textFrame.t -height 10 -width 70 \
 			     -font $::logFont -wrap char \
-		       -yscrollcommand [list [$textFrame getframe].vsb set] ]
-    set textScrollBar [scrollbar [$textFrame getframe].vsb -orient vertical \
+		       -yscrollcommand [list $textFrame.vsb set] ]
+    set textScrollBar [ttk::scrollbar $textFrame.vsb -orient vertical \
 			   -command {$::logTextWidget yview} ]
     pack $::logTextWidget -side left -expand 1 -fill both
     pack $textScrollBar -side left -fill y
-    pack $textFrame -side top -expand 1 -fill both
+    pack $textFrame -side top -expand 1 -fill both -pady 5
     #
-    # A status line giving some hint as to the current state.
+    # (4) A status line giving some hint as to the current state.
     #
-    set statusFrame [LabelFrame .fs -text "Status:" -side left -borderwidth 3 -relief flat]
-    set statusEntry [entry [$statusFrame getframe].state -textvariable ::statusText ]
+    set statusFrame [ttk::labelframe .fs -text "Status:" \
+                         -labelanchor w -borderwidth 3 -relief flat]
+    set statusEntry [ttk::entry $statusFrame.state -textvariable ::statusText ]
     pack $statusEntry -side right -expand 1 -fill x
-    pack $statusFrame -side bottom -expand 1 -fill both
+    pack $statusFrame -side bottom -expand 1 -fill both -pady 5
 }; # end proc initializeDisplay
 
 proc clearLogText {} {
@@ -387,100 +390,99 @@ proc displayOptionsDialog {} {
     } else {
         # Create the dialog widget and show it.
         toplevel .optionsDialog
-        set nb [NoteBook .optionsDialog.notebook -height 200 -width 400]
-        set bF [frame .optionsDialog.buttonFrame]
+        set nb [ttk::notebook .optionsDialog.notebook]
+        set bF [ttk::frame .optionsDialog.buttonFrame]
 	#--------------------------------------------------
-        set page [$nb insert end 1 -text "Prepare grids"]
-	set lab0 [label $page.lab0 -text "Use e4shared to generate grid and initial flow-field data."]
-	set frame1 [frame $page.f1 -relief groove -borderwidth 2]
-	set lab1 [label $frame1.lab1 -text "Standard options:"]
-        set lab2 [label $frame1.lab2 -text "None, for the moment."]
+        $nb add [ttk::frame $nb.page1] -text "Prepare grids"
+	set lab0 [ttk::label $nb.page1.lab0 -text "Use e4shared to generate grid and initial flow-field data."]
+	set frame1 [ttk::frame $nb.page1.f1 -relief groove -borderwidth 2]
+	set lab1 [ttk::label $frame1.lab1 -text "Standard options:"]
+        set lab2 [ttk::label $frame1.lab2 -text "None, for the moment."]
 	pack $lab1 $lab2 -anchor w
 	pack $lab0 -anchor w -pady 10
 	pack $frame1 -anchor w -fill x
 	#--------------------------------------------------
-        set page [$nb insert end 2 -text "Run simulation"]
-	set lab0 [label $page.lab0 -text "Use e4shared to generate subsequent flow-field data."]
-	set fWallClock [frame $page.fWC]
-	set labWallClock [label $fWallClock.labwc -text "Maximum Wall-Clock (sec):"]
-	set entryWallClock [entry $fWallClock.entrywc -width 20 \
+        $nb add [ttk::frame $nb.page2] -text "Run simulation"
+	set lab0 [ttk::label $nb.page2.lab0 -text "Use e4shared to generate subsequent flow-field data."]
+	set fWallClock [ttk::frame $nb.page2.fWC]
+	set labWallClock [ttk::label $fWallClock.labwc -text "Maximum Wall-Clock (sec):"]
+	set entryWallClock [ttk::entry $fWallClock.entrywc -width 20 \
 			     -textvariable ::optionsMain(maxWallClock)]
 	pack $labWallClock -side left
 	pack $entryWallClock -side left
-	set labExtras [label $page.labe -text "Extra options:"]
-	set entryExtras [entry $page.entrye -width 60 \
+	set labExtras [ttk::label $nb.page2.labe -text "Extra options:"]
+	set entryExtras [ttk::entry $nb.page2.entrye -width 60 \
 			     -textvariable ::optionsMain(extras)]
 	pack $lab0 -anchor w -pady 10
         pack $fWallClock $labExtras $entryExtras -anchor w
 	#--------------------------------------------------
-        set page [$nb insert end 3 -text "Extract field data"]
-	set lab0 [label $page.lab0 -text "Use e4shared to extract flow-field data."]
-	set frame1 [frame $page.f1]
-	set labTime [label $frame1.labt -text "TimeIndex (integer, 'last' or 'all'):"]
-	set ::tindxListBox [ComboBox $frame1.entryt -width 20 -entrybg white \
+        $nb add [ttk::frame $nb.page3] -text "Extract field data"
+	set lab0 [ttk::label $nb.page3.lab0 -text "Use e4shared to extract flow-field data."]
+	set frame1 [ttk::frame $nb.page3.f1]
+	set labTime [ttk::label $frame1.labt -text "TimeIndex (integer, 'last' or 'all'):"]
+	set ::tindxListBox [ttk::combobox $frame1.entryt -width 20 \
 				-textvariable ::optionsPost(tindxPlot) \
 				-values [list "all" "last" "0 0.0"] ]
-	set checkTime [button $frame1.butt -text "Check times file" \
+	set checkTime [ttk::button $frame1.butt -text "Check times file" \
 			   -command checkTimesFile]
 	pack $labTime -side left
 	pack $::tindxListBox -side left
 	pack $checkTime -side left
 
-	set buttonFrame [frame $page.bf]
-	set formatFrame [LabelFrame $buttonFrame.ff -text "Output Format:" \
-			     -side top -relief groove -borderwidth 2]
-	set rb2 [radiobutton [$formatFrame getframe].rb2 \
+	set buttonFrame [ttk::frame $nb.page3.bf]
+	set formatFrame [ttk::labelframe $buttonFrame.ff -text "Output Format:" \
+                             -labelanchor nw -borderwidth 2]
+	set rb2 [ttk::radiobutton $formatFrame.rb2 \
 		     -text "TECPLOT binary" \
 		     -variable ::optionsPost(outputFormat) \
 		     -value "tecplot"]
-	set rb3 [radiobutton [$formatFrame getframe].rb3 \
+	set rb3 [ttk::radiobutton $formatFrame.rb3 \
 		     -text "VTK-XML" \
 		     -variable ::optionsPost(outputFormat) \
 		     -value "vtk-xml"]
-	set rb4 [radiobutton [$formatFrame getframe].rb4 \
+	set rb4 [ttk::radiobutton $formatFrame.rb4 \
 		     -text "TECPLOT ascii" \
 		     -variable ::optionsPost(outputFormat) \
 		     -value "tecplot-ascii"]
 	grid $rb2 -row 0 -column 0 -sticky w
 	grid $rb3 -row 1 -column 0 -sticky w
 	grid $rb4 -row 2 -column 0 -sticky w
+	pack $formatFrame -side left -anchor w -padx 5 -pady 5
 
-	set deFrame [LabelFrame $buttonFrame.lf -text "Add data elements:" \
-			 -side top -relief groove -borderwidth 2]
-	set cb1 [checkbutton [$deFrame getframe].cb1 \
+	set deFrame [ttk::labelframe $buttonFrame.lf -text "Add data elements:" \
+			 -labelanchor nw -borderwidth 2]
+	set cb1 [ttk::checkbutton $deFrame.cb1 \
 		     -text "Mach number" \
 		     -variable ::optionsPost(addMach)]
-	set cb2 [checkbutton [$deFrame getframe].cb2 \
+	set cb2 [ttk::checkbutton $deFrame.cb2 \
 		     -text "Pitot pressure" \
 		     -variable ::optionsPost(addPitotP)]
-	set cb3 [checkbutton [$deFrame getframe].cb3 \
+	set cb3 [ttk::checkbutton $deFrame.cb3 \
 		     -text "Total pressure" \
 		     -variable ::optionsPost(addTotalP)]
 	grid $cb1 -row 0 -column 0 -sticky w
 	grid $cb2 -row 1 -column 0 -sticky w
 	grid $cb3 -row 2 -column 0 -sticky w
+	pack $deFrame -side right -anchor e -padx 5 -pady 5
 
-	pack $formatFrame -side left -anchor w
-	pack $deFrame -side right -anchor e -padx 5
-
-	set labExtras [label $page.labe -text "Extra options:"]
-	set entryExtras [entry $page.entrye -width 60 \
+	set labExtras [ttk::label $nb.page3.labe -text "Extra options:"]
+	set entryExtras [ttk::entry $nb.page3.entrye -width 60 \
 			     -textvariable ::optionsPost(extras)]
 
 	pack $lab0 -anchor w -pady 10 
         pack $frame1 $buttonFrame $labExtras $entryExtras -anchor w
 	#--------------------------------------------------
 
-        NoteBook::compute_size $nb
-        $nb raise 1
+        $nb select $nb.page1
+        ttk::notebook::enableTraversal $nb
 
-        set bDismiss [button $bF.dismiss -text "Lower Window" \
+        set bDismiss [button $bF.dismiss -text "Dismiss or Lower this Window" \
             -command {lower .optionsDialog .} ]
         pack $bDismiss -side left -padx 4 -pady 4
 
         pack $nb $bF
     }; # end if
-    wm title .optionsDialog "e4console options for programs."
+    wm title .optionsDialog "Options for actions"
 }; # end proc
 
 # --------------------------------------------------------
