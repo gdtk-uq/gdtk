4bd2a5e720af336a33cc2eae0a14a12430b83c2a
Peter Jacobs <peterj@mech.uq.edu.au>
2018-04-14 16:13:29 +1000

    Update the little nitrogen-reactor example.

--- examples/eilmer/2D/cylinder-dlr-n90/cpu-chem/prep-chem.sh
+++ examples/eilmer/2D/cylinder-dlr-n90/cpu-chem/prep-chem.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# run.sh
+# prep-chem.sh
 prep-gas nitrogen-2sp.inp nitrogen-2sp.lua
 prep-chem nitrogen-2sp.lua nitrogen-2sp-2r.lua e4-chem.lua
--- examples/eilmer/2D/reactor-n2/reactor.lua
+++ examples/eilmer/2D/reactor-n2/reactor.lua
@@ -1,33 +1,29 @@
 -- reactor.lua
 -- A no-flow chemical reactor.
--- RG & PJ  2015-03-10
+-- RG & PJ
+--   2015-03-10: ported from Eilmer3
+--   2018-04-14: refreshed
 --
-
-job_title = "Nitrogen reactor -- recombination driven."
-print(job_title)
-
+config.job_title = "Nitrogen reactor -- recombination driven."
+print(config.job_title)
 config.dimensions = 2
-config.job_title = job_title
-
+--
 setGasModel('nitrogen-2sp.lua')
 config.reacting = true
 config.reactions_file = 'chem.lua'
-
 initial = FlowState:new{p=1.0e5, T=4000.0, massf={N2=0.8, N=0.2}}
-
+--
 -- Geometry is a square
-a = Vector3:new{x=0.0, y=0.0}
-b = Vector3:new{x=0.0, y=0.01}
-c = Vector3:new{x=0.01, y=0.0}
-d = Vector3:new{x=0.01, y=0.01}
-patch0 = makePatch{north = Line:new{p0=b, p1=d},
-		   east = Line:new{p0=c, p1=d},
-		   south = Line:new{p0=a, p1=c},
-		   west = Line:new{p0=a, p1=b}}
-
+pnts = {}
+pnts.a = Vector3:new{x=0.0, y=0.0}
+pnts.b = Vector3:new{x=0.0, y=0.01}
+pnts.c = Vector3:new{x=0.01, y=0.0}
+pnts.d = Vector3:new{x=0.01, y=0.01}
+patch0 = CoonsPatch:new{p00=pnts.a, p01=pnts.b, p10=pnts.c, p11=pnts.d}
 grid0 = StructuredGrid:new{psurface=patch0, niv=3, njv=3}
-blk0 = FluidBlock:new{grid=grid0, initialState=initial, label="blk0"}
+blk0 = FluidBlock:new{grid=grid0, initialState=initial}
 setHistoryPoint{ib=0, i=0, j=0}
+--
 -- Finish off config
 config.max_time = 2.0e-4
 config.max_step = 100000
