00817878c88ce2673b9b2a72935d3a8714c71a38
Ingo Jahn <devnull@localhost>
2018-04-27 11:59:29 +1000

    fomaMesh: fixed indents from previous commit.

--- src/geom/grid/usgrid.d
+++ src/geom/grid/usgrid.d
@@ -1770,89 +1770,89 @@ public:
         size_t[] new_vtx_ids;
         // switch between searching entire grid and only vtx located on grid boundaries
         if (false) {
-        // Search all vtx in grid
-        new_vtx_ids.length = other.vertices.length;  
-        foreach (i, vtx; other.vertices) {
-            bool found = false;
-            size_t jsave;
-            foreach (j, v; vertices) {
-                if (approxEqualVectors(v, vtx, relTol, absTol)) {
-                    found = true;
-                    jsave = j; // remember where we found it
-                    break;
+            // Search all vtx in grid
+            new_vtx_ids.length = other.vertices.length;  
+            foreach (i, vtx; other.vertices) {
+                bool found = false;
+                size_t jsave;
+                foreach (j, v; vertices) {
+                    if (approxEqualVectors(v, vtx, relTol, absTol)) {
+                        found = true;
+                        jsave = j; // remember where we found it
+                        break;
+                    }
+                }
+                if (!found) {
+                    // We have a new vertex for the master grid.
+                    vertices ~= vtx;
+                    new_vtx_ids[i] = vertices.length - 1;
+                } else {
+                    // This vertex was already in master grid.
+                    new_vtx_ids[i] = jsave;
                 }
             }
-            if (!found) {
-                // We have a new vertex for the master grid.
-                vertices ~= vtx;
-                new_vtx_ids[i] = vertices.length - 1;
-            } else {
-                // This vertex was already in master grid.
-                new_vtx_ids[i] = jsave;
-            }
-        }
-        nvertices = vertices.length;
+            nvertices = vertices.length;
         } else {
-        // Search only vertices on boundaries
-        // Collect vertices on boundaries that need to be compared.
-        size_t[] vtx_ids_boundary;
-        foreach (i,b; boundaries) {
-            // for OpenFOAM 2-D meshes the top and bottom boundaries (index 4 and 5 and mutiples) can be skipped 
-            if (openFoamDimensions == 2  && ( (i%6)==4  || (i%6)==5) ) { continue; }  
-            foreach (j,f; b.face_id_list) {
-                vtx_ids_boundary ~= faces[f].vtx_id_list;
-            }
-        }
-        // Sort resulting array and remove duplicates
-        vtx_ids_boundary.length -= vtx_ids_boundary.sort().uniq().copy(vtx_ids_boundary).length;
-        size_t[] vtx_ids_other_boundary;
-        foreach (i,b; other.boundaries) {
-            // for OpenFOAM 2-D meshes the top and bottom boundaries (index 4 and 5) can be skipped 
-            if (openFoamDimensions == 2 && (i == 4 || i == 5)) { continue; } 
-            foreach (f; b.face_id_list) {
-                vtx_ids_other_boundary ~= other.faces[f].vtx_id_list;
-            }
-        }
-        // Sort resulting array and remove duplicates
-        vtx_ids_other_boundary.length -= vtx_ids_other_boundary.sort().uniq().copy(vtx_ids_other_boundary).length;
-        //
-        // Compare vertics on boundaries and add them to master grid if new. 
-        new_vtx_ids.length = other.vertices.length;
-        foreach (i; vtx_ids_other_boundary) {
-            bool found = false;
-            size_t jsave;
-            Vector3 vtx;
-            vtx = other.vertices[i];
-            foreach (j; vtx_ids_boundary) {
-                Vector3 v;
-                v = vertices[j];
-                if (approxEqualVectors(v, vtx, relTol, absTol)) {
-                    found = true;
-                    jsave = j; // remember where we found it
-                    break;
+            // Search only vertices on boundaries
+            // Collect vertices on boundaries that need to be compared.
+            size_t[] vtx_ids_boundary;
+            foreach (i,b; boundaries) {
+                // for OpenFOAM 2-D meshes the top and bottom boundaries (index 4 and 5 and mutiples) can be skipped 
+                if (openFoamDimensions == 2  && ( (i%6)==4  || (i%6)==5) ) { continue; }  
+                foreach (j,f; b.face_id_list) {
+                    vtx_ids_boundary ~= faces[f].vtx_id_list;
                 }
             }
-            if (!found) {
-                // We have a new vertex for the master grid.
-                vertices ~= vtx;
-                new_vtx_ids[i] = vertices.length - 1;
-            } else {
-                // This vertex was already in master grid.
-                new_vtx_ids[i] = jsave;
+            // Sort resulting array and remove duplicates
+            vtx_ids_boundary.length -= vtx_ids_boundary.sort().uniq().copy(vtx_ids_boundary).length;
+            size_t[] vtx_ids_other_boundary;
+            foreach (i,b; other.boundaries) {
+                // for OpenFOAM 2-D meshes the top and bottom boundaries (index 4 and 5) can be skipped 
+                if (openFoamDimensions == 2 && (i == 4 || i == 5)) { continue; } 
+                foreach (f; b.face_id_list) {
+                    vtx_ids_other_boundary ~= other.faces[f].vtx_id_list;
+                }
             }
+            // Sort resulting array and remove duplicates
+            vtx_ids_other_boundary.length -= vtx_ids_other_boundary.sort().uniq().copy(vtx_ids_other_boundary).length;
+            //
+            // Compare vertics on boundaries and add them to master grid if new. 
+            new_vtx_ids.length = other.vertices.length;
+            foreach (i; vtx_ids_other_boundary) {
+                bool found = false;
+                size_t jsave;
+                Vector3 vtx;
+                vtx = other.vertices[i];
+                foreach (j; vtx_ids_boundary) {
+                    Vector3 v;
+                    v = vertices[j];
+                    if (approxEqualVectors(v, vtx, relTol, absTol)) {
+                        found = true;
+                        jsave = j; // remember where we found it
+                        break;
+                    }
+                }
+                if (!found) {
+                    // We have a new vertex for the master grid.
+                    vertices ~= vtx;
+                    new_vtx_ids[i] = vertices.length - 1;
+                } else {
+                    // This vertex was already in master grid.
+                    new_vtx_ids[i] = jsave;
+                }
+            }
+            //
+            // Add internal vertices from other to master grid
+            foreach (i, vtx; other.vertices) {
+                if ( !canFind(vtx_ids_other_boundary, i)) { 
+                    // We have a new vertex for the master grid.
+                    vertices ~= vtx;
+                    new_vtx_ids[i]= vertices.length - 1;
+                }
+            }
+            nvertices = vertices.length;
         }
         //
-        // Add internal vertices from other to master grid
-        foreach (i, vtx; other.vertices) {
-            if ( !canFind(vtx_ids_other_boundary, i)) { 
-               // We have a new vertex for the master grid.
-               vertices ~= vtx;
-               new_vtx_ids[i]= vertices.length - 1;
-            }
-        }
-        nvertices = vertices.length;
-        //
-        }
         //
         // Make sure that we have a full dictionary of the faces in the master-grid,
         // in preparation for merging the other collection of faces.
