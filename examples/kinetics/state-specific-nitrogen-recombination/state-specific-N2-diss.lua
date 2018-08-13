52fc43afc8f3e63167c655640ca3aa44c85d0852
pmariotto <pmariotto@pmariotto>
2018-08-13 10:19:37 +1000

    examples/kinetics: Updated state-specific reactor to use 3-species, 2 reactions
    The example now runs with 3 pseudo-species (N(4s), N2(v=0) and N2(v=1))
    and two dissociation reactions. This gives a good comparison to the
    thermal equilibrium finite-rate chemistry calculation. The addition of an
    extra species and reaction gives confidence that the loops are working for
    multiple species and reactions in the implementation.

--- examples/kinetics/state-specific-nitrogen-recombination/state-specific-N2-diss.lua
+++ examples/kinetics/state-specific-nitrogen-recombination/state-specific-N2-diss.lua
@@ -10,7 +10,7 @@ reaction[0]={
 	molecule_idx = 1,
 	atom_idx = 0
 }
---[=[
+
 reaction[1]={
 	equation = 'N2_eX1SIGGPlus_v1 + N_e2p3Minus4S <=> N_e2p3Minus4S + N_e2p3Minus4S + N_e2p3Minus4S',
 	frc={model='Arrhenius', A=3.000000000000e+16, n=-1.600000, C=1.132000000000e+05},
@@ -20,6 +20,6 @@ reaction[1]={
         molecule_idx = 2,
         atom_idx = 0
 }
---]=]
+
 
 
--- examples/kinetics/state-specific-nitrogen-recombination/state-specific-chem-reactor.lua
+++ examples/kinetics/state-specific-nitrogen-recombination/state-specific-chem-reactor.lua
@@ -1,7 +1,7 @@
 -- Authors: Pierre M. and Rowan G.
 -- Date: 2018-08-12
 
-gm = GasModel:new{"pseudo-species-nitrogen-2sp.lua"}
+gm = GasModel:new{"pseudo-species-nitrogen-3sp.lua"}
 
 Q = GasState:new{gm}
 Q.p = 1.0e5 -- Pa
@@ -11,8 +11,9 @@ molef['N2_eX1SIGGPlus_v0'] = 2/3
 molef['N_e2p3Minus4S'] = 1/3
 Q.massf = gm:molef2massf(molef)
 mfN2 = Q.massf['N2_eX1SIGGPlus_v0']
+print("mfN2 = ",mfN2)
 
---[=[
+
 -- Now split mass fraction in N2 amonst the two pseudo-species N2(v=0) and N2(v=1)
 -- according to Boltzmann populations
 eV2J = 1.60219e-19 -- eV2J
@@ -33,8 +34,9 @@ end
 qvib = Qvib_N2X(Q.T)
 for sp,eV in pairs(Evib_eV) do
    Q.massf[sp] = mfN2*math.exp(-eV*eV2J/(k_b*Q.T))/qvib
+   print("Q.massf",sp,Q.massf[sp])
 end
---]=]
+
 
 gm:updateThermoFromPT(Q)
 
@@ -46,16 +48,16 @@ dt = 1.0e-6
 dtSuggest = 1.0e-11
 print("# Start integration")
 f = assert(io.open("state-specific-chem.data", 'w'))
-f:write('# 1:t(s)  2:T(K)  3:p(Pa)  4:mf_N_e2p3Minus4S  5:mf_N2_eX1SIGGPlus_v0\n')
+f:write('# 1:t(s)  2:T(K)  3:p(Pa)  4:mf_N_e2p3Minus4S  5:mf_N2_eX1SIGGPlus_v0+1\n')
 f:write(string.format("%10.3e %10.3f %10.3e %20.12e %20.12e \n",
-                      t, Q.T, Q.p, Q.massf.N_e2p3Minus4S, Q.massf.N2_eX1SIGGPlus_v0))
+                      t, Q.T, Q.p, Q.massf.N_e2p3Minus4S, Q.massf.N2_eX1SIGGPlus_v0+Q.massf.N2_eX1SIGGPlus_v1))
 while t <= tFinal do
    dtSuggest = chemUpdate:updateState(Q, dt)
    t = t + dt
    gm:updateThermoFromRHOE(Q)
    conc = gm:massf2conc(Q)
    f:write(string.format("%10.3e %10.3f %10.3e %20.12e %20.12e \n",
-                         t, Q.T, Q.p, Q.massf.N_e2p3Minus4S, Q.massf.N2_eX1SIGGPlus_v0))
+                         t, Q.T, Q.p, Q.massf.N_e2p3Minus4S, Q.massf.N2_eX1SIGGPlus_v0+Q.massf.N2_eX1SIGGPlus_v1))
 end
 f:close()
 print("# Done.")
