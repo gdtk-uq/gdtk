53b1d3595ff55ebd8697670e3500f0f915b8688f
Peter Jacobs <peterj@mech.uq.edu.au>
2018-09-15 21:14:59 +1000

    Eilmer: make use of the Tvib option in the Giordano cylinder example.

--- examples/eilmer/2D/cylinder-giordano/vibrationally-specific/post.sh
+++ examples/eilmer/2D/cylinder-giordano/vibrationally-specific/post.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-e4shared --job=inf_cyl --post --slice-list="0,:,0,0" --output-file=stag-prof-50Pa-vib-specific.data
+e4shared --job=inf_cyl --post --slice-list="0,:,0,0" \
+         --add-vars="Tvib" \
+         --output-file=stag-prof-50Pa-vib-specific.data
 gnuplot plot-prof.gplot
