3c7b0ccaa914a3561c8e25a60c8d29068905c9f2
Peter Jacobs <peterj@mech.uq.edu.au>
2019-02-11 18:17:56 +1000

    Gas: Rework Daniel and Rory's argon gas model to be @nogc.
    This rework allows the model to be used in an optimised build of the code.
    To do this, the use of Matrix class is avoided and fixed-length arrays are
    used instead.  This uglifies the code a bit, particularly when explicit loops
    have to be used to be compatible with the complex numbers build.
    
    Also, note that this commit has broken the Backward_Euler integration.
    To end on a positive note, note that the RK4 and Forward_Euler integrations
    do pass the unit test.

--- src/kinetics/sample-input/two-temperature-reacting-argon-model.lua
+++ src/kinetics/sample-input/two-temperature-reacting-argon-model.lua
@@ -4,6 +4,8 @@ TwoTemperatureReactingArgon = {
   ion_tol = 1.0e-15,
   chem_dt = 1.0e-11,
   integration_method = "Backward_Euler",
+  -- integration_method = "RK4",
+  -- integration_method = "Forward_Euler",
   Newton_Raphson_tol = 1.0e-10
 }
 
--- src/kinetics/two_temperature_argon_kinetics.d
+++ src/kinetics/two_temperature_argon_kinetics.d
@@ -8,8 +8,9 @@
  * The Physics of Fluids 10, 1769 (1967); doi 10.1063/1.1762356
  *
  *
- * Authors: Daniel Smith and Rory Kelly
+ * Authors: Daniel Smith, Rory Kelly and Peter J.
  * Version: 19-July-2017: initial cut.
+ *          11-Feb-2019: rebuild to remove use of Matrix class
  */
 
 module kinetics.two_temperature_argon_kinetics;
@@ -43,24 +44,24 @@ final class UpdateArgonFrac : ThermochemicalReactor {
         _Newton_Raphson_tol = getDouble(L, -1, "Newton_Raphson_tol");
         lua_close(L);
     }
-    
-    Matrix!number F(Matrix!number y, GasState Q)
+
+    @nogc
+    number[2] F(number[2] y, GasState Q)
     {
-        auto y_dash = new Matrix!number([[0.0],
-                                         [0.0]]);
+        number[2] y_dash; y_dash[0] = 0.0; y_dash[1] = 0.0;
         number Q_ea;
         number Q_ei;
 
         number v_ea;
         number v_ei;
 
-        //unpack dependant variables
-        number n_e = y[0,0];
-        number n_Ar = _n_total-n_e;
-        Q.u = y[1,0];
-        Q.u_modes[0] = _u_total-Q.u;
+        // Unpack dependant variables
+        number n_e = y[0];
+        number n_Ar = _n_total - n_e;
+        Q.u = y[1];
+        Q.u_modes[0] = _u_total - Q.u;
 
-        //Convenient variables
+        // Convenient variables
         number alpha = n_e/(_n_total);
         number Te;
         number T = Q.T;
@@ -69,14 +70,15 @@ final class UpdateArgonFrac : ThermochemicalReactor {
         if (n_e <= 0.0) {
             // Do not let the number of electrons go negative.
             // Force the numbers back to something physically realizable.
-            Q.u = _u_total;
-            Q.u_modes[0] = 0.0;
-            //mass
+            // mass
             n_Ar = _n_total;
             n_e = 0.0;
-            //energy
-            //temperature
-            Q.T =  2.0/3.0*Q.u/_Rgas; T = Q.T;
+            // energy
+            Q.u = _u_total;
+            Q.u_modes[0] = 0.0;
+            // temperature
+            Q.T =  2.0/3.0*Q.u/_Rgas;
+            T = Q.T;
             Q.T_modes[0] = Q.T;
         } else if (alpha < _ion_tol) {
             Te = Q.T;
@@ -107,77 +109,95 @@ final class UpdateArgonFrac : ThermochemicalReactor {
         number n_dot_e = kfe*n_Ar*n_e - kre*pow(n_e,3);
         number n_dot = n_dot_A + n_dot_e;
 
-        y_dash[0,0] = n_dot;
+        y_dash[0] = n_dot;
 
         // Energy moving to electronic mode due to reactions:
         number alpha_dot = n_dot/_n_total;
-        number u_dot_reac = 3.0/2.0*_Rgas*alpha_dot*Te+alpha_dot*_Rgas*_theta_ion - n_dot_e*_Kb*_theta_ion/Q.rho;
+        number u_dot_reac = 3.0/2.0*_Rgas*alpha_dot*Te
+            + alpha_dot*_Rgas*_theta_ion - n_dot_e*_Kb*_theta_ion/Q.rho;
 
         //=====================================================================
-        //now need temperatures...
+        // now need temperatures...
         if (alpha > _ion_tol) {
-
-            //calculate Qea
-            if (Te < 1.0e4) {
-                    Q_ea = (0.39 - 0.551e-4*Te + 0.595e-8*pow(Te,2))*1.0e-20;
-            } else if (Te<1.0e5) {
-                    Q_ea = (-0.35 + 0.775e-4*Te)*1.0e-20;
+            // Calculate Qea
+            if (Te < 10.0e3) {
+                Q_ea = 0.39 + Te*(-0.551e-4 + 0.595e-8*Te);
+            } else if (Te < 500.0e3) {
+                Q_ea = -0.35 + 0.775e-4*Te;
             } else {
-                    Q_ea = (-0.35 + 0.775e-4*50000)*1.0e-20;
+                Q_ea = -0.35 + 0.775e-4*500.0e3;
             }
-
-            //Calculate Q_ei
+            Q_ea *= 1.0e-20;
+            //
+            // Calculate Q_ei
             Q_ei = 1.95e-10*pow(Te,-2)*log(1.53e8*pow(Te,3)/(n_e/1.0e6));
-            if (Q_ei < 0.0) {Q_ei = 0.0;}
-
-            //find the collision frequencies
-            v_ea = (1.0-alpha)*Q.rho/_m_Ar*sqrt(8*_Kb*Te/to!number(PI)/_m_e)*Q_ea; //electron-Ar collisions
-            v_ei = alpha*Q.rho/_m_Ar*sqrt(8*_Kb*Te/to!number(PI)/_m_e)*Q_ei; //electron-Ar+ collisions
-
+            if (Q_ei < 0.0) { Q_ei = 0.0; }
+            //
+            // Find the collision frequencies
+            // electron-Ar collisions
+            v_ea = (1.0-alpha)*Q.rho/_m_Ar*sqrt(8*_Kb*Te/to!number(PI)/_m_e)*Q_ea;
+            // electron-Ar+ collisions
+            v_ei = alpha*Q.rho/_m_Ar*sqrt(8*_Kb*Te/to!number(PI)/_m_e)*Q_ei;
+            //
             alpha_dot = n_dot/(n_e+n_Ar);
-            number u_dot = 3*n_e*_m_e/_m_Ar*(v_ea+v_ei)*_Kb*(T-Te)/Q.rho;// energy transferred to electron mode through collisions
-            y_dash[1,0] = - u_dot - u_dot_reac;
+            // energy transferred to electron mode through collisions
+            number u_dot = 3*n_e*_m_e/_m_Ar*(v_ea+v_ei)*_Kb*(T-Te)/Q.rho;
+            y_dash[1] = - u_dot - u_dot_reac;
         } else {
-            y_dash[1,0] = 0.0 - u_dot_reac;            
+            y_dash[1] = 0.0 - u_dot_reac;            
         }
         return y_dash;
-    }
+    } // end F()
 
-    Matrix!number BackEuler_F(Matrix!number y, Matrix!number y_prev, GasState Q)
+    @nogc
+    number[2] BackEuler_F(number[2] y, number[2] y_prev, GasState Q)
     {
-        auto output = y_prev - y + _chem_dt*F(y, Q);
+        number[2] myF = F(y, Q);
+        number[2] output;
+        foreach (i; 0 .. 2) { output[i] = y_prev[i] - y[i] + _chem_dt*myF[i]; }
         return output;
     }
 
-    Matrix!number Jacobian(Matrix!number y, Matrix!number y_prev, Matrix!number h, GasState Q){
-
-        auto J = new Matrix!number([[0.0,0.0],
-                                    [0.0,0.0]]);
-
-        auto yph0 = new Matrix!number([[y[0,0] + h[0,0]],[y[1,0]]]); //y plus increment in index 0
-        auto col1 = (BackEuler_F(yph0,y_prev, Q) - BackEuler_F(y, y_prev, Q))/h[0,0];
-        auto yph1 = new Matrix!number([[y[0,0]],[y[1,0] + h[1,0]]]); //y plus increment in index 1
-        auto col2 = (BackEuler_F(yph1,y_prev, Q) - BackEuler_F(y, y_prev, Q))/h[1,0];
-
-        J[0,0] = col1[0,0]; J[1,0] = col1[1,0]; J[0,1] = col2[0,0]; J[1,1] = col2[1,0];
-
+    @nogc
+    number[2][2] Jacobian(number[2] y, number[2] y_prev, double[2] h, GasState Q)
+    {
+        number[2] myF0 = BackEuler_F(y, y_prev, Q);
+        number[2] yph0; yph0[0] = y[0]+h[0]; yph0[1] = y[1]; // y plus increment in index 0
+        number[2] myF1 = BackEuler_F(yph0, y_prev, Q);
+        number[2] col1; foreach (i; 0 .. 2) { col1[i] = (myF1[i] - myF0[i])/h[0]; }
+        number[2] yph1; yph1[0] = y[0]; yph1[1] = y[1]+h[1]; // y plus increment in index 1
+        number[2] myF2 = BackEuler_F(yph1, y_prev, Q);
+        number[2] col2; foreach (i; 0 .. 2) { col2[i] = (myF2[i] - myF0[i])/h[1]; }
+        number[2][2] J;
+        J[0][0] = col1[0]; J[0][1] = col2[0];
+        J[1][0] = col1[1]; J[1][1] = col2[1];
         return J;
     }
 
+    @nogc
+    void solve2(number[2][2] a, number[2] b, number[2] x)
+    {
+        number det = a[0][0]*a[1][1] - a[0][1]*a[1][0];
+        if (fabs(det) < 1.0e-20) {
+            throw new Error("Effectively singular Jacobian.");
+        }
+        x[0] = (a[0][1]*b[1] - a[1][1]*b[0])/det;
+        x[1] = (a[0][0]*b[1] - a[1][0]*b[0])/det;
+    }
+    
     @nogc
     override void opCall(GasState Q, double tInterval,
                          ref double dtChemSuggest, ref double dtThermSuggest, 
                          ref number[maxParams] params)
     {
-        debug {
         // Implement in debug context because of @nogc requirement.
         //
         if (Q.T > 3000.0) {
-            double chem_dt_start = _chem_dt;
-            int NumberSteps = to!int(tInterval/_chem_dt);
-            if (NumberSteps < 1) {NumberSteps = 1;}
+            double chem_dt_saved = _chem_dt;
+            int NumberSteps = cast(int) floor(tInterval/_chem_dt);
+            if (NumberSteps < 1) { NumberSteps = 1; }
             _chem_dt = tInterval/NumberSteps;
-            //writeln("Number of Steps ", NumberSteps);
+            //
             // Determine the current number densities.
             number[3] numden;
             _gmodel.massf2numden(Q, numden);
@@ -186,71 +206,72 @@ final class UpdateArgonFrac : ThermochemicalReactor {
             _n_total = n_e + n_Ar;
             number alpha = n_e/_n_total;
             _u_total = 3.0/2.0*_Rgas*(Q.T+alpha*Q.T_modes[0])+alpha*_Rgas*_theta_ion;
-            //writeln("_u_total = ", _u_total);
 
-            auto y = new Matrix!number([[n_e],
-                                        [Q.u]]);
-            auto y_prev = y;
-            number norm_error = 1.0e50;
-            number prev_norm_error;
-            
-            //Initialise variables for RK4
-            auto k1 = zeros!number(2,1);
-            auto k2_in = zeros!number(2,1);
-            auto k2 = zeros!number(2,1);
-            auto k3_in = zeros!number(2,1);
-            auto k3 = zeros!number(2,1);
-            auto k4_in = zeros!number(2,1);
-            auto k4 = zeros!number(2,1);
-            auto h = new Matrix!number([[1.0e10],[1.0e-5]]); //working: auto h = new Matrix!number([[1.0e10],[1.0e0]]); 
+            // Definition of our state vector:
+            // [0] number density of electrons and
+            // [1] translational energy of heavy particles
+            number[2] y; y[0] = n_e; y[1] = Q.u;
+
+            // The following needed for Backward-Euler step
+            number[2] y_prev; y_prev[0] = n_e; y_prev[1] = Q.u;
+            // Perturbation sizes for the finite-difference Jacobian
+            double[2] h = [1.0e10, 1.0e-5]; // working: [1.0e10, 1.0e0]; 
 
             foreach (n; 1 .. NumberSteps) {
-                //writeln("y = ", y);
-                if (_integration_method == "Forward_Euler") {
-                    y = y + _chem_dt*F(y, Q);
-                } else if (_integration_method == "Backward_Euler"){
-                    if (y[0,0] < 0.0) {
-                        y = y + _chem_dt*F(y, Q);
+                switch (_integration_method) {
+                case "Forward_Euler":
+                    number[2] myF = F(y, Q);
+                    foreach (i; 0 .. 2) { y[i] = y[i] + _chem_dt * myF[i]; }
+                    break;
+                case "Backward_Euler":
+                    if (y[0] <= 0.0) {
+                        // There are no electrons, fall back to a forward-Euler step.
+                        number[2] myF = F(y, Q);
+                        foreach (i; 0 .. 2) { y[i] = y[i] + _chem_dt * myF[i]; }
                     } else {
+                        // Some electrons already.
+                        double norm_error = 1.0e50; // something large that will be replaced
                         do {
-                            //writeln(" ");
-                            //prev_norm_error = norm_error;
-                            auto J = Jacobian(y,y_prev,h,Q); 
-                            auto J_inv = inverse!number(J);
-                            y = y - dot!number(J_inv,BackEuler_F(y,y_prev,Q));
-                            auto error = BackEuler_F(y,y_prev,Q);
-                            //writeln("error = ", error);
-                            norm_error = sqrt(pow(error[0,0]/1.0e22,2) + pow(error[1,0]/1.0e7,2));
-                            //writeln("norm_error = ", norm_error);
-                        } while (norm_error > _Newton_Raphson_tol);//while (norm_error < prev_norm_error); // 
+                            number[2][2] J = Jacobian(y, y_prev, h, Q); 
+                            number[2] dy;
+                            number[2] rhs = BackEuler_F(y, y_prev, Q);
+                            solve2(J, rhs, dy);
+                            foreach (i; 0 .. 2) { y[i] = y[i] - dy[i]; }
+                            number[2] error = BackEuler_F(y, y_prev, Q);
+                            norm_error = sqrt(pow(error[0].re/1.0e22,2) +
+                                              pow(error[1].re/1.0e7,2));
+                        } while (norm_error > _Newton_Raphson_tol);
                     }
+                    foreach (i; 0 .. 2) { y_prev[i] = y[i]; } // needed for next step
+                    break;
+                case "RK4":
+                    number[2] k1, k2_in, k2, k3_in, k3, k4_in, k4;
+                    number[2] myF = F(y, Q);
+                    foreach (i; 0 .. 2) { k1[i] = _chem_dt * myF[i]; }
+                    foreach (i; 0 .. 2) { k2_in[i] = y[i] + k1[i]/2.0; }
+                    myF = F(k2_in, Q);
+                    foreach (i; 0 .. 2) { k2[i] = _chem_dt * myF[i]; }
+                    foreach (i; 0 .. 2) { k3_in[i] = y[i] + k2[i]/2.0; }
+                    myF = F(k3_in, Q);
+                    foreach (i; 0 .. 2) { k3[i] = _chem_dt * myF[i]; }
+                    foreach (i; 0 .. 2) { k4_in[i] = y[i] + k3[i]; }
+                    myF = F(k4_in, Q);
+                    foreach (i; 0 .. 2) { k4[i] = _chem_dt * myF[i]; }
+                    foreach (i; 0 .. 2) { y[i] = y[i] + 1.0/6.0*(k1[i]+2.0*k2[i]+2.0*k3[i]+k4[i]); }
+                    break;
+                default:
+                    throw new Error("Invalid ODE update selection.");
+                } // end switch
+            } // end foreach n
 
-                    y_prev = y;
-                } else if (_integration_method == "RK4"){
-                    k1 = _chem_dt*F(y, Q);
-
-                    k2_in = y + k1/2.0;
-                    k2 = _chem_dt*F(k2_in, Q);
-
-                    k3_in = y+k2/2.0;
-                    k3 = _chem_dt*F(k3_in, Q);
-
-                    k4_in = y+k3;
-                    k4 = _chem_dt*F(k4_in, Q);
-
-                    y = y + 1.0/6.0*(k1+2.0*k2+2.0*k3+k4);
-                }
-            }
-
-            //Unpack the results
-            n_e = y[0,0];
-            Q.u = y[1,0];
-
-            //reconstruct the flow state
-            n_Ar = _n_total - n_e;
-            Q.u_modes[0] = _u_total-Q.u;    
-
-            if (n_e <= 0.0) {
+            // Unpack the results.
+            n_e = y[0];
+            Q.u = y[1];
+            // Reconstruct the other parts of the flow state.
+            if (n_e > 0.0) {
+                n_Ar = _n_total - n_e;
+                Q.u_modes[0] = _u_total - Q.u;    
+            } else {
                 // Do not let the number of electrons go negative.
                 // Force the numbers back to something physically realizable.
                 Q.u = _u_total;
@@ -272,15 +293,11 @@ final class UpdateArgonFrac : ThermochemicalReactor {
             _gmodel.update_thermo_from_rhou(Q);
             _gmodel.update_sound_speed(Q);
 
-            _chem_dt = chem_dt_start; // return _chem_dt back to its original value
-        }
-        } else {
-            // We are not in a debug build context, so...
-            throw new Error("2T argon only available in debug build.");
-        }
+            _chem_dt = chem_dt_saved; // restore _chem_dt value
+        } // end if Q.T > 3000
     } // end opCall()
     
-private:
+    private:
     double _m_Ar = 6.6335209e-26; // mass of argon (kg)
     double _m_e = 9.10938e-31; // mass of electron (kg)
     double _Kb = Boltzmann_constant;
@@ -291,11 +308,11 @@ private:
     double _chem_dt;
     string _integration_method;
     double _Newton_Raphson_tol;
-public:
+    public:
     number _n_total;
     number _u_total;
 
-} // end class UpdateArgonFrac
+    } // end class UpdateArgonFrac
 
 
 version(two_temperature_argon_kinetics_test) {
