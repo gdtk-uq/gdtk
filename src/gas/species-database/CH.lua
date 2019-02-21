39d831150933413aae4f67f5597e486acfd93c18
Rowan J. Gollan <r.gollan@uq.edu.au>
2019-02-22 09:42:10 +1000

    gas: Added cea thermo coefficients for methylidine (CH).

--- src/gas/species-database/CH.lua
+++ src/gas/species-database/CH.lua
@@ -25,6 +25,54 @@ db.CH.epsilon = {
    description = 'Lennard-Jones potential well depth.',
    reference = 'GRI-Mech 3.0 transport file.'
 }
+db.CH.ceaThermoCoeffs = {
+   nsegments = 3,
+   segment0 = {
+      T_lower = 200.0,
+      T_upper = 1000.0,
+      coeffs = {
+         2.220590133e+04,
+        -3.405411530e+02,
+         5.531452290e+00,
+        -5.794964260e-03,
+         7.969554880e-06,
+        -4.465911590e-09,
+         9.596338320e-13,
+         7.240783270e+04,
+        -9.107673050e+00
+      }
+   },
+   segment1 = {
+      T_lower = 1000.0,
+      T_upper = 6000.0,
+      coeffs = {
+         2.060763440e+06,
+        -5.396206660e+03,
+         7.856293850e+00,
+        -7.965907450e-04,
+         1.764308305e-07,
+        -1.976386267e-11,
+         5.030429510e-16,
+         1.062236592e+05,
+        -3.154757439e+01
+      }
+   },
+   segment2 = {
+      T_lower = 6000.0,
+      T_upper = 20000.0,
+      coeffs = {
+        -8.068368690e+08,
+         4.575450540e+05,
+        -9.843975080e+01,
+         1.235244098e-02,
+        -8.485608570e-07,
+         3.040410624e-11,
+        -4.400315170e-16,
+        -3.595851590e+06,
+         8.953477440e+02
+      }
+   }
+}
 db.CH.grimechThermoCoeffs = {
    notes = 'data from GRIMECH 3.0',
    nsegments = 2, 
