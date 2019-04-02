e1789bef6e5ebf6d94e1d736c7e030e3ae228985
Rowan J. Gollan <r.gollan@uq.edu.au>
2019-04-02 23:27:20 +1000

    geom: Added function to find a Bezier triangle of best fit to a point cloud.

--- src/geom/surface/beziertrianglepatch.d
+++ src/geom/surface/beziertrianglepatch.d
@@ -38,10 +38,19 @@ import std.stdio : File;
 import std.conv : text;
 import std.range : iota;
 import std.string : format;
+import std.algorithm.comparison : max;
 
 import nm.nelmin;
 
 import geom;
+
+@nogc
+size_t toSingleIndex()(size_t i, size_t j, size_t k)
+{
+    auto n = i + j + k;
+    auto m = n - i;
+    return (m*(m+5)/2) - 2*j - k;
+}
 
 class BezierTrianglePatch {
 public:
@@ -76,15 +85,10 @@ public:
         _b[0][] = B[];
     }
 
-    @nogc size_t toSingleIndex()(size_t i, size_t j, size_t k) const
-    {
-        auto n = i + j + k;
-        auto m = n - i;
-        return (m*(m+5)/2) - 2*j - k;
-    }
-
     Vector3 opCall(double u, double v)
     {
+        // Ensure _b[0] is up-to-date in case caller has changed.
+        _b[0][] = B[];
         auto w = 1.0 - u - v;
         // We may allow u or v to get slightly larger than 1.0
         // if we are perturbing to get sensitivity.
@@ -114,12 +118,14 @@ public:
         return _b[n][0];
     }
 
-    double projectPoint(Vector3 p, ref Vector3 q, ref double uFound, ref double vFound)
+    double projectPoint(Vector3 p, ref Vector3 q, ref double uFound, ref double vFound, bool returnOnFailure=false)
     {
         // Establish an initial guess for u and v
-        double[] samples = [0.0, 0.2, 0.4, 0.8, 1.0];
+        double[] samples = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5,
+                            0.6, 0.7, 0.8, 0.9, 1.0];
         q = opCall(0.0, 0.0);
         double minDist = distance_between(p, q);
+        double maxDist = minDist;
         double min_u = 0.0;
         double min_v = 0.0;
         foreach (u; samples) {
@@ -132,20 +138,25 @@ public:
                     min_u = u;
                     min_v = v;
                 }
+                maxDist = max(maxDist, dist);
             }
         }
 
         // Define function to minimize
-        double penalty = 1.0e6 * minDist;
+        double penalty = 1000 * maxDist;
         double fMin(double[] x)
         {
             double u = x[0];
             double v = x[1];
             // Check that u and v are feasible.
             // Apply penalty if not.
-            if (u < 0.0) return penalty;
-            if (v < 0.0) return penalty;
-            if (u + v > 1.0) return penalty;
+            // We let the values of u and v wander off the
+            // edge of the patch a little. This produces
+            // a better behaved approach to the answer
+            // than applying a harsh penalty right at the edge.
+            if (u < -0.1) return penalty;
+            if (v < -0.1) return penalty;
+            if (u + v > 1.1) return penalty;
             // Now proceed to evaluate distance.
             q = opCall(u, v);
             return distance_between(p, q);
@@ -157,13 +168,13 @@ public:
         // Call optimizer
         double f_min;
         int n_fe, n_restart;
-        double[] dx = [0.001, 0.001];
-        double tol = 1.0e-9;
-        int max_steps = 1000;
+        double[] dx = [0.01, 0.01];
+        double tol = 1.0e-8;
+        int max_steps = 10000;
 
         bool success = nm.nelmin.minimize!(fMin, double)(x, f_min, n_fe, n_restart, dx, tol, max_steps);
 
-        if (success) {
+        if (success || returnOnFailure) {
             uFound = x[0];
             vFound = x[1];
             q = opCall(uFound, vFound);
@@ -231,6 +242,138 @@ void writeBezierTriangleAsVtkXml(BezierTrianglePatch btp, string fileName, int n
     f.close();
 }
 
+BezierTrianglePatch bezierTriangleFromPointCloud(Vector3[] points, Vector3 p0, Vector3 p1, Vector3 p2, int n, BezierTrianglePatch initGuess)
+{
+    // NOTE: This function has no constraint at the edges. It's quite possible to find a patch that fits the point
+    //       cloud well, but the patch overlaps at the edges. It's not usually a big deal, just something to be
+    //       aware of.
+    double tol = 1.0e-3;
+    int maxSteps = 10000;
+    // Corners are located at
+    //
+    //            p1
+    //             + b(0,n,0)
+    //            / \
+    //           /   \
+    //       p2 +_____+ p0
+    //   b(0,0,n)        b(n,0,0)
+    //  
+
+    auto nCtrlPts = (n+1)*(n+2)/2;
+    Vector3[] ctrlPts;
+    ctrlPts.length = nCtrlPts;
+    double[] d;
+
+    // -----------------------------------------------------------------
+    // Establish an initial guess for the location of control points
+    // -----------------------------------------------------------------
+    // We know the corner points. Put them in the list.
+    // These points are not to be touched by the optimiser.
+    ctrlPts[toSingleIndex!()(n,0,0)] = p0;
+    ctrlPts[toSingleIndex!()(0,n,0)] = p1;
+    ctrlPts[toSingleIndex!()(0,0,n)] = p2;
+    // The user might have supplied an initial guess.
+    // We can use this if the degree is correct.
+    if (initGuess !is null && initGuess.n == n) {
+        foreach (i; iota(n, -1, -1)) {
+            foreach (j; iota(n-i, -1, -1)) {
+                auto k = n - (i+j);
+                // Skip corner points
+                if (i == n || j == n || k == n) continue;
+                auto idx = toSingleIndex!()(i,j,k);
+                ctrlPts[idx] = initGuess.B[idx];
+                d ~= ctrlPts[idx].x;
+                d ~= ctrlPts[idx].y;
+                d ~= ctrlPts[idx].z;
+            }
+        }
+    }
+    else {
+        // Distribute the remaining control points roughly equally using barycentric coordinates
+        double du = 1.0/n;
+        double dv = du;
+        foreach (i; iota(n, -1, -1)) {
+            foreach (j; iota(n-i, -1, -1)) {
+                auto k = n - (i+j);
+                // Skip corner points
+                if (i == n || j == n || k == n) continue;
+                double u = i*du;
+                double v = j*dv;
+                double w = 1.0 - u - v;
+                auto idx = toSingleIndex!()(i,j,k);
+                ctrlPts[idx] = u*p0 + v*p1 + w*p2;
+                d ~= ctrlPts[idx].x;
+                d ~= ctrlPts[idx].y;
+                d ~= ctrlPts[idx].z;
+            }
+        }
+    }
+    auto myBezTriPatch = new BezierTrianglePatch(ctrlPts, n);
+    // -------------- Done: establishing starting guess ------------------------
+
+    // --------------------------------------------------------------
+    // Build cost function to be minimized.
+    // --------------------------------------------------------------
+    double fMin(double[] d)
+    {
+        // Adjust control points based on supplied design values 'd'.
+        size_t pos = 0;
+        foreach (i; iota(n, -1, -1)) {
+            foreach (j; iota(n-i, -1, -1)) {
+                auto k = n - (i+j);
+                // Skip corner points
+                if (i == n || j == n || k == n) continue;
+                auto idx = toSingleIndex!()(i,j,k);
+                myBezTriPatch.B[idx].refx = d[pos]; pos++;
+                myBezTriPatch.B[idx].refy = d[pos]; pos++;
+                myBezTriPatch.B[idx].refz = d[pos]; pos++;
+            }
+        }
+        // Evaluate error between point cloud and triangle batch
+        double err = 0.0;
+        Vector3 q;
+        double uFound, vFound;
+        foreach (p; points) {
+            err += myBezTriPatch.projectPoint(p, q, uFound, vFound, true);
+        }
+        return err;
+    }
+    // ----------- Done: building cost function ----------------------------
+
+    // ---------------------------------------------------------------------
+    // Optimize the placement of control points using Nelder-Mead minimiser
+    // ---------------------------------------------------------------------
+    double f_min;
+    int n_fe, n_restart;
+    double[] dx;
+    dx.length = d.length;
+    // Make the initial perturbations of control points 1/100th of the longest side length
+    double dp = 0.01*max(distance_between(p0, p1), distance_between(p0, p2), distance_between(p1, p2));
+    dx[] = dp;
+    bool success = nm.nelmin.minimize!(fMin, double)(d, f_min, n_fe, n_restart, dx, tol, maxSteps);
+    if (success) {
+        size_t pos = 0;
+        foreach (i; iota(n, -1, -1)) {
+            foreach (j; iota(n-i, -1, -1)) {
+                auto k = n - (i+j);
+                // Skip corner points
+                if (i == n || j == n || k == n) continue;
+                auto idx = toSingleIndex!()(i,j,k);
+                myBezTriPatch.B[idx].refx = d[pos]; pos++;
+                myBezTriPatch.B[idx].refy = d[pos]; pos++;
+                myBezTriPatch.B[idx].refz = d[pos]; pos++;
+            }
+        }
+        return myBezTriPatch;
+    }
+    // Otherwise, we failed in the optimizer.
+    string errMsg = "Error in bezierTriangleFromPointCloud().\n";
+    errMsg ~= "Optimizer stats:\n";
+    errMsg ~= format("  number of function evaluations: %d\n", n_fe);
+    errMsg ~= format("  number of restarts: %d\n", n_restart);
+    errMsg ~= format("  minimum of function found: %12.6e\n", f_min);
+    throw new Exception(errMsg);
+}
 
 
 version(beziertrianglepatch_test) {
@@ -253,10 +396,36 @@ version(beziertrianglepatch_test) {
         // Test projection of point that is ON surface
         Vector3 q;
         auto dist = btp.projectPoint(p, q, u, v);
-        assert(dist < 1.0e-9, failedUnitTest());
+        assert(dist < 1.0e-6, failedUnitTest());
         assert(approxEqualVectors(q, Vector3(2, 2, 7./3)), failedUnitTest());
         assert(approxEqual(u, 1./3, 1.0e-6), failedUnitTest());
         assert(approxEqual(v, 1./3, 1.0e-6), failedUnitTest());
+
+        // Test optimizer with this surface.
+        // 1. Generate a bunch of points
+        Vector3[] points;
+        double du = 1.0/4;
+        double dv = du;
+        foreach (uu; iota(0.0, 1.0+0.5*du, du)) {
+            foreach (vv; iota(0.0, (1.0-uu)+0.5*dv, dv)) {
+                if ((uu + vv) > 1.0) continue;
+                points ~= btp(uu, vv);
+            }
+        }
+        // 2. Find Bezier triangle to fit this point cloud.
+        auto p0 = Vector3(6, 0, 9);
+        auto p1 = Vector3(0, 6, 0);
+        auto p2 = Vector3(0, 0, 0);
+        // Let's perturb the points a little as an initial guess.
+        B[1].refx = 3.1; B[1].refy = 2.9; B[1].refz = 6.2;
+        B[2].refx = 2.8; B[2].refy = 0.15; B[2].refz = -0.05;
+        B[4].refx = -0.3; B[4].refy = 2.85; B[4].refz = -0.1;
+        auto guess = new BezierTrianglePatch(B, 2);
+        auto testPatch = bezierTriangleFromPointCloud(points, p0, p1, p2, 2, guess);
+
+        p = testPatch(0.2, 0.3);
+        q = btp(0.2, 0.3);
+        assert(distance_between(p, q) < 0.1);
 
         return 0;
     }
