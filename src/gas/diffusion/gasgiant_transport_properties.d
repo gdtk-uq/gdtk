bfe2fe0ada1f0e130694d713dcf91f5a104a647f
Rowan J. Gollan <r.gollan@uq.edu.au>
2019-04-17 22:03:03 +1000

    gas: Added H2 and H self-viscosity evalulation to gasgiant_transport_properties.

--- src/gas/diffusion/gasgiant_transport_properties.d
+++ src/gas/diffusion/gasgiant_transport_properties.d
@@ -17,16 +17,29 @@ import gas.physical_constants;
 import gas.diffusion.viscosity;
 import gas.diffusion.therm_cond;
 
-enum Species {He=0}
+enum Species {He=0, H2, H }	
+
+
+// Keep order consistent with above.
+
 
 class GasGiantViscosity : Viscosity {
     this () {
-        _a_22.length = 1;
-        _a_22[0].length = 1;
-        _a_22[0][0].length = 7;
-        foreach (i, ref e; _a_22[0][0]) {
-            e = a_22["He:He"][i];
+        _a_22.length = 3;
+	foreach (ref e; _a_22) {
+	    e.length = 3;
+	    foreach (ref ee; e) {
+               ee.length = 7;
+	    }
+	}
+	foreach (i; 0 .. _a_22.length) {
+            string key = speciesNames[i] ~ ":" ~ speciesNames[i];
+            foreach (j, ref e; _a_22[i][i]) {
+                e = a_22[key][j];
+            }
         }
+
+
     }
     override GasGiantViscosity dup() const {
         return new GasGiantViscosity();
@@ -43,8 +56,10 @@ private:
     @nogc
     number selfViscosity(number T, int isp, int jsp)
     {
+        // FIX ME: Igor, check on this when isp != jsp.
         number sig2Omega_22 = collisionIntegral_22(T, isp, jsp);
         number mu = 2.6693e-6*PI*sqrt(M[isp]*T)/(PI*sig2Omega_22);
+        
         return mu;
     }
 
@@ -69,7 +84,6 @@ private:
         return sig2Omega_22;
     }
 
-
 }
 
 /*
@@ -85,17 +99,19 @@ version(gasgiant_transport_properties_test) {
     import std.stdio;
     int main()
     {
-        auto ggv = new GasGiantViscosity();
+        GasGiantViscosity ggv = new GasGiantViscosity();
         auto Q = new GasState(1, 1);
-        auto f = File("He-visc-test.dat", "w");
-        f.writeln("# T(K)   mu");
+        auto f = File("gasgiant-visc-test.dat", "w");
+        f.writeln("# T(K)   mu-He  mu-H2  mu-H");
         double T = 300.0;
         double dT = 100.0;
         double Tmax = 10000.0;
         while (T <= Tmax) {
             Q.T = T;
-            auto mu = ggv.eval(Q);
-            f.writefln("%f %.8e", T, mu);
+            auto mu0 = ggv.selfViscosity(Q.T, 0, 0);
+            auto mu1 = ggv.selfViscosity(Q.T, 1, 1);
+            auto mu2 = ggv.selfViscosity(Q.T, 2, 2);
+            f.writefln("%f %.8e %.8e %.8e", T, mu0, mu1, mu2);
             T += dT;
         }
         f.close();
@@ -103,17 +119,30 @@ version(gasgiant_transport_properties_test) {
     }
 }
 
-static double[1] M;
-static double[1] m;
+static double[3] M;
+static double[3] m;
 static double[7][string] a_22;
-
+static string[] speciesNames;
 static this()
 {
+    speciesNames ~= "He";
+    speciesNames ~= "H2";
+    speciesNames ~= "H";
+
     M[Species.He] = 4.002602; 
-    m[Species.He] = M[Species.He]/Avogadro_number;
+    m[Species.He] = (M[Species.He]/Avogadro_number)*10e-3;
 
     a_22["He:He"] = [15.9887272, -4.3159308, 8.0, -15.52249873, 14.35288253, 4.0, 4.80040319];
     
+    M[Species.H2] = 2.015882;
+    m[Species.H2] = (M[Species.H2]/Avogadro_number)*10e-3;
+
+    a_22["H2:H2"] = [27.54387526,-1.98253166,3.88885724,-12.91940775,0.3470796,8.72131306,-0.88296275];
+
+    M[Species.H] = 2.015882/2;
+    m[Species.H] = (M[Species.H2]/Avogadro_number)*10e-3;
+
+    a_22["H:H"] = [22.08948804,-1.85066626,8.50932055,-7.66943974,0.77454531,9.69545318,-0.62104466];
 }
 
 
