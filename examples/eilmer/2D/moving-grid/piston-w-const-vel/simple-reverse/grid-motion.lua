903701b151b26fc6539448d3f6d39d48124a05f5
Peter Jacobs <peterj@mech.uq.edu.au>
2019-05-22 22:09:50 +1000

    Eilmer: added shock-processing to the one-sided flux calculator functions.
    It didn't seem to make much difference to the piston-with-constant-velocity
    example but debug statements did show the shock-processing being active in
    the early steps of the simulation.
    
    Also, added a reverse-moving piston example to exercise the other one-sided flux
    calculator.

---
+++ examples/eilmer/2D/moving-grid/piston-w-const-vel/simple-reverse/grid-motion.lua
@@ -0,0 +1,11 @@
+-- Authors: Rowan J. Gollan & Peter J.
+-- Dates: 2017-01-04 -- 2019-05-21
+--
+zero = Vector3:new{x=0, y=0}
+pSpeed = Vector3:new{x=-293.5, y=0}
+
+function assignVtxVelocities(t, dt)
+   local blkId = 0
+   -- Corner labels -               p00   p10     p11   p01
+   setVtxVelocitiesByCorners(blkId, zero, pSpeed, pSpeed, zero)
+end
---
+++ examples/eilmer/2D/moving-grid/piston-w-const-vel/simple-reverse/piston-test.rb
@@ -0,0 +1,63 @@
+#! /usr/bin/env ruby
+# piston-test.rb
+# Tests for the piston-with-constant-velocity example - reversed.
+# PJ, 2019-05-22
+#
+require 'test/unit'
+require 'open3'
+
+class TestPiston < Test::Unit::TestCase
+  def test_0_prep
+    cmd = "prep-gas ideal-air.inp ideal-air-gas-model.lua"
+    o, e, s = Open3.capture3(*cmd.split)
+    assert_equal(s.success?, true)
+    cmd = "e4shared --prep --job=piston"
+    o, e, s = Open3.capture3(*cmd.split)
+    assert_equal(s.success?, true)
+  end
+
+  def test_1_run
+    cmd = "e4shared --run --job=piston"
+    o, e, s = Open3.capture3(*cmd.split)
+    assert_equal(s.success?, true)
+    steps = 0
+    lines = o.split("\n")
+    lines.each do |txt|
+      if txt.match('final-t=') then
+        items = txt.split(' ')
+        steps = items[1].to_i
+      end
+    end
+    assert((steps - 506).abs < 3, "Failed to take correct number of steps.")
+  end
+
+  def test_2_post
+    # Check post-shock conditions.
+    cmd = 'e4shared --post --job=piston --probe=0.22,0,0'
+    o, e, s = Open3.capture3(*cmd.split)
+    assert_equal(s.success?, true)
+    # Sift through the output from probe, looking for our data line.
+    # The data line that we want comes immediately after a comment line
+    # that specifies the varible names.
+    posx = nil; velx = nil; p = nil; temp=nil
+    lines = o.split("\n")
+    found_comment_line = false
+    lines.each do |txt|
+      if found_comment_line then
+        # Next line is my data line and...
+        items = txt.split(' ')
+        posx = items[0].to_f; velx = items[5].to_f
+        p = items[8].to_f; temp = items[19].to_f
+        # once I have extracted values from it, get out.
+        break
+      end
+      if txt.match('^# ') then
+        found_comment_line = true
+      end
+    end
+    assert((posx - 0.22).abs < 1.0e-2, "Failed to get correct location.")
+    assert((velx + 293.0).abs < 1.0, "Failed to see correct velocity.")
+    assert((p - 30.257e3).abs < 500.0, "Failed to see correct pressure.")
+    assert((temp - 398.5).abs < 1.0, "Failed to see correct temperature.")
+  end
+end
