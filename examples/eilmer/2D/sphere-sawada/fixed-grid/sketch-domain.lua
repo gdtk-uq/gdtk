5cdbfd41c809bffc4c93cde85b11694b518c9fba
Peter Jacobs <peterj@mech.uq.edu.au>
2019-05-26 18:37:03 +1000

    Eilmer examples: improve the Sawada sphere grid, using the new Billig-patch.
    Adjust the test script so that it passes when using the WallBC_WithSlip1
    condition.  The changes are a different number of steps (because of the new grid)
    and a slightly relaxed tolerance on the shock stand-off distance.

--- examples/eilmer/2D/sphere-sawada/fixed-grid/sketch-domain.lua
+++ examples/eilmer/2D/sphere-sawada/fixed-grid/sketch-domain.lua
@@ -8,20 +8,7 @@ s:start{file_name="sphere.svg"}
 s:text{point=Vector3:new{x=-0.010,y=0.110}, text="flow domain for sphere", font_size=16}
 
 s:set{line_width=0.1, fill_colour="green"} -- for flow domain
-s:render{surf=psurf}
-
-s:set{line_width=0.5} -- thick line for sphere surface
-s:render{path=bc}; s:render{path=cd}; s:render{path=de}
-s:set{line_width=0.2} -- construction lines for Bezier curves
-s:line{p0=i, p1=j, dashed=true}; s:line{p0=h, p1=k, dashed=true}
-s:line{p0=e, p1=f, dashed=true}; s:line{p0=h, p1=g, dashed=true}
-
-s:dotlabel{point=a, label="a"}; s:dotlabel{point=b, label="b"}
-s:dotlabel{point=c, label="c"}; s:dotlabel{point=d, label="d"}
-s:dotlabel{point=e, label="e"}; s:dotlabel{point=f, label="f"}
-s:dotlabel{point=g, label="g"};
-s:dotlabel{point=h, label="h"}; s:dotlabel{point=i, label="i"}
-s:dotlabel{point=j, label="j"}; s:dotlabel{point=k, label="k"}
+s:render{surf=bp.patch}
 
 s:set{line_width=0.3} -- for drawing rules
 s:rule{direction="x", vmin=-0.060, vmax=0.040, vtic=0.020, anchor_point=Vector3:new{x=0,y=-0.002},
