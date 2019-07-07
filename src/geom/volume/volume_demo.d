05bd1ede93b353e078dd17f50273b0d3aa8e53c4
Peter Jacobs <peterj@mech.uq.edu.au>
2019-07-07 20:31:31 +1000

    Small fix to the demo program for defining volumes.

--- src/geom/volume/volume_demo.d
+++ src/geom/volume/volume_demo.d
@@ -40,8 +40,8 @@ void main()
     writeln("ssv(0.1, 0.1, 0.1)= ", ssv(0.1, 0.1, 0.1));
 
     writeln("TwoSurfaceVolume demo");
-    auto face0123 = new CoonsPatch(p[0], p[1], p[2], p[3]);
-    auto edge4567 = new CoonsPatch(p[4], p[5], p[6], p[7]);
+    face0123 = new CoonsPatch(p[0], p[1], p[2], p[3]);
+    auto face4567 = new CoonsPatch(p[4], p[5], p[6], p[7]);
     auto tsv = new TwoSurfaceVolume(face0123, face4567);
     writeln("tsv=", tsv);
     writeln("tsv(0.1, 0.1, 0.1)= ", tsv(0.1, 0.1, 0.1));
