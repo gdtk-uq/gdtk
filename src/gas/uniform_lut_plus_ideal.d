983e82e27f43011120ffcc4de2e1c0e6693be2d8
Peter Jacobs <peterj@mech.uq.edu.au>
2019-07-10 08:57:38 +1000

    Gas: handle, more carefully, zero mass-fractions in the LUT+ideal gas model.
    At this point, the Sod shock tube case runs nicely.

--- src/gas/uniform_lut_plus_ideal.d
+++ src/gas/uniform_lut_plus_ideal.d
@@ -43,10 +43,10 @@ public:
         // There are just two file-names to be read from the composite-gas-model file.
         string lut_file = getString(L, -1, "lut_file");
         string ideal_file = getString(L, -1, "ideal_file");
-        luaL_dofile(L, lut_file.toStringz());
+        doLuaFile(L, lut_file);
         lut_gas = new UniformLUT(L);
         Q_lut = new GasState(lut_gas);
-        luaL_dofile(L, ideal_file.toStringz());
+        doLuaFile(L, ideal_file);
         ideal_gas = new IdealGas(L);
         Q_ideal = new GasState(ideal_gas);
         create_species_reverse_lookup();
@@ -68,14 +68,28 @@ public:
             string msg = "Temperature and/or pressure was negative for update_thermo_from_pT."; 
             throw new GasModelException(msg);
         }
-        Q_lut.p = Q.massf[0]*Q.p; Q_lut.T = Q.T;
-        lut_gas.update_thermo_from_pT(Q_lut);
-        Q_ideal.p = Q.massf[1]*Q.p; Q_ideal.T = Q.T;
-        ideal_gas.update_thermo_from_pT(Q_ideal);
-        // Masses add.
-        Q.rho = Q_lut.rho + Q_ideal.rho;
-        // Internal energy is a weighted average.
-        Q.u = Q.massf[0]*Q_lut.u + Q.massf[1]*Q_ideal.u;
+        bool with_lut = Q.massf[0] > massf_tiny;
+        bool with_ideal = Q.massf[1] > massf_tiny;
+        if (with_lut) {
+            Q_lut.p = Q.massf[0]*Q.p; Q_lut.T = Q.T;
+            lut_gas.update_thermo_from_pT(Q_lut);
+        }
+        if (with_ideal) {
+            Q_ideal.p = Q.massf[1]*Q.p; Q_ideal.T = Q.T;
+            ideal_gas.update_thermo_from_pT(Q_ideal);
+        }
+        if (with_lut && with_ideal) {
+            // Masses add.
+            Q.rho = Q_lut.rho + Q_ideal.rho;
+            // Internal energy is a weighted average.
+            Q.u = Q.massf[0]*Q_lut.u + Q.massf[1]*Q_ideal.u;
+        } else if (with_lut) {
+            Q.rho = Q_lut.rho;
+            Q.u = Q_lut.u;
+        } else {
+            Q.rho = Q_ideal.rho;
+            Q.u = Q_ideal.u;
+        }
     }
     override void update_thermo_from_rhou(GasState Q)
     {
@@ -83,34 +97,48 @@ public:
             string msg = "Internal energy and/or density was negative for update_thermo_from_rhou."; 
             throw new GasModelException(msg);
         }
-        Q_lut.rho = Q.massf[0]*Q.rho;
-        Q_ideal.rho = Q.massf[1]*Q.rho;
-        // Need to determine the temperature at which the individual energies sum
-        // to the mixture internal energy.
-        // The LUT update of thermo from rhou is quite fast
-        // because that is the basic tabulation.
-        // Let's assume that the form of the internal energies is similar,
-        // such that we can just form an average T from the known energy.
-        Q_lut.u = Q.u; lut_gas.update_thermo_from_rhou(Q_lut);
-        Q_ideal.u = Q.u; ideal_gas.update_thermo_from_rhou(Q_ideal);
-        // Initial guess for Temperature
-        number T = Q.massf[0]*Q_lut.T + Q.massf[1]*Q_ideal.T;
-        number u_error(number T)
-        {
+        bool with_lut = Q.massf[0] > massf_tiny;
+        bool with_ideal = Q.massf[1] > massf_tiny;
+        if (with_lut && with_ideal) {
+            Q_lut.rho = Q.massf[0]*Q.rho;
+            Q_ideal.rho = Q.massf[1]*Q.rho;
+            // Need to determine the temperature at which the individual energies sum
+            // to the mixture internal energy.
+            // The LUT update of thermo from rhou is quite fast
+            // because that is the basic tabulation.
+            // Let's assume that the form of the internal energies is similar,
+            // such that we can just form an average T from the known energy.
+            Q_lut.u = Q.u; lut_gas.update_thermo_from_rhou(Q_lut);
+            Q_ideal.u = Q.u; ideal_gas.update_thermo_from_rhou(Q_ideal);
+            // Initial guess for Temperature
+            number T = Q.massf[0]*Q_lut.T + Q.massf[1]*Q_ideal.T;
+            number u_error(number T)
+            {
+                Q_lut.T = T; Q_ideal.T = T;
+                lut_gas.update_thermo_from_rhoT(Q_lut);
+                ideal_gas.update_thermo_from_rhoT(Q_ideal);
+                return (Q.massf[0]*Q_lut.u + Q.massf[1]*Q_ideal.u) - Q.u;
+            }
+            number T2 = T*1.1; number T1 = T*0.9;
+            bracket!(u_error, number)(T1, T2);
+            T = solve!(u_error, number)(T1, T2, 1.0e-6);
             Q_lut.T = T; Q_ideal.T = T;
             lut_gas.update_thermo_from_rhoT(Q_lut);
             ideal_gas.update_thermo_from_rhoT(Q_ideal);
-            return (Q.massf[0]*Q_lut.u + Q.massf[1]*Q_ideal.u) - Q.u;
+            // Partial pressures just sum to the mixture pressure.
+            Q.p = Q_lut.p + Q_ideal.p;
+            Q.T = T;
+        } else if (with_lut) {
+            Q_lut.rho = Q.rho; Q_lut.u = Q.u;
+            lut_gas.update_thermo_from_rhou(Q_lut);
+            Q.p = Q_lut.p;
+            Q.T = Q_lut.T;
+        } else {
+            Q_ideal.rho = Q.rho; Q_ideal.u = Q.u;
+            ideal_gas.update_thermo_from_rhou(Q_ideal);
+            Q.p = Q_ideal.p;
+            Q.T = Q_ideal.T;
         }
-        number T2 = T*1.1; number T1 = T*0.9;
-        bracket!(u_error, number)(T1, T2);
-        T = solve!(u_error, number)(T1, T2, 1.0e-6);
-        Q_lut.T = T; Q_ideal.T = T;
-        lut_gas.update_thermo_from_rhoT(Q_lut);
-        ideal_gas.update_thermo_from_rhoT(Q_ideal);
-        // Partial pressures just sum to the mixture pressure.
-        Q.p = Q_lut.p + Q_ideal.p;
-        Q.T = T;
     }
     override void update_thermo_from_rhoT(GasState Q)
     {
@@ -118,14 +146,28 @@ public:
             string msg = "Temperature and/or density was negative for update_thermo_from_rhoT."; 
             throw new GasModelException(msg);
         }
-        Q_lut.T = Q.T;
-        Q_lut.rho = Q.massf[0] * Q.rho;
-        lut_gas.update_thermo_from_rhoT(Q_lut);
-        Q_ideal.T = Q.T;
-        Q_ideal.rho = Q.massf[1] * Q.rho;
-        ideal_gas.update_thermo_from_rhoT(Q_ideal);
-        Q.p = Q_lut.p + Q_ideal.p; // partial pressures add
-        Q.u = Q.massf[0]*Q_lut.u + Q.massf[1]*Q_ideal.u;
+        bool with_lut = Q.massf[0] > massf_tiny;
+        bool with_ideal = Q.massf[1] > massf_tiny;
+        if (with_lut) {
+            Q_lut.T = Q.T;
+            Q_lut.rho = Q.massf[0] * Q.rho;
+            lut_gas.update_thermo_from_rhoT(Q_lut);
+        }
+        if (with_ideal) {
+            Q_ideal.T = Q.T;
+            Q_ideal.rho = Q.massf[1] * Q.rho;
+            ideal_gas.update_thermo_from_rhoT(Q_ideal);
+        }
+        if (with_lut && with_ideal) {
+            Q.p = Q_lut.p + Q_ideal.p; // partial pressures add
+            Q.u = Q.massf[0]*Q_lut.u + Q.massf[1]*Q_ideal.u;
+        } else if (with_lut) {
+            Q.p = Q_lut.p;
+            Q.u = Q_lut.u;
+        } else {
+            Q.p = Q_ideal.p;
+            Q.u = Q_ideal.u;
+        }
     }
     override void update_thermo_from_rhop(GasState Q)
     {
@@ -133,27 +175,41 @@ public:
             string msg = "Pressure and/or density was negative for update_thermo_from_rhop."; 
             throw new GasModelException(msg);
         }
-        Q_lut.rho = Q.massf[0]*Q.rho;
-        Q_ideal.rho = Q.massf[1]*Q.rho;
-        // Need to determine the temperature at which the partial pressures sum
-        // to the mixture pressure.
-        // First guess is just ideal gas
-        number T = Q.p/ideal_gas.gas_constant(Q_ideal)/Q.rho;
-        number p_error(number T)
-        {
+        bool with_lut = Q.massf[0] > massf_tiny;
+        bool with_ideal = Q.massf[1] > massf_tiny;
+        if (with_lut && with_ideal) {
+            Q_lut.rho = Q.massf[0]*Q.rho;
+            Q_ideal.rho = Q.massf[1]*Q.rho;
+            // Need to determine the temperature at which the partial pressures sum
+            // to the mixture pressure.
+            // First guess is just ideal gas
+            number T = Q.p/ideal_gas.gas_constant(Q_ideal)/Q.rho;
+            number p_error(number T)
+            {
+                Q_lut.T = T; Q_ideal.T = T;
+                lut_gas.update_thermo_from_rhoT(Q_lut);
+                ideal_gas.update_thermo_from_rhoT(Q_ideal);
+                return (Q_lut.p + Q_ideal.p) - Q.p;
+            }
+            number T2 = T*1.1; number T1 = T*0.9;
+            bracket!(p_error, number)(T1, T2);
+            T = solve!(p_error, number)(T1, T2, 1.0e-6);
             Q_lut.T = T; Q_ideal.T = T;
             lut_gas.update_thermo_from_rhoT(Q_lut);
             ideal_gas.update_thermo_from_rhoT(Q_ideal);
-            return (Q_lut.p + Q_ideal.p) - Q.p;
+            Q.T = T;
+            Q.u = Q.massf[0]*Q_lut.u + Q.massf[1]*Q_ideal.u;
+        } else if (with_lut) {
+            Q_lut.rho = Q.rho; Q_lut.p = Q.p;
+            lut_gas.update_thermo_from_rhop(Q_lut);
+            Q.T = Q_lut.T;
+            Q.u = Q_lut.u;
+        } else {
+            Q_ideal.rho = Q.rho; Q_ideal.p = Q.p;
+            lut_gas.update_thermo_from_rhop(Q_ideal);
+            Q.T = Q_ideal.T;
+            Q.u = Q_ideal.u;
         }
-        number T2 = T*1.1; number T1 = T*0.9;
-        bracket!(p_error, number)(T1, T2);
-        T = solve!(p_error, number)(T1, T2, 1.0e-6);
-        Q_lut.T = T; Q_ideal.T = T;
-        lut_gas.update_thermo_from_rhoT(Q_lut);
-        ideal_gas.update_thermo_from_rhoT(Q_ideal);
-        Q.T = T;
-        Q.u = Q.massf[0]*Q_lut.u + Q.massf[1]*Q_ideal.u;
     }
     
     override void update_thermo_from_ps(GasState Q, number s)
@@ -175,26 +231,54 @@ public:
             string msg = "Temperature was negative for update_sound_speed."; 
             throw new GasModelException(msg);
         }
-        Q_lut.rho = Q.massf[0]*Q.rho;
-        Q_ideal.rho = Q.massf[1]*Q.rho;
-        Q_lut.T = Q.T; Q_ideal.T = Q.T;
-        lut_gas.update_thermo_from_rhoT(Q_lut);
-        ideal_gas.update_thermo_from_rhoT(Q_ideal);
-        lut_gas.update_sound_speed(Q_lut);
-        ideal_gas.update_sound_speed(Q_ideal);
-        Q.a = Q.massf[0]*Q_lut.a + Q.massf[1]*Q_ideal.a;
+        bool with_lut = Q.massf[0] > massf_tiny;
+        bool with_ideal = Q.massf[1] > massf_tiny;
+        if (with_lut) {
+            Q_lut.rho = Q.massf[0]*Q.rho;
+            Q_lut.T = Q.T;
+            lut_gas.update_thermo_from_rhoT(Q_lut);
+            lut_gas.update_sound_speed(Q_lut);
+        }
+        if (with_ideal) {
+            Q_ideal.rho = Q.massf[1]*Q.rho;
+            Q_ideal.T = Q.T;
+            ideal_gas.update_thermo_from_rhoT(Q_ideal);
+            ideal_gas.update_sound_speed(Q_ideal);
+        }
+        if (with_lut && with_ideal) {
+            Q.a = Q.massf[0]*Q_lut.a + Q.massf[1]*Q_ideal.a;
+        } else if (with_lut) {
+            Q.a = Q_lut.a;
+        } else {
+            Q.a = Q_ideal.a;
+        }
     }
     override void update_trans_coeffs(GasState Q)
     {
-        Q_lut.rho = Q.massf[0]*Q.rho;
-        Q_ideal.rho = Q.massf[1]*Q.rho;
-        Q_lut.T = Q.T; Q_ideal.T = Q.T;
-        lut_gas.update_thermo_from_rhoT(Q_lut);
-        ideal_gas.update_thermo_from_rhoT(Q_ideal);
-        lut_gas.update_trans_coeffs(Q_lut);
-        ideal_gas.update_trans_coeffs(Q_ideal);
-        Q.mu = Q.massf[0]*Q_lut.mu + Q.massf[1]*Q_ideal.mu;
-        Q.k = Q.massf[0]*Q_lut.k + Q.massf[1]*Q_ideal.k;
+        bool with_lut = Q.massf[0] > massf_tiny;
+        bool with_ideal = Q.massf[1] > massf_tiny;
+        if (with_lut) {
+            Q_lut.rho = Q.massf[0]*Q.rho;
+            Q_lut.T = Q.T;
+            lut_gas.update_thermo_from_rhoT(Q_lut);
+            lut_gas.update_trans_coeffs(Q_lut);
+        }
+        if (with_ideal) {
+            Q_ideal.rho = Q.massf[1]*Q.rho;
+            Q_ideal.T = Q.T;
+            ideal_gas.update_thermo_from_rhoT(Q_ideal);
+            ideal_gas.update_trans_coeffs(Q_ideal);
+        }
+        if (with_lut && with_ideal) {
+            Q.mu = Q.massf[0]*Q_lut.mu + Q.massf[1]*Q_ideal.mu;
+            Q.k = Q.massf[0]*Q_lut.k + Q.massf[1]*Q_ideal.k;
+        } else if (with_lut) {
+            Q.mu = Q_lut.mu;
+            Q.k = Q_lut.k;
+        } else {
+            Q.mu = Q_ideal.mu;
+            Q.k = Q_ideal.k;
+        }
     }
     /*
     override void eval_diffusion_coefficients(ref GasState Q) {
@@ -203,43 +287,103 @@ public:
     */
     override number dudT_const_v(in GasState Q)
     {
-        Q_lut.rho = Q.massf[0]*Q.rho;
-        Q_ideal.rho = Q.massf[1]*Q.rho;
-        Q_lut.T = Q.T; Q_ideal.T = Q.T;
-        lut_gas.update_thermo_from_rhoT(Q_lut);
-        ideal_gas.update_thermo_from_rhoT(Q_ideal);
-        return Q.massf[0]*lut_gas.dudT_const_v(Q_lut) +
-            Q.massf[1]*ideal_gas.dudT_const_v(Q_ideal);
+        bool with_lut = Q.massf[0] > massf_tiny;
+        bool with_ideal = Q.massf[1] > massf_tiny;
+        if (with_lut) {
+            Q_lut.rho = Q.massf[0]*Q.rho;
+            Q_lut.T = Q.T;
+            lut_gas.update_thermo_from_rhoT(Q_lut);
+        }
+        if (with_ideal) {
+            Q_ideal.rho = Q.massf[1]*Q.rho;
+            Q_ideal.T = Q.T;
+            ideal_gas.update_thermo_from_rhoT(Q_ideal);
+        }
+        number result;
+        if (with_lut && with_ideal) {
+            result = Q.massf[0]*lut_gas.dudT_const_v(Q_lut) +
+                Q.massf[1]*ideal_gas.dudT_const_v(Q_ideal);
+        } else if (with_lut) {
+            result = lut_gas.dudT_const_v(Q_lut);
+        } else {
+            result = ideal_gas.dudT_const_v(Q_ideal);
+        }
+        return result;
     }
     override number dhdT_const_p(in GasState Q)
     {
-        Q_lut.rho = Q.massf[0]*Q.rho;
-        Q_ideal.rho = Q.massf[1]*Q.rho;
-        Q_lut.T = Q.T; Q_ideal.T = Q.T;
-        lut_gas.update_thermo_from_rhoT(Q_lut);
-        ideal_gas.update_thermo_from_rhoT(Q_ideal);
-        return Q.massf[0]*lut_gas.dhdT_const_p(Q_lut) +
-            Q.massf[1]*ideal_gas.dhdT_const_p(Q_ideal);
+        bool with_lut = Q.massf[0] > massf_tiny;
+        bool with_ideal = Q.massf[1] > massf_tiny;
+        if (with_lut) {
+            Q_lut.rho = Q.massf[0]*Q.rho;
+            Q_lut.T = Q.T;
+            lut_gas.update_thermo_from_rhoT(Q_lut);
+        }
+        if (with_ideal) {
+            Q_ideal.rho = Q.massf[1]*Q.rho;
+            Q_ideal.T = Q.T;
+            ideal_gas.update_thermo_from_rhoT(Q_ideal);
+        }
+        number result;
+        if (with_lut && with_ideal) {
+            result = Q.massf[0]*lut_gas.dhdT_const_p(Q_lut) +
+                Q.massf[1]*ideal_gas.dhdT_const_p(Q_ideal);
+        } else if (with_lut) {
+            result = lut_gas.dhdT_const_p(Q_lut);
+        } else {
+            result = ideal_gas.dhdT_const_p(Q_ideal);
+        }
+        return result;
     }
     override number dpdrho_const_T(in GasState Q)
     {
-        Q_lut.rho = Q.massf[0]*Q.rho;
-        Q_ideal.rho = Q.massf[1]*Q.rho;
-        Q_lut.T = Q.T; Q_ideal.T = Q.T;
-        lut_gas.update_thermo_from_rhoT(Q_lut);
-        ideal_gas.update_thermo_from_rhoT(Q_ideal);
-        return Q.massf[0]*lut_gas.dpdrho_const_T(Q_lut) +
-            Q.massf[1]*ideal_gas.dpdrho_const_T(Q_ideal);
+        bool with_lut = Q.massf[0] > massf_tiny;
+        bool with_ideal = Q.massf[1] > massf_tiny;
+        if (with_lut) {
+            Q_lut.rho = Q.massf[0]*Q.rho;
+            Q_lut.T = Q.T;
+            lut_gas.update_thermo_from_rhoT(Q_lut);
+        }
+        if (with_ideal) {
+            Q_ideal.rho = Q.massf[1]*Q.rho;
+            Q_ideal.T = Q.T;
+            ideal_gas.update_thermo_from_rhoT(Q_ideal);
+        }
+        number result;
+        if (with_lut && with_ideal) {
+            result = Q.massf[0]*lut_gas.dpdrho_const_T(Q_lut) +
+                Q.massf[1]*ideal_gas.dpdrho_const_T(Q_ideal);
+        } else if (with_lut) {
+            result = lut_gas.dpdrho_const_T(Q_lut);
+        } else {
+            result = ideal_gas.dpdrho_const_T(Q_ideal);
+        }
+        return result;
     }
     override number gas_constant(in GasState Q)
     {
-        Q_lut.rho = Q.massf[0]*Q.rho;
-        Q_ideal.rho = Q.massf[1]*Q.rho;
-        Q_lut.T = Q.T; Q_ideal.T = Q.T;
-        lut_gas.update_thermo_from_rhoT(Q_lut);
-        ideal_gas.update_thermo_from_rhoT(Q_ideal);
-        return Q.massf[0]*lut_gas.gas_constant(Q_lut) +
-            Q.massf[1]*ideal_gas.gas_constant(Q_ideal);
+        bool with_lut = Q.massf[0] > massf_tiny;
+        bool with_ideal = Q.massf[1] > massf_tiny;
+        if (with_lut) {
+            Q_lut.rho = Q.massf[0]*Q.rho;
+            Q_lut.T = Q.T;
+            lut_gas.update_thermo_from_rhoT(Q_lut);
+        }
+        if (with_ideal) {
+            Q_ideal.rho = Q.massf[1]*Q.rho;
+            Q_ideal.T = Q.T;
+            ideal_gas.update_thermo_from_rhoT(Q_ideal);
+        }
+        number result;
+        if (with_lut && with_ideal) {
+            result = Q.massf[0]*lut_gas.gas_constant(Q_lut) +
+                Q.massf[1]*ideal_gas.gas_constant(Q_ideal);
+        } else if (with_lut) {
+            result = lut_gas.gas_constant(Q_lut);
+        } else {
+            result = ideal_gas.gas_constant(Q_ideal);
+        }
+        return result;
     }
     override @nogc number internal_energy(in GasState Q)
     {
@@ -251,13 +395,28 @@ public:
     }
     override number entropy(in GasState Q)
     {
-        Q_lut.rho = Q.massf[0]*Q.rho;
-        Q_ideal.rho = Q.massf[1]*Q.rho;
-        Q_lut.T = Q.T; Q_ideal.T = Q.T;
-        lut_gas.update_thermo_from_rhoT(Q_lut);
-        ideal_gas.update_thermo_from_rhoT(Q_ideal);
-        return Q.massf[0]*lut_gas.entropy(Q_lut) +
-            Q.massf[1]*ideal_gas.entropy(Q_ideal);
+        bool with_lut = Q.massf[0] > massf_tiny;
+        bool with_ideal = Q.massf[1] > massf_tiny;
+        if (with_lut) {
+            Q_lut.rho = Q.massf[0]*Q.rho;
+            Q_lut.T = Q.T;
+            lut_gas.update_thermo_from_rhoT(Q_lut);
+        }
+        if (with_ideal) {
+            Q_ideal.rho = Q.massf[1]*Q.rho;
+            Q_ideal.T = Q.T;
+            ideal_gas.update_thermo_from_rhoT(Q_ideal);
+        }
+        number result;
+        if (with_lut && with_ideal) {
+            result = Q.massf[0]*lut_gas.entropy(Q_lut) +
+                Q.massf[1]*ideal_gas.entropy(Q_ideal);
+        } else if (with_lut) {
+            result = lut_gas.entropy(Q_lut);
+        } else {
+            result = ideal_gas.entropy(Q_ideal);
+        }
+        return result;
     }
 
 private:
@@ -265,6 +424,7 @@ private:
     GasModel lut_gas;
     GasModel ideal_gas;
     GasState Q_lut, Q_ideal;
+    immutable double massf_tiny = 1.0e-6;
 } // end class UniformLUTPlusIdealGas
 
 version(uniform_lut_plus_ideal_test) {
