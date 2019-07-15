b80bbfa20e9b25ad261db1c4738565b386917a0f
Peter Jacobs <peterj@mech.uq.edu.au>
2019-07-15 12:36:43 +1000

    Eilmer: simplify the input script for the forward-facing-step example.

--- examples/eilmer/2D/forward-facing-step/ffs.lua
+++ examples/eilmer/2D/forward-facing-step/ffs.lua
@@ -1,7 +1,7 @@
--- ffs.lua -- Forward-facing-step example
--- PJ & RG  2015-03-08
+-- ffs.lua
+-- Forward-facing-step
+-- PJ & RG  2015-03-08, 2019-07-15 simplify
 
--- We can set individual attributes of the global data object.
 config.title = "Forward-facing step with supersonic flow."
 print(config.title)
 config.dimensions = 2
@@ -15,14 +15,9 @@ print("sound speed=", sspeed)
 inflow = FlowState:new{p=101.325e3, T=300.0, velx=3.0*sspeed, vely=0.0}
 
 -- Geometry of the flow domain.
-a0 = Vector3:new{x=0.0, y=0.0};
-a1 = Vector3:new{x=0.0, y=0.2};
-a2 = Vector3:new{x=0.0, y=1.0}
-b0 = Vector3:new{x=0.6, y=0.0};
-b1 = Vector3:new{x=0.6, y=0.2};
-b2 = Vector3:new{x=0.6, y=1.0}
-c1 = Vector3:new{x=3.0, y=0.2};
-c2 = Vector3:new{x=3.0, y=1.0}
+a0 = {x=0.0, y=0.0}; a1 = {x=0.0, y=0.2}; a2 = {x=0.0, y=1.0}
+b0 = {x=0.6, y=0.0}; b1 = {x=0.6, y=0.2}; b2 = {x=0.6, y=1.0}
+c1 = {x=3.0, y=0.2}; c2 = {x=3.0, y=1.0}
 surf0 = CoonsPatch:new{p00=a0, p10=b0, p11=b1, p01=a1}
 surf1 = CoonsPatch:new{p00=a1, p10=b1, p11=b2, p01=a2}
 surf2 = CoonsPatch:new{p00=b1, p10=c1, p11=c2, p01=b2}
@@ -38,28 +33,20 @@ grid1 = StructuredGrid:new{psurface=surf1, niv=nab+1, njv=n12+1}
 grid2 = StructuredGrid:new{psurface=surf2, niv=nbc+1, njv=n12+1}
 
 -- Set boundary conditions that we care about.
-bcList0 = {north=nil, east=nil, south=nil, 
-	   west=InFlowBC_Supersonic:new{flowState=inflow, label="inflow-boundary"}}
-bcList1 = {north=nil, east=nil, south=nil,
-	   west=InFlowBC_Supersonic:new{flowState=inflow, label="inflow-boundary"}}
-bcList2 = {north=nil, east=OutFlowBC_Simple:new{label="outflow-boundary"},
-	   south=nil, west=nil}
+bcList01 = { west=InFlowBC_Supersonic:new{flowState=inflow, label="inflow"} }
+bcList2 = { east=OutFlowBC_Simple:new{label="outflow"} }
 
 -- Define the flow-solution blocks and stitch them together.
 blk0 = FluidBlockArray{grid=grid0, nib=1, njb=1, 
-		       initialState=inflow, bcList=bcList0, label="BLOCK-0"}
+		       initialState=inflow, bcList=bcList01}
 blk1 = FluidBlockArray{grid=grid1, nib=1, njb=4,
-		       initialState=inflow, bcList=bcList1, label="BLOCK-1"}
+		       initialState=inflow, bcList=bcList01}
 blk2 = FluidBlockArray{grid=grid2, nib=4, njb=4,
-		       initialState=inflow, bcList=bcList2, label="BLOCK-2"}
+		       initialState=inflow, bcList=bcList2}
 identifyBlockConnections()
 
--- Do a little more setting of global data.
+-- The number of MPI tasks needs to match the mpirun command.
 mpiTasks = mpiDistributeBlocks{ntasks=3}
 config.max_time = 5.0e-3  -- seconds
 config.max_step = 6000
-config.dt_init = 1.0e-6
-config.cfl_value = 0.5
--- config.dt_max = 10.0e-6
 config.dt_plot = 1.0e-3
-config.dt_history = 10.0e-6
