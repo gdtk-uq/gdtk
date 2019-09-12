2b9a81c7620c89619a32b3d06f7f7d2af1183ea2
Rowan J. Gollan <r.gollan@uq.edu.au>
2019-09-12 14:44:20 +1000

    Eilmer/examples: replace use of Python2 execfile in manufactured-solution.

--- examples/eilmer/2D/manufactured-solution/sg/smoke-tests/analytic_solution.py
+++ examples/eilmer/2D/manufactured-solution/sg/smoke-tests/analytic_solution.py
@@ -15,7 +15,7 @@
 
 from __future__ import print_function
 from sympy import *
-execfile('constants.txt')
+exec(open('./constants.txt').read())
 # Read case no.
 fp = open('case.txt', 'r');
 case_str = fp.readline().strip()
