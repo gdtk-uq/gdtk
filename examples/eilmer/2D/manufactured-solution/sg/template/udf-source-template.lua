ebd75e536a5f05d1a0d13d971a13057dd5d57454
Peter Jacobs <peterj@mech.uq.edu.au>
2019-10-09 22:42:53 +1000

    Eilmer: sample at 4 Gauss points for the MMS cases with 2D flows.
    This gives us close to order 3 convergence for the classic PPR on the
    supersonic Euler case.
    $ cat rho-L2-norm-case-1-with-4-gauss-points.dat
      1.250000000000e-01   2.512372747384e-03
      6.250000000000e-02   3.238976064309e-04
      3.125000000000e-02   4.310609003992e-05
    $ cat rho-L2-observed-order-case-1-with-4-gauss-points.dat
      1.250000000000e-01   2.955440813179e+00
      6.250000000000e-02   2.909574193641e+00

--- examples/eilmer/2D/manufactured-solution/sg/template/udf-source-template.lua
+++ examples/eilmer/2D/manufactured-solution/sg/template/udf-source-template.lua
@@ -20,17 +20,20 @@ end
 function sourceTerms(t, cell)
    src = {}
    if multipoint_sample then
-      -- Sample at two Gauss points for case 6 1D high-order test
+      -- Sample at Gauss points, cross-product of 2 points in each direction.
       local dx = cell.iLength / (2*math.sqrt(3))
-      fmassA, fxmomA, fymomA, feA = sourceTermsAtPoint(cell.x-dx, cell.y)
-      fmassB, fxmomB, fymomB, feB = sourceTermsAtPoint(cell.x+dx, cell.y)
-      src.mass = 0.5*(fmassA + fmassB)
-      src.momentum_x = 0.5*(fxmomA + fxmomB)
-      src.momentum_y = 0.5*(fymomA + fymomB)
+      local dy = cell.jLength / (2*math.sqrt(3))
+      fmassA, fxmomA, fymomA, feA = sourceTermsAtPoint(cell.x-dx, cell.y-dy)
+      fmassB, fxmomB, fymomB, feB = sourceTermsAtPoint(cell.x+dx, cell.y-dy)
+      fmassC, fxmomC, fymomC, feC = sourceTermsAtPoint(cell.x+dx, cell.y+dy)
+      fmassD, fxmomD, fymomD, feD = sourceTermsAtPoint(cell.x-dx, cell.y+dy)
+      src.mass = 0.25*(fmassA + fmassB + fmassC + fmassD)
+      src.momentum_x = 0.25*(fxmomA + fxmomB + fxmomC + fxmomD)
+      src.momentum_y = 0.25*(fymomA + fymomB + fymomC + fymomD)
       src.momentum_z = 0.0
-      src.total_energy = 0.5*(feA + feB)
+      src.total_energy = 0.25*(feA + feB + feC + feD)
    else
-      -- Single sample at midpoint sample.
+      -- Single sample at midpoint of cell.
       fmass, fxmom, fymom, fe = sourceTermsAtPoint(cell.x, cell.y)
       src.mass = fmass
       src.momentum_x = fxmom
