b1acc97424b125f28f787c01115338e217eeaa96
Peter Jacobs <peterj@mech.uq.edu.au>
2019-11-28 07:57:58 +1000

    Refresh nitrogen reactor script.

--- examples/kinetics/fixed-volume-reactor-n2/fvreactor.lua
+++ examples/kinetics/fixed-volume-reactor-n2/fvreactor.lua
@@ -3,15 +3,15 @@
 -- PJ & RJG 2018-04-21.
 
 gm = GasModel:new{"nitrogen-2sp.lua"}
-chemUpdate = ChemistryUpdate:new{filename="chem.lua", gasmodel=gm}
+chemUpdate = ChemistryUpdate:new{gasmodel=gm, filename="chem.lua"}
 
-Q = GasState:new{gm}
-Q.p = 1.0e5 -- Pa
-Q.T = 4000.0 -- degree K
+gs = GasState:new{gm}
+gs.p = 1.0e5 -- Pa
+gs.T = 4000.0 -- degree K
 molef = {N2=2/3, N=1/3}
-Q.massf = gm:molef2massf(molef)
-gm:updateThermoFromPT(Q)
-conc = gm:massf2conc(Q)
+gs.massf = gm:molef2massf(molef)
+gm:updateThermoFromPT(gs)
+conc = gm:massf2conc(gs)
 
 tFinal = 200.0e-6 -- s
 t = 0.0
@@ -21,15 +21,15 @@ print("# Start integration")
 f = assert(io.open("fvreactor.data", 'w'))
 f:write('# 1:t(s)  2:T(K)  3:p(Pa)  4:massf_N2  5:massf_N  6:conc_N2  7:conc_N\n')
 f:write(string.format("%10.3e %10.3f %10.3e %20.12e %20.12e %20.12e %20.12e\n",
-                      t, Q.T, Q.p, Q.massf.N2, Q.massf.N, conc.N2, conc.N))
+                      t, gs.T, gs.p, gs.massf.N2, gs.massf.N, conc.N2, conc.N))
 while t <= tFinal do
-   dtSuggest = chemUpdate:updateState(Q, dt, dtSuggest, gm)
+   dtSuggest = chemUpdate:updateState(gs, dt, dtSuggest, gm)
    t = t + dt
    -- dt = dtSuggest -- uncomment this to get quicker stepping
-   gm:updateThermoFromRHOE(Q)
-   conc = gm:massf2conc(Q)
+   gm:updateThermoFromRHOE(gs)
+   conc = gm:massf2conc(gs)
    f:write(string.format("%10.3e %10.3f %10.3e %20.12e %20.12e %20.12e %20.12e\n",
-                         t, Q.T, Q.p, Q.massf.N2, Q.massf.N, conc.N2, conc.N))
+                         t, gs.T, gs.p, gs.massf.N2, gs.massf.N, conc.N2, conc.N))
 end
 f:close()
 print("# Done.")
