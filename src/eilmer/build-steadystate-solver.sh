f465ec661e8b3546ef68fca8f2de2edf69bd5d01
Kyle Damm <kyle.damm@uqconnect.edu.au>
2019-12-19 10:11:58 +1000

    eilmer: tidied up the build script for the steady-state solvers.

--- src/eilmer/build-steadystate-solver.sh
+++ src/eilmer/build-steadystate-solver.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
-# build-complex-solvers.sh
-#
-# Trial build of Kyle's complex-numbers flavour of the solvers.
+# build-steadystate-solvers.sh
+# Trial build of the steady-state solvers.
 
-if make DMD=ldmd2  WITH_COMPLEX_NUMBERS=1 WITH_SSC=1 WITH_SSS=1
+if make DMD=ldmd2  WITH_COMPLEX_NUMBERS=1 WITH_SSC=1 WITH_SSS=1 FLAVOUR=fast install
 then
-    echo "Build successful for steady-state solver."
+    echo "Build and install successful for FLAVOUR=fast."
 else
-    echo "Build failed."
+    echo "Build and install failed for FLAVOUR=fast."
 fi
