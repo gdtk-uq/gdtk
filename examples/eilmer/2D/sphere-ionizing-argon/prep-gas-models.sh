c95315ca54e462813cbc5797617f9771da76f49c
Peter Jacobs <peterj@mech.uq.edu.au>
2020-01-24 08:00:43 +1000

    Eilmer: argon flow over a sphere: use Hoffert & Lien 2T argon model.

--- examples/eilmer/2D/sphere-ionizing-argon/prep-gas-models.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# prep-gas-models.sh
-prep-gas ionizing-argon-model.inp ionizing-argon-model.lua
-prep-chem ionizing-argon-model.lua ionizing-argon-reactions.lua \
-          ionizing-argon-reactor.lua
