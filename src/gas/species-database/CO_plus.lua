b04d8f7ea3ba01d757218df270603ef1896c02b0
Jens Kunze <j.kunze@uq.edu.au>
2020-01-31 12:55:58 +1000

    gas: updated C2.lua to new format, added CO_plus.lua and C_plus.lua, updated species-list.txt

---
+++ src/gas/species-database/CO_plus.lua
@@ -0,0 +1,58 @@
+db['CO+'] = {}
+db['CO+'].atomicConstituents = {C=1,O=1,}
+db['CO+'].charge = 1
+db['CO+'].M = {
+   value = 28.095514e-3,
+   units = 'kg/mol',
+   description = 'molecular mass',
+   reference = 'Periodic table'
+}
+db['CO+'].gamma = {
+   value = 1.3992e+00,
+   units = 'non-dimensional',
+   description = 'ratio of specific heats at 300.0K',
+   reference = 'diatomic molecule at low temperatures, gamma = 7/5'
+}
+db['CO+'].ceaThermoCoeffs = {
+   nsegments = 3,
+   T_break_points = {298.15, 1000.0, 6000.0, 20000.0},
+   T_blend_ranges = {400.0, 1000.0},
+   segment0 = {
+     -2.178786658e+04,
+      1.288857032e+02, 
+      3.769057550e+00,
+     -3.431730130e-03, 
+      8.193945750e-06,
+     -6.463814690e-09,
+      1.803727574e-12,
+      1.482345898e+05,
+      3.990547070e+00
+   },
+   segment1 = { 
+      2.316847506e+05,
+     -1.057646148e+03,
+      4.554257780e+00,
+      4.495520320e-04,
+     -2.489507047e-07,
+      5.267566420e-11,
+     -3.289510270e-15,
+      1.555050724e+05,
+     -3.873462640e+00
+   },
+   segment2 = {
+     -3.035604054e+08,
+      2.393118392e+05,
+     -7.034999240e+01,
+      1.139551440e-02,
+     -8.315173100e-07,
+      2.863705515e-11,
+     -3.803269410e-16,
+     -1.688617704e+06,
+      6.291980420e+02
+   },
+}
+-- No CEA transport data for CO+, just use CO
+db['CO+'].ceaViscosity = db.CO.ceaViscosity 
+db['CO+'].ceaThermCond = db.CO.ceaThermCond 
+
+
---
+++ src/gas/species-database/C_plus.lua
@@ -0,0 +1,53 @@
+db['C+'] = {}
+db['C+'].atomicConstituents = {C=1,}
+db['C+'].charge = 1
+db['C+'].M = { 
+   value = 12.0101514e-3,
+   units = 'kg/mol',
+   description = 'molecular mass',
+   reference = 'molecular weight from CEA2'
+}
+db['C+'].gamma = { 
+   value = 1.664,
+   units = 'non-dimensional',
+   description = 'ratio of specific heats at room temperature (= Cp/(Cp - R))',
+   reference = 'copied from C.lua'
+}
+db['C+'].ceaThermoCoeffs = {
+   nsegments = 3,
+   T_break_points = {298.15, 1000.0, 6000.0, 20000.0},
+   T_blend_ranges = {400.0, 1000.0},
+   segment0 = {
+      2.258535929e+03,
+     -1.574575687e+00,
+      2.503637730e+00,
+     -5.202878370e-06,
+      4.516908390e-09,
+     -2.181431053e-12,
+      4.495047033e-16,
+      2.168951913e+05,
+      4.345699505e+00
+   },
+   segment1 = {
+      1.255112551e+04,
+     -3.411874670e+01,
+      2.543383218e+00,
+     -2.805120849e-05,
+      9.751641970e-09,
+     -1.736855394e-12,
+      1.246191931e-16,
+      2.171001786e+05, 
+      4.063913515e+00
+   },
+   segment2 = {
+      5.618135320e+05,
+     -6.047058900e+03,
+      5.884541470e+00,
+     -7.211894530e-04,
+      6.823484110e-08,
+     -2.599878590e-12,
+      3.633868358e-17,
+      2.581370458e+05,
+     -2.280019759e+01
+   }
+}
