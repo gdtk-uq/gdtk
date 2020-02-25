016e284a0015dc6c0ddbfcf82745a9f3dee1fdfb
Peter Jacobs <peterj@mech.uq.edu.au>
2020-02-25 22:52:37 +1000

    Eilmer: get the loads writing for multiple groups closer to Ingo's design.

--- src/eilmer/globalconfig.d
+++ src/eilmer/globalconfig.d
@@ -740,7 +740,10 @@ final class GlobalConfig {
     shared static double dt_plot = 1.0e-3; // interval for writing soln
     shared static double dt_history = 1.0e-3; // interval for writing sample
     shared static double dt_loads = 1.0e-3; // interval for writing loads on boundary groups
+    // The following initialization preserves old behaviour
+    // where only one group called loads was expected.
     shared static string boundary_group_for_loads = "loads";
+    shared static string[] group_names_for_loads = ["loads"];
     shared static bool write_loads = false;
     shared static bool compute_run_time_loads = false;
     shared static int run_time_loads_count = 100;
@@ -1549,6 +1552,10 @@ void read_config_file()
     mixin(update_bool("propagate_inflow_data", "propagate_inflow_data"));
     mixin(update_bool("save_intermediate_results", "save_intermediate_results"));
     mixin(update_string("boundary_group_for_loads", "boundary_group_for_loads"));
+    string[] group_names = GlobalConfig.boundary_group_for_loads.replace(" ","").split(",");
+    foreach (name; group_names) {
+        if (name.length > 0) { GlobalConfig.group_names_for_loads ~= name; }
+    }
     mixin(update_bool("write_loads", "write_loads"));
     mixin(update_bool("compute_run_time_loads", "compute_run_time_loads"));
     mixin(update_int("run_time_loads_count", "run_time_loads_count"));
@@ -1565,6 +1572,7 @@ void read_config_file()
         writeln("  propagate_inflow_data: ", GlobalConfig.propagate_inflow_data);
         writeln("  save_intermediate_results: ", GlobalConfig.save_intermediate_results);
         writeln("  boundary_group_for_loads: ", GlobalConfig.boundary_group_for_loads);
+        writeln("  group_names_for_loads: ", GlobalConfig.group_names_for_loads);
         writeln("  write_loads: ", GlobalConfig.write_loads);
         writeln("  thermionic_emission_bc_time_delay: ", GlobalConfig.thermionic_emission_bc_time_delay);
     }
