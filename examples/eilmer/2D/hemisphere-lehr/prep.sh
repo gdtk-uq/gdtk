3e4196a1205d57c26c8effb012da15ae8295d47e
Peter Jacobs <peterj@mech.uq.edu.au>
2020-03-08 10:31:48 +1000

    Eilmer examples: remove redundant Lehr example.
    The up-to-date example is in examples/eilmer/sphere-lehr/m355/

--- examples/eilmer/2D/hemisphere-lehr/prep.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-prep-gas Evans-Schexnayder-species.inp Evans-Schexnayder-gas-model.lua
-prep-chem Evans-Schexnayder-gas-model.lua Evans-Schexnayder-reactions.inp Evans-Schexnayder-chemistry.lua
-e4shared --job=lehr --prep
-
--- examples/eilmer/2D/hemisphere-lehr/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-e4shared --job=lehr --run
-
