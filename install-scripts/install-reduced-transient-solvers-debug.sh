5acc92f109886df091aa56e79dc0626949b7488c
Peter Jacobs <peterj@mech.uq.edu.au>
2020-03-27 13:18:31 +1000

    Fix message printed at end of build/install.

--- install-scripts/install-reduced-transient-solvers-debug.sh
+++ install-scripts/install-reduced-transient-solvers-debug.sh
@@ -8,7 +8,7 @@ cd ${DGD_REPO}/src/eilmer
 make clean
 if make DMD=ldmd2 FLAVOUR=debug WITH_MPI=1 MULTI_SPECIES_GAS=0 MULTI_T_GAS=0 MHD=0 TURBULENCE=0 install
 then
-    echo "Build and install successful for reduced code with FLAVOUR=fast."
+    echo "Build and install successful for reduced code with FLAVOUR=debug."
 else
-    echo "Build and install failed for reduced code with FLAVOUR=fast."
+    echo "Build and install failed for reduced code with FLAVOUR=debug."
 fi
