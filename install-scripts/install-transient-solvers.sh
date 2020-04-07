ad1e65cd641a45566e9e2d16b6259450bcca895a
Rowan J. Gollan <r.gollan@uq.edu.au>
2020-04-07 18:02:54 +1000

    install-scripts: Added "-i install_dir" option to install-transient-solvers.sh
    This also revealed a problem with the hard-coded DFLAGS in the ugrid_partition build.
    This has been fixed so that it should work for our usual selection of compilers.
    
    To set a non-standard install area do:
    
    > ./install-transient-solvers.sh -i /work/dgdinst

--- install-scripts/install-transient-solvers.sh
+++ install-scripts/install-transient-solvers.sh
@@ -4,12 +4,22 @@
 # build and install a fast flavour of the transient solver,
 # shared-memory and MPI variants.
 #
+# Look for a "-i /my/install/dir" if not standard $HOME/dgdinst
+INSTALL_DIR=${HOME}/dgdinst
+while getopts i: option
+do
+case "${option}"
+in
+i) INSTALL_DIR=${OPTARG};;
+esac
+done
+#
 cd ${DGD_REPO}/src/eilmer
 #
 # We want the debug build to be able to operate with the CEA-gas model
 # for the T4 shock tunnel example.
 # This build should be relatively quick.
-if make DMD=ldc2 FLAVOUR=debug install
+if make DMD=ldc2 FLAVOUR=debug INSTALL_DIR=${INSTALL_DIR} install
 then
     echo "Debug build successful"
     if mv ${DGD}/bin/e4shared ${DGD}/bin/e4shared-debug
@@ -22,7 +32,7 @@ fi
 #
 # The build of the "fast" code will take longer.
 make clean
-if make DMD=ldc2 FLAVOUR=fast WITH_MPI=1 install
+if make DMD=ldc2 FLAVOUR=fast WITH_MPI=1 INSTALL_DIR=${INSTALL_DIR} install
 then
     echo "Build and install successful for FLAVOUR=fast."
 else
