690e2cd2a5039526c2cfd796418ae0f72c3c7c7a
Peter Jacobs <peterj@mech.uq.edu.au>
2020-04-12 21:01:33 +1000

    Update run script for shock-fitted cylinder, MPI flavour.

--- examples/eilmer/2D/shock-fitting/cylinder-mpi/run-mpi.sh
+++ examples/eilmer/2D/shock-fitting/cylinder-mpi/run-mpi.sh
@@ -3,4 +3,4 @@
 prep-gas ideal-air.inp ideal-air-gas-model.lua
 e4shared --prep --job=cyl-sf-mpi
 mpirun -np 8 e4mpi --run --job=cyl-sf-mpi --verbosity=1
-e4shared --post --job=cyl-sf --tindx-plot=all --vtk-xml
+e4shared --post --job=cyl-sf-mpi --tindx-plot=all --vtk-xml
