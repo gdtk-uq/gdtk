4a3c3ffc26ff3bdfac2ff78591e47c3feec2aa4c
Fabian Zander <fabian.zander@usq.edu.au>
2020-04-17 08:58:04 +1000

    Added the TUSQ Mach 4 nozzle example using the Python imoc implementation

---
+++ examples/imoc/TUSQ-mach4-nozzle/TUSQ-Mach4-MOC.py
@@ -0,0 +1,100 @@
+# TUSQ-Mach4-MOC.py
+# A TUSQ Mach 4 nozzle design for the RDE work
+#
+# The isentropic nozzle design is undertaken using the method of Sivell
+# ("Aerodynamic Design of Axisymmetric Hypersonic Wind-Tunnel Nozzles",
+# James C. Sivells, J. Spacecraft, Vol. 7, No. 11, November, 1970).
+#
+# This method calculates an axial Mach number distribution along the nozzle
+# which forms the basis for the MOC calculation. This is a little bit
+# different to other common uses of MOC as it starts from the axis and works
+# 'up' to the nozzle wall (and beyond it). A sample solution for the centreline
+# Mach distribution is provided in the dat file. The other important number is
+# the node number at which the design Mach has been reached. In this case it is
+# node 27.
+#
+# The inputs to the analytical calculation of the centreline profile can be used
+# as a check for the final result. The centreline profile was calculated for a
+# Mach 4 nozzle with a throat radius of 20mm, an exit radius of 65mm and has a
+# computed length of 480mm.
+#
+# Fabian Zander
+# Tuesday 14th April, 2020
+# Covid Isolation, Boshammer St, Rangeville
+#
+import eilmer.imoc.kernel as kernel
+import eilmer.imoc.unit_process as unit
+from eilmer.ideal_gas_flow import PM1
+import numpy as np
+import matplotlib.pyplot as mplt
+# Import the centreline data, two columns with x and Mach
+centreline_data = np.loadtxt('machRDEnozzleMachProfile.dat')
+cl_x = centreline_data[:,0]
+cl_mach = centreline_data[:,1]
+# Set up the calculation parameters
+kernel.axisymmetric = True
+kernel.g = 1.4
+# First create and add all the centreline nodes
+for i in range(len(cl_x)):
+    kernel.Node(x=cl_x[i], y=0.0, nu=PM1(cl_mach[i]),
+                mach=cl_mach[i], theta=0.0)
+# Now add the interior nodes. As we are working up from the centreline at each
+# step up we lose one node (two nodes 'merge' into one). The max height of
+# 150mm was iteratively chosen to ensure the computed mesh covered the entire
+# region of interest
+top_node_count = len(cl_mach)
+while kernel.nodes[-1].y < 0.15 and top_node_count >= 2:
+    for i in range(len(kernel.nodes) - top_node_count, len(kernel.nodes) - 1):
+        unit.interior(i+1, i, -1)
+    top_node_count -= 1
+# Create our graphical mesh by adding all kernel nodes to mesh
+[kernel.mesh_nodes.append(node) for node in kernel.nodes]
+# Now, we know our Mach cone starts at node 27, we use this to calculate the
+# node which represents our nozzle exit. From this we can then use streamlines
+# to define our wall profile
+nozz_exit_node = 27
+# We are going to find the first node above the design radius of 65mm
+while kernel.nodes[nozz_exit_node].y < 0.065:
+    nozz_exit_node = kernel.nodes[nozz_exit_node].cplus_down
+# We can do a bit of a check here by looking at the Mach number and theta of
+# the computed node, these should be Mach 4.0 and 0.0
+print(f"The nozzle exit node (node {nozz_exit_node}) has a Mach number of "
+      + f"{kernel.nodes[nozz_exit_node].mach} and a theta of "
+      + f"{kernel.nodes[nozz_exit_node].theta:.5f}")
+# Now, we are going to create our nozzle wall by following the streamline
+# from the nozzle exit point.
+# We are going to step upstream in 5mm increments
+dL = -5.0e-3
+# We want to store our wall node indices for later reference
+nozz_wall_nodes = []
+nozz_wall_nodes.append(nozz_exit_node) # Nozzle exit node
+# I (FZ) haven't figured out a neater way of visually creating the nozzle
+# wall yet. Hence the global list creation.... PJ.... ideas??
+global wall_x
+global wall_y
+wall_x = []
+wall_y = []
+wall_x.append(kernel.nodes[nozz_exit_node].x)
+wall_y.append(kernel.nodes[nozz_exit_node].y)
+# Let's try starting a streamline......
+while kernel.nodes[nozz_wall_nodes[-1]].x > 0.01:
+    new_idx = unit.step_stream_node(nozz_wall_nodes[-1], -1, dL)
+    nozz_wall_nodes.append(new_idx)
+    kernel.mesh_nodes.append(kernel.nodes[new_idx])
+    wall_x.append(kernel.nodes[new_idx].x)
+    wall_y.append(kernel.nodes[new_idx].y)
+# Now, we can do a check of the throat radius and see that this matches what
+# we are expecting from our analytical solution, it should be ~20mm.
+# The value will probably be slightly higher due to the way we defined the end
+# of the nozzle wall solution above
+print(f"The throat radius is approximately "
+      + f"{kernel.nodes[nozz_wall_nodes[-1]].y*1000:.2f} mm.")
+# Let's define the axis as a wall for clarity
+def wall0(x): return 0.0
+# And the create the nozzle wall - at this point in time this appears to be
+# the best method for the wall creation
+def wall1(x):
+    import numpy as np
+    if x < 0.01 or x > 0.48: pass
+    return np.interp(x, np.flip(wall_x), np.flip(wall_y))
+kernel.walls = [wall0, wall1]
