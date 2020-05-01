655a260d5315fdfca8972364cdf40011f2dd462f
Rowan J. Gollan <r.gollan@uq.edu.au>
2020-05-01 13:44:37 +1000

    eilmer: Update mpirun command for OpenMPI 4 in vortex-subsonic case
    In the launcher for OpenMPI 4, you need to be explicit if some
    of the tasks are using hyperthreads as opposed to real cores.
    The command line option to add is:
      --use-hwthreads-cpus
    
    I suspect we'll need this option on other cases in the examples.
    We'll find them as they arise.

--- examples/eilmer/2D/vortex-subsonic/run.sh
+++ examples/eilmer/2D/vortex-subsonic/run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 # run.sh
 # e4shared --run --job=vortex --verbosity=1
-mpirun -np 4 e4mpi --run --job=vortex --verbosity=1
+
+# Test OpenMPI version. If 4.0 or later, we need to explicitly ask for hwthreads.
+OMPI_VER=$(ompi_info --parsable | grep ompi:version:full | awk -F: '{print $4}' | awk -F. '{print $1}')
+
+if [ $OMPI_VER -ge 4 ]
+then
+    mpirun --use-hwthread-cpus -np 4 e4mpi --run --job=vortex --verbosity=1
+else
+    mpirun -np 4 e4mpi --run --job=vortex --verbosity=1
+fi
