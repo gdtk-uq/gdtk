f75c5afde541a779ec3a716c1e85c226ce96c0f1
Rowan J. Gollan <r.gollan@uq.edu.au>
2020-05-27 00:40:16 +1000

    eilmer: Revert some unintended file changes from previous commit.

--- src/eilmer/onedinterp.d
+++ src/eilmer/onedinterp.d
@@ -6,8 +6,6 @@
 
 module onedinterp;
 
-import std.string;
-import core.stdc.stdlib : exit;
 import std.math;
 import std.stdio;
 import nm.complex;
@@ -334,26 +332,14 @@ public:
                 try {
                     scale_mass_fractions(Lft.gas.massf); 
                 } catch(Exception e) {
-                    debug {
-                        string msg = "Bad Lft reconstruction of mass fractions that do not add correctly.";
-                        msg ~= format("scale_mass_fraction exception with message:\n %s", e.msg);
-                        msg ~= format("The interface is located at: %s\n", IFace.pos);
-                        writeln(msg);
-                        exit(1);
-                    }
+                    debug { writeln(e.msg); }
                     Lft.gas.massf[] = gL0.massf[];
                 }
                 try {
                     scale_mass_fractions(Rght.gas.massf);
                 } catch(Exception e) {
-                    debug {
-                        string msg = "Bad Rght reconstruction of mass fractions that do not add correctly.";
-                        msg ~= format("scale_mass_fraction exception with message:\n %s", e.msg);
-                        msg ~= format("The interface is located at: %s\n", IFace.pos);
-                        writeln(msg);
-                    }
+                    debug { writeln(e.msg); }
                     Rght.gas.massf[] = gR0.massf[];
-                    exit(1);
                 }
             } else {
                 // Only one possible mass-fraction value for a single species.
