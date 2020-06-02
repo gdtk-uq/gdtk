ab98b5dcab4846f0ea2d3c3311d2985cacbd69f9
Kyle Damm <kyle.anthony.damm@gmail.com>
2020-06-02 14:45:19 +1000

    examples: modified the steady-state examples effected by previous commit (df5dcccab707).

--- examples/eilmer/2D/manufactured-solution/steady-state/ns/smoke-test/mms-ns-test.rb
+++ examples/eilmer/2D/manufactured-solution/steady-state/ns/smoke-test/mms-ns-test.rb
@@ -35,7 +35,7 @@ class TestMMS_NS < Test::Unit::TestCase
         steps = items[1].to_i
       end
     end
-    assert((steps - 120).abs < 3, "Failed to take correct number of steps.")
+    assert((steps - 126).abs < 3, "Failed to take correct number of steps.")
   end
 
   def test_2_check_norms
