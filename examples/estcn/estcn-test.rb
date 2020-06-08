7299fb7c681869c2790ade2ba57436e9abfa4ad5
Peter Jacobs <peterj@mech.uq.edu.au>
2020-06-08 16:15:56 +1000

    estcn: add a test that exercises the code for more than one species.

--- examples/estcn/estcn-test.rb
+++ examples/estcn/estcn-test.rb
@@ -7,7 +7,7 @@ require 'test/unit'
 require 'open3'
 
 class TestESTCN < Test::Unit::TestCase
-  def test_0_prep
+  def test_0_prepare_gas_models
     cmd = "cp #{ENV['DGD_REPO']}/src/gas/sample-data/ideal-air-gas-model.lua ."
     o, e, s = Open3.capture3(*cmd.split)
     assert_equal(s.success?, true)
@@ -18,6 +18,9 @@ class TestESTCN < Test::Unit::TestCase
     o, e, s = Open3.capture3(*cmd.split)
     assert_equal(s.success?, true)
     cmd = "cp #{ENV['DGD_REPO']}/src/gas/sample-data/cea-lut-air-version-test.lua ./cea-lut-air.lua"
+    o, e, s = Open3.capture3(*cmd.split)
+    assert_equal(s.success?, true)
+    cmd = "prep-gas he-n2-o2-mix.inp he-n2-o2-mix-gas-model.lua"
     o, e, s = Open3.capture3(*cmd.split)
     assert_equal(s.success?, true)
   end
@@ -92,7 +95,7 @@ class TestESTCN < Test::Unit::TestCase
     assert((velocity - 2949.81).abs/2949.81 < 1.0e-3, "Incorrect velocity.")
   end
 
-  def test_3_incident_cea2_gas
+  def test_3_incident_shock_cea2_gas
     cmd = "estcn --task=ishock --gas=cea-air13species-gas-model.lua --p1=59 --T1=283 --Vs=11296"
     o, e, s = Open3.capture3(*cmd.split)
     assert_equal(s.success?, true)
@@ -122,7 +125,7 @@ class TestESTCN < Test::Unit::TestCase
     assert((velocity - 10561.1).abs/10561.1 < 1.0e-3, "Incorrect velocity.")
   end
 
-  def test_4_incident_ideal_air_gas
+  def test_4_incident_shock_ideal_air_gas
     cmd = "estcn --task=ishock --gas=ideal-air-gas-model.lua --p1=59 --T1=283 --Vs=11296"
     o, e, s = Open3.capture3(*cmd.split)
     assert_equal(s.success?, true)
@@ -188,7 +191,7 @@ class TestESTCN < Test::Unit::TestCase
     assert((angle_deg - 21).abs/21 < 1.0e-3, "Incorrect shock angle.")
   end
 
-  def test_5_total_pressure
+  def test_7_total_pressure
     cmd = "estcn --gas=cea-lut-air.lua --task=total --p1=93.6e3 --T1=1284 --V1=2.95e3"
     o, e, s = Open3.capture3(*cmd.split)
     assert_equal(s.success?, true)
@@ -200,6 +203,36 @@ class TestESTCN < Test::Unit::TestCase
     temperature = temperature_item.split('=')[-1].to_f
     assert((pressure - 3.4273e+07).abs/3.4273e+07 < 1.0e-3, "Incorrect pressure.")
     assert((temperature - 4160.5).abs/4160.5 < 1.0e-3, "Incorrect temperature.")
+  end
+
+  def test_8_incident_shock_thermally_perfect_air_gas
+    cmd = "estcn --task=ishock --gas=he-n2-o2-mix-gas-model.lua --T1=300 --p1=125.0e3 --molef=0.0,0.79,0.21 --Vs=2414.0"
+    o, e, s = Open3.capture3(*cmd.split)
+    assert_equal(s.success?, true)
+    velocity = 0.0
+    found_state_2 = false
+    pressure = 0.0; temperature = 0.0
+    lines = o.split("\n")
+    lines.each do |txt|
+      if txt.match('Vg') then
+        items = txt.split(' ')
+        velocity = items[4].to_f
+      end
+      if found_state_2 && pressure == 0.0 then
+        items = txt.split(', ')
+        pressure_item = items[1]
+        pressure = pressure_item.split('=')[-1].to_f
+        temperature_item = items[2]
+        temperature = temperature_item.split('=')[-1].to_f
+      end
+      if txt.match('State 2') then
+        # Note that we want the data from the following line.
+        found_state_2 = true
+      end
+    end
+    assert((pressure - 7.8815e+06).abs/7.8815e+06 < 1.0e-3, "Incorrect pressure.")
+    assert((temperature - 2792.97).abs/2792.97 < 1.0e-3, "Incorrect temperature.")
+    assert((velocity - 2057.56).abs/2057.56 < 1.0e-3, "Incorrect velocity.")
   end
 
 end
