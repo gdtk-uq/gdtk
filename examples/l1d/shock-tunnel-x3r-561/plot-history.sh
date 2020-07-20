25de0c0a916e1e8b1924c02e27620b0bf4740c90
Peter Jacobs <peterj@mech.uq.edu.au>
2020-07-20 21:12:33 +1000

    L1d x3r-561 compression-tube simulation revisited.
    The code now does a better job of the high-order reconstruction and the
    comparison against the experimental data is better.

--- examples/l1d/shock-tunnel-x3r-561/plot-history.sh
+++ examples/l1d/shock-tunnel-x3r-561/plot-history.sh
@@ -10,7 +10,7 @@ set ylabel "p, MPa"
 set xrange [-0.20:0.50]
 set yrange [0:20.0]
 set key top right
-plot "x3r-561/history-loc-0001.data" using (\$1-0.1705):(\$5/1.0e6) title "L1d4 cells 600:300:50" with lines linestyle 1, \
+plot "x3r-561/history-loc-0001.data" using (\$1-0.1687):(\$5/1.0e6) title "L1d4 cells 600:300:50" with lines linestyle 1, \
      "exp-data/x3s561/x3s561.lvm" using (\$1-1.0522):(\$2/1000.0) title "x3s561 Experiment" with lines linestyle 2
 EOF
 
@@ -23,6 +23,6 @@ set ylabel "p, MPa"
 set xrange [-0.10:0.10]
 set yrange [0:20.0]
 set key top right
-plot "x3r-561/history-loc-0001.data" using (\$1-0.1705):(\$5/1.0e6) title "L1d4 cells 600:300:50" with lines linestyle 1, \
+plot "x3r-561/history-loc-0001.data" using (\$1-0.1687):(\$5/1.0e6) title "L1d4 cells 600:300:50" with lines linestyle 1, \
      "exp-data/x3s561/x3s561.lvm" using (\$1-1.0522):(\$2/1000.0) title "x3s561 Experiment" with lines linestyle 2
 EOF
