1f6a5573e5b51179964cc6fb20dd322df0b07727
Peter Jacobs <peterj@mech.uq.edu.au>
2020-09-02 20:27:43 +1000

    Update rotating-frame example scripts to emit the nonrotating-frame velocities.

--- examples/eilmer/3D/rotating-frame/uniform-flow/run.sh
+++ examples/eilmer/3D/rotating-frame/uniform-flow/run.sh
@@ -2,4 +2,4 @@
 # run.sh
 e4shared --run --job=annulus --verbosity=1
 e4shared --post --job=annulus --vtk-xml \
-	 --add-vars="mach,pitot,total-p,total-h"
+	 --add-vars="mach,pitot,total-p,total-h,nrf"
