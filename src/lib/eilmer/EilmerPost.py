841098fa32f2791902d4920af4d4e184217701e8
Lachlan Whyborn <lachlan.whyborn@uqconnect.edu.au>
2020-10-30 17:42:41 +1000

    Add a few functions to assist in post-processing in Python. Aim is to behave similarly to the currently available Lua post-processing. Will be built upon over time.

---
+++ src/lib/eilmer/EilmerPost.py
@@ -0,0 +1,225 @@
+import numpy as np
+import gzip as gz
+import re
+import os
+
+class flowState:
+
+    def __init__(self, data_list):
+
+        self.posx = data_list[0]
+        self.posy = data_list[1]
+        self.posz = data_list[2]
+        self.vol = data_list[3]
+        self.rho = data_list[4]
+        self.velx = data_list[5]
+        self.vely = data_list[6]
+        self.velz = data_list[7]
+        self.p = data_list[8]
+        self.a = data_list[9]
+        self.mu = data_list[10]
+        self.k = data_list[11]
+        self.mu_t = data_list[12]
+        self.k_t = data_list[13]
+        self.S = data_list[14]
+        self.massf = data_list[15]
+        self.u = data_list[16]
+        self.T = data_list[17]
+
+def get_block_size(jobName, tstep, block_id):
+
+    """
+    get_block_size(jobName, tstep, block_id):
+    Gets the dimensions of the specified data block
+    jobName: name of job as string
+    tstep: index of time file as integer
+    block_id: id of block of interest as integer
+    returns ni, nj, nk (dimensions of the data block in terms of number of cells)
+    """
+
+    # Build the file name
+    if 100 <= block_id < 1000:
+        block_str = "0" + str(block_id)
+    elif 10 <= block_id < 100:
+        block_str = "00" + str(block_id)
+    elif 0 <= block_id < 10:
+        block_str = "000" + str(block_id)
+    else:
+        print("Block ID out of range")
+
+    if 100 <= tstep < 1000:
+        time_str = "0" + str(tstep)
+    elif 10 <= tstep < 100:
+        time_str = "00" + str(tstep)
+    elif 0 <= tstep < 10:
+        time_str = "000" + str(tstep)
+    else:
+        print("Time ID out of range")
+    
+    filename = "flow/t" + time_str + "/" + jobName + ".flow.b" + block_str + ".t" + time_str + ".gz"
+    f = gz.open(filename, "rt")
+
+    # Check that the desired file exists
+    if not os.path.isfile(filename):
+        raise IOError ("File does not exist- check job name, time step and block_id")
+
+    # Pull out information about the data block
+    inc = 0
+    for line in f:
+        
+        inc += 1
+
+        if inc == 3:
+            simTime = float(line.split()[1])
+        if inc == 7:
+            ni = int(line.split()[1])
+        if inc == 8:
+            nj = int(line.split()[1])
+        if inc == 9:
+            nk = int(line.split()[1])
+
+    # No useful information past here- stop searching
+        if inc == 10:
+            break
+
+    f.close()
+
+    return ni, nj, nk
+
+def get_cell_data(jobName, tstep, block_id, i, j = 0, k = 0, directory = "."):
+
+    """
+    get_cell_data(jobName, tstep, block_id, i, j, k = 0):
+    Get the full set of cell data from a given cell
+    jobName: your jobname as a string
+    tstep: index of time file as integer
+    block_id: data block containing cell of interest as integer
+    i, j, k: index of cell in the block
+
+    Returns: [x, y, z, volume, density, v_x, v_y, v_z, p, a, mu, k, mu_t, k_t, S, massf, u (int energy), T], simTime
+    """
+
+    # Build the file name
+    if 100 <= block_id < 1000:
+        block_str = "0" + str(block_id)
+    elif 10 <= block_id < 100:
+        block_str = "00" + str(block_id)
+    elif 0 <= block_id < 10:
+        block_str = "000" + str(block_id)
+    else:
+        print("Block ID out of range")
+
+    if 100 <= tstep < 1000:
+        time_str = "0" + str(tstep)
+    elif 10 <= tstep < 100:
+        time_str = "00" + str(tstep)
+    elif 0 <= tstep < 10:
+        time_str = "000" + str(tstep)
+    else:
+        print("Time ID out of range")
+    
+    filename = directory + "/flow/t" + time_str + "/" + jobName + ".flow.b" + block_str + ".t" + time_str + ".gz"
+    f = gz.open(filename, "rt")
+
+    # Check that the desired file exists
+    if not os.path.isfile(filename):
+        raise IOError ("File does not exist- check job name, time step and block_id")
+
+    # Pull out information about the data block
+    inc = 0
+    for line in f:
+        
+        inc += 1
+
+        if inc == 3:
+            simTime = float(line.split()[1])
+        if inc == 7:
+            ni = int(line.split()[1])
+        if inc == 8:
+            nj = int(line.split()[1])
+        if inc == 9:
+            nk = int(line.split()[1])
+            break
+
+    # Now we know the line number we're looking for
+    line_number = 10 + nk * k + nj * j + i
+
+    #Return the pointer to the start of the file
+    f.seek(0)
+
+    # Pull out the data from the selected line- order of variables is: x, y, z, volume, rho, vx, vy, vz, p, a, mu, k, mu_t, k_t, S, massf, u, T
+    inc = 0
+    cell_data = np.empty(18, dtype = float)
+
+    for line in f:
+
+        inc += 1
+
+        if inc == line_number:
+            for indx, element in enumerate(line.split()):
+                cell_data[indx] = float(element)
+
+    f.close()
+        
+    return flowState(cell_data), simTime
+
+def get_block_as_array(jobName, tstep, block_id):
+
+    # Build the file name
+    if 100 <= block_id < 1000:
+        block_str = "0" + str(block_id)
+    elif 10 <= block_id < 100:
+        block_str = "00" + str(block_id)
+    elif 0 <= block_id < 10:
+        block_str = "000" + str(block_id)
+    else:
+        print("Block ID out of range")
+
+    if 100 <= tstep < 1000:
+        time_str = "0" + str(tstep)
+    elif 10 <= tstep < 100:
+        time_str = "00" + str(tstep)
+    elif 0 <= tstep < 10:
+        time_str = "000" + str(tstep)
+    else:
+        print("Time ID out of range")
+    
+    filename = "flow/t" + time_str + "/" + jobName + ".flow.b" + block_str + ".t" + time_str + ".gz"
+    f = gz.open(filename, "rt")
+
+    # Check that the desired file exists
+    if not os.path.isfile(filename):
+        raise IOError ("File does not exist- check job name, time step and block_id")
+
+    # Pull out information about the data block
+    inc = 0
+    for line in f:
+        
+        inc += 1
+    
+        if inc == 3:
+            simTime = float(line.split()[1])
+        if inc == 7:
+            ni = int(line.split()[1])
+        if inc == 8:
+            nj = int(line.split()[1])
+        if inc == 9:
+            nk = int(line.split()[1])
+            break
+
+    data_array = np.empty((ni * nj * nk, 18), dtype = np.double)
+
+    f.seek(0)
+
+    inc = 0
+    for line in f:
+
+        inc += 1
+
+        if inc >= 10:
+            for indx, element in enumerate(line.split()):
+                data_array[inc - 10, indx] = np.double(element)
+
+    f.close()
+
+    return data_array
