b45a985211914a71f6dc2736caf887baff16ddf8
Whyborn <lachlan.whyborn@uqconnect.edu.au>
2020-11-04 00:55:27 +1000

    Added get_vertex function to the python postprocessing suite

--- src/lib/eilmer/EilmerPost.py
+++ src/lib/eilmer/EilmerPost.py
@@ -26,7 +26,7 @@ class flowState:
         self.u = data_list[16]
         self.T = data_list[17]
 
-def get_block_size(jobName, tstep, block_id):
+def get_block_size(jobName, tstep, block_id, directory = "."):
 
     """
     get_block_size(jobName, tstep, block_id):
@@ -56,7 +56,7 @@ def get_block_size(jobName, tstep, block_id):
     else:
         raise ValueError ("Time ID out of range")
     
-    filename = "flow/t" + time_str + "/" + jobName + ".flow.b" + block_str + ".t" + time_str + ".gz"
+    filename = directory + "/flow/t" + time_str + "/" + jobName + ".flow.b" + block_str + ".t" + time_str + ".gz"
     f = gz.open(filename, "rt")
 
     # Check that the desired file exists
@@ -89,7 +89,7 @@ def get_block_size(jobName, tstep, block_id):
 def get_cell_data(jobName, tstep, block_id, i, j = 0, k = 0, directory = "."):
 
     """
-    get_cell_data(jobName, tstep, block_id, i, j, k = 0):
+    get_cell_data(jobName, tstep, block_id, i, j = 0, k = 0):
     Get the full set of cell data from a given cell
     jobName: your jobname as a string
     tstep: index of time file as integer
@@ -107,7 +107,7 @@ def get_cell_data(jobName, tstep, block_id, i, j = 0, k = 0, directory = "."):
     elif 0 <= block_id < 10:
         block_str = "000" + str(block_id)
     else:
-        print("Block ID out of range")
+        raise ValueError ("Block ID out of range")
 
     if 100 <= tstep < 1000:
         time_str = "0" + str(tstep)
@@ -116,7 +116,7 @@ def get_cell_data(jobName, tstep, block_id, i, j = 0, k = 0, directory = "."):
     elif 0 <= tstep < 10:
         time_str = "000" + str(tstep)
     else:
-        print("Time ID out of range")
+        raise ValueError ("Time ID out of range")
     
     filename = directory + "/flow/t" + time_str + "/" + jobName + ".flow.b" + block_str + ".t" + time_str + ".gz"
     f = gz.open(filename, "rt")
@@ -152,10 +152,8 @@ def get_cell_data(jobName, tstep, block_id, i, j = 0, k = 0, directory = "."):
     # Now we know the line number we're looking for
     line_number = 10 + (ni + nj) * k + ni * j + i
 
-    #Return the pointer to the start of the file
-    f.seek(0)
-
     # Pull out the data from the selected line- order of variables is: x, y, z, volume, rho, vx, vy, vz, p, a, mu, k, mu_t, k_t, S, massf, u, T
+    f.seek(0)
     inc = 0
     cell_data = np.empty(18, dtype = float)
 
@@ -170,6 +168,92 @@ def get_cell_data(jobName, tstep, block_id, i, j = 0, k = 0, directory = "."):
     f.close()
         
     return flowState(cell_data), simTime
+
+def get_vertex(jobName, block_id, i, j = 0, k = 0, tstep = 0, directory = "."):
+
+    """
+    get_vertex(jobName, block_id, i, j = 0, k = 0, tstep = 0, directory = ".")
+    Gets the coordinates of a given vertex in the block
+    jobName: your jobName as a string
+    block_id: local block id to search in
+    i, j = 0, k = 0: indices of the vertex
+    tstep = 0: index of the flow solution, only relevant for moving grid simulations
+    directory = ".": directory of the job files
+
+    Returns x, y, z: coordinates of the vertex
+    """
+
+    # Build the file name
+    if 100 <= block_id < 1000:
+        block_str = "0" + str(block_id)
+    elif 10 <= block_id < 100:
+        block_str = "00" + str(block_id)
+    elif 0 <= block_id < 10:
+        block_str = "000" + str(block_id)
+    else:
+        raise ValueError ("Block ID out of range")
+
+    if 100 <= tstep < 1000:
+        time_str = "0" + str(tstep)
+    elif 10 <= tstep < 100:
+        time_str = "00" + str(tstep)
+    elif 0 <= tstep < 10:
+        time_str = "000" + str(tstep)
+    else:
+        raise ValueError ("Time ID out of range")
+    
+    filename = directory + "/grid/t" + time_str + "/" + jobName + ".grid.b" + block_str + ".t" + time_str + ".gz"
+    f = gz.open(filename, "rt")
+
+    # Check that the desired file exists
+    if not os.path.isfile(filename):
+        raise IOError ("File does not exist- check job name, time step and block_id")
+
+    # Pull out information about the block
+    inc = 0
+    for line in f:
+
+        inc += 1
+
+        if inc == 4:
+            ni = int(line.split()[1])
+        if inc == 5:
+            nj = int(line.split()[1])
+        if inc == 6:
+            nk = int(line.split()[1])
+            break
+
+    # Check the inputs
+    if i >= (ni * nj * nk):
+        raise ValueError ("i index out of range")
+    if j >= nj:
+        raise ValueError ("j index out of range")
+    if k >= nk:
+        raise ValueError ("k index out of range")
+
+    # What line number are we looking for?
+    line_number = 7 + (ni * nj) * nk + ni * j + i
+
+    # Pull out the information about the vertex
+    f.seek(0)
+    inc = 0
+    vector = np.empty(3, dtype = float)
+
+    for line in f:
+
+        inc += 1
+
+        if line_number == inc:
+            for indx, element in enumerate(line.split()):
+                vector[indx] = float(element)
+
+    f.close()
+
+    return vector
+
+
+
+
 
 if __name__ == "__main__":
 
