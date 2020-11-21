aa8180fde60081643199cc5a0ea1a0829d852e70
Peter Jacobs <peterj@mech.uq.edu.au>
2020-11-21 11:15:16 +1000

    Structured-grid rework: implement linear extrapolation for flow states.
    This linear extrapolation is used in the mms-euler-test case.

--- src/eilmer/bc/ghost_cell_effect/extrapolate_copy.d
+++ src/eilmer/bc/ghost_cell_effect/extrapolate_copy.d
@@ -81,138 +81,83 @@ public:
     @nogc
     override void apply_structured_grid(double t, int gtl, int ftl)
     {
-        // Fill ghost cells with data from just inside the boundary
-        // using zero-order extrapolation (i.e. just copy the data).
-        // We assume that this boundary is an outflow boundary.
-        FVCell src_cell, dest_cell;
         auto blk = cast(SFluidBlock) this.blk;
         assert(blk !is null, "Oops, this should be an SFluidBlock object.");
         BoundaryCondition bc = blk.bc[which_boundary];
         if (xOrder == 0) {
+            // Fill ghost cells with data from just inside the boundary
+            // using zero-order extrapolation (i.e. just copy the data).
+            // We assume that this boundary is an outflow boundary.
             foreach (i, f; bc.faces) {
                 foreach (n; 0 .. blk.n_ghost_cell_layers) {
+                    FVCell src_cell, dest_cell;
                     if (bc.outsigns[i] == 1) {
-                        src_cell = f.left_cells[n];
+                        src_cell = f.left_cells[0];
                         dest_cell = f.right_cells[n];
                     } else {
-                        src_cell = f.right_cells[n];
+                        src_cell = f.right_cells[0];
                         dest_cell = f.left_cells[n];
                     }
-                    dest_cell.copy_values_from(src_cell, CopyDataOption.minimal_flow);
+                    dest_cell.fs.copy_values_from(src_cell.fs);
                 }
             }
         } else {
-            throw new Error("FIX-ME PJ 2020-11-18 Linear extrapolation not implemented.");
-        }
-/+
-// FIX-ME PJ 2020-11-18 Need to implement linear-extrapolation, at least.
-// Have just kept one example for later rework.
-
-        size_t i, j, k;
-        bool nghost3 = (blk.n_ghost_cell_layers == 3);
-        FVCell cell_1, cell_2;
-        auto gmodel = blk.myConfig.gmodel;
-        size_t nsp = blk.myConfig.n_species;
-        size_t nmodes = blk.myConfig.n_modes;
-        size_t nturb = blk.myConfig.turb_model.nturb;
-
-        final switch (which_boundary) {
-        case Face.north:
-            j = blk.jmax;
-            for (k = blk.kmin; k <= blk.kmax; ++k) {
-                for (i = blk.imin; i <= blk.imax; ++i) {
-                    if (xOrder == 1) {
-                        //  |--- [2] ---|--- [1] ---|||--- [dest] ---|---[ghost cell 2]----
-                        //      (j-1)        (j)           (j+1)
-                        //  dest: ghost cell 1
-                        //  [1]: first interior cell
-                        //  [2]: second interior cell
-                        // This extrapolation assumes that cell-spacing between
-                        // cells 1 and 2 continues on in the exterior
-                        cell_1 = blk.get_cell(i,j,k);
-                        cell_2 = blk.get_cell(i,j-1,k);
-                        dest_cell = blk.get_cell(i,j+1,k);
-                        // Extrapolate on primitive variables
-                        // 1. First exterior point
-                        dest_cell.fs.gas.rho = 2.0*cell_1.fs.gas.rho - cell_2.fs.gas.rho;
-                        dest_cell.fs.gas.u = 2.0*cell_1.fs.gas.u - cell_2.fs.gas.u;
-                        version(multi_T_gas) {
-                            for ( size_t imode = 0; imode < nmodes; ++imode ) {
-                                dest_cell.fs.gas.u_modes[imode] = 2.0*cell_1.fs.gas.u_modes[imode] - cell_2.fs.gas.u_modes[imode];
-                            }
-                        }
-                        version(multi_species_gas) {
-                            if (nsp > 1) {
-                                for ( size_t isp = 0; isp < nsp; ++isp ) {
-                                    dest_cell.fs.gas.massf[isp] = 2.0*cell_1.fs.gas.massf[isp] - cell_2.fs.gas.massf[isp];
-                                }
-                                scale_mass_fractions(dest_cell.fs.gas.massf);
-                            } else {
-                                dest_cell.fs.gas.massf[0] = 1.0;
-                            }
-                        }
-                        gmodel.update_thermo_from_rhou(dest_cell.fs.gas);
-                        dest_cell.fs.vel.refx = 2.0*cell_1.fs.vel.x - cell_2.fs.vel.x;
-                        dest_cell.fs.vel.refy = 2.0*cell_1.fs.vel.y - cell_2.fs.vel.y;
-                        dest_cell.fs.vel.refz = 2.0*cell_1.fs.vel.z - cell_2.fs.vel.z;
-                        version(MHD) {
-                            dest_cell.fs.B.refx = 2.0*cell_1.fs.B.x - cell_2.fs.B.x;
-                            dest_cell.fs.B.refy = 2.0*cell_1.fs.B.y - cell_2.fs.B.y;
-                            dest_cell.fs.B.refz = 2.0*cell_1.fs.B.z - cell_2.fs.B.z;
-                        }
-                        version(turbulence) {
-                            foreach(it; 0 .. nturb) {
-                                dest_cell.fs.turb[it] = 2.0*cell_1.fs.turb[it] - cell_2.fs.turb[it];
-                            }
-                        }
-                        dest_cell.fs.mu_t = 2.0*cell_1.fs.mu_t - cell_2.fs.mu_t;
-                        dest_cell.fs.k_t = 2.0*cell_1.fs.k_t - cell_2.fs.k_t;
-                        // 2. Second exterior point
-                        //  |---[2]---|||---[1]---|---[dest]------
-                        //      (j)        (j+1)       (j+2)
-                        cell_2 = cell_1;
-                        cell_1 = dest_cell;
-                        dest_cell = blk.get_cell(i,j+2,k);
-                        dest_cell.fs.gas.rho = 2.0*cell_1.fs.gas.rho - cell_2.fs.gas.rho;
-                        dest_cell.fs.gas.u = 2.0*cell_1.fs.gas.u - cell_2.fs.gas.u;
-                        version(multi_T_gas) {
-                            for ( size_t imode = 0; imode < nmodes; ++imode ) {
-                                dest_cell.fs.gas.u_modes[imode] = 2.0*cell_1.fs.gas.u_modes[imode] - cell_2.fs.gas.u_modes[imode];
-                            }
-                        }
-                        version(multi_species_gas) {
-                            if (nsp > 1) {
-                                for ( size_t isp = 0; isp < nsp; ++isp ) {
-                                    dest_cell.fs.gas.massf[isp] = 2.0*cell_1.fs.gas.massf[isp] - cell_2.fs.gas.massf[isp];
-                                }
-                                scale_mass_fractions(dest_cell.fs.gas.massf);
-                            } else {
-                                dest_cell.fs.gas.massf[0] = 1.0;
-                            }
-                        }
-                        gmodel.update_thermo_from_rhou(dest_cell.fs.gas);
-                        dest_cell.fs.vel.refx = 2.0*cell_1.fs.vel.x - cell_2.fs.vel.x;
-                        dest_cell.fs.vel.refy = 2.0*cell_1.fs.vel.y - cell_2.fs.vel.y;
-                        dest_cell.fs.vel.refz = 2.0*cell_1.fs.vel.z - cell_2.fs.vel.z;
-                        version(MHD) {
-                            dest_cell.fs.B.refx = 2.0*cell_1.fs.B.x - cell_2.fs.B.x;
-                            dest_cell.fs.B.refy = 2.0*cell_1.fs.B.y - cell_2.fs.B.y;
-                            dest_cell.fs.B.refz = 2.0*cell_1.fs.B.z - cell_2.fs.B.z;
-                        }
-                        version(turbulence) {
-                            foreach(it; 0 .. nturb) {
-                                dest_cell.fs.turb[it] = 2.0*cell_1.fs.turb[it] - cell_2.fs.turb[it];
-                            }
-                        }
-                        dest_cell.fs.mu_t = 2.0*cell_1.fs.mu_t - cell_2.fs.mu_t;
-                        dest_cell.fs.k_t = 2.0*cell_1.fs.k_t - cell_2.fs.k_t;
-                        if (nghost3) {
-                            // FIX-ME just a fudge for now, PJ 2019-09-28
-                            dest_cell = blk.get_cell(i,j+3,k);
-                            src_cell = blk.get_cell(i,j+2,k);
-                            dest_cell.copy_values_from(src_cell, CopyDataOption.minimal_flow);
-                        }
-+/
+            // Extrapolate FlowState (presumably) from cells 0 and 1 into a destination state in cell 2.
+            //    |---c0---|---c1---|---c2---|
+            // This extrapolation assumes that cell-spacing is uniform.
+            //
+            // Helper function
+            void linearly_extrapolate_flowstate(ref FlowState fs0, ref FlowState fs1, ref FlowState fs2)
+            {
+                auto gmodel = blk.myConfig.gmodel;
+                // Extrapolate on primitive variables
+                fs2.gas.rho = 2.0*fs1.gas.rho - fs0.gas.rho;
+                fs2.gas.u = 2.0*fs1.gas.u - fs0.gas.u;
+                version(multi_T_gas) {
+                    size_t nmodes = blk.myConfig.n_modes;
+                    foreach (i; 0 .. nmodes) { fs2.gas.u_modes[i] = 2.0*fs1.gas.u_modes[i] - fs0.gas.u_modes[i]; }
+                }
+                version(multi_species_gas) {
+                    size_t nsp = blk.myConfig.n_species;
+                    if (nsp > 1) {
+                        foreach (i; 0 .. nsp) { fs2.gas.massf[i] = 2.0*fs1.gas.massf[i] - fs0.gas.massf[i]; }
+                        scale_mass_fractions(fs2.gas.massf);
+                    } else {
+                        fs2.gas.massf[0] = 1.0;
+                    }
+                }
+                gmodel.update_thermo_from_rhou(fs2.gas);
+                fs2.vel.set(2.0*fs1.vel.x-fs0.vel.x, 2.0*fs1.vel.y-fs0.vel.y, 2.0*fs1.vel.z-fs0.vel.z) ;
+                version(MHD) {
+                    fs2.B.set(2.0*fs1.B.x-fs0.B.x, 2.0*fs1.B.y-fs0.B.y, 2.0*fs1.B.z-fs0.B.z);
+                }
+                version(turbulence) {
+                    size_t nturb = blk.myConfig.turb_model.nturb;
+                    foreach(it; 0 .. nturb) { fs2.turb[it] = 2.0*fs1.turb[it] - fs0.turb[it]; }
+                }
+                fs2.mu_t = 2.0*fs1.mu_t - fs0.mu_t;
+                fs2.k_t = 2.0*fs1.k_t - fs0.k_t;
+            } // end linearly_extrapolate_flow()
+            //
+            // Apply the helper function.
+            foreach (i, f; bc.faces) {
+                if (blk.n_ghost_cell_layers > 0) {
+                    FVCell c0, c1, c2;
+                    if (bc.outsigns[i] == 1) {
+                        c0 = f.left_cells[1]; c1 = f.left_cells[0]; c2 = f.right_cells[0];
+                    } else {
+                        c0 = f.right_cells[1]; c1 = f.right_cells[0]; c2 = f.left_cells[0];
+                    }
+                    linearly_extrapolate_flowstate(c0.fs, c1.fs, c2.fs);
+                    foreach (n; 1 .. blk.n_ghost_cell_layers) {
+                        // Shuffle along and do next cell.
+                        c0 = c1; c1 = c2;
+                        c2 = (bc.outsigns[i] == 1) ? f.right_cells[n] : f.left_cells[n];
+                        linearly_extrapolate_flowstate(c0.fs, c1.fs, c2.fs);
+                    }
+                } // end if blk.n_ghost_cell_layers > 0
+            } // end foreach
+        } // end else
 
     } // end apply_structured_grid()
 } // end class GhostCellExtrapolateCopy
