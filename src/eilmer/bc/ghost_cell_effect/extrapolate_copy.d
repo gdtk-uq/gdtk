9dfaf68753e3a4ca58d46ae66f66fa03860d78bc
Peter Jacobs <peterj@mech.uq.edu.au>
2020-11-21 11:38:46 +1000

    Structured-grid rework: tidy the code around the extrapolate function.

--- src/eilmer/bc/ghost_cell_effect/extrapolate_copy.d
+++ src/eilmer/bc/ghost_cell_effect/extrapolate_copy.d
@@ -105,59 +105,57 @@ public:
             // Extrapolate FlowState (presumably) from cells 0 and 1 into a destination state in cell 2.
             //    |---c0---|---c1---|---c2---|
             // This extrapolation assumes that cell-spacing is uniform.
-            //
-            // Helper function
-            void linearly_extrapolate_flowstate(ref FlowState fs0, ref FlowState fs1, ref FlowState fs2)
-            {
-                auto gmodel = blk.myConfig.gmodel;
-                // Extrapolate on primitive variables
-                fs2.gas.rho = 2.0*fs1.gas.rho - fs0.gas.rho;
-                fs2.gas.u = 2.0*fs1.gas.u - fs0.gas.u;
-                version(multi_T_gas) {
-                    size_t nmodes = blk.myConfig.n_modes;
-                    foreach (i; 0 .. nmodes) { fs2.gas.u_modes[i] = 2.0*fs1.gas.u_modes[i] - fs0.gas.u_modes[i]; }
-                }
-                version(multi_species_gas) {
-                    size_t nsp = blk.myConfig.n_species;
-                    if (nsp > 1) {
-                        foreach (i; 0 .. nsp) { fs2.gas.massf[i] = 2.0*fs1.gas.massf[i] - fs0.gas.massf[i]; }
-                        scale_mass_fractions(fs2.gas.massf);
-                    } else {
-                        fs2.gas.massf[0] = 1.0;
-                    }
-                }
-                gmodel.update_thermo_from_rhou(fs2.gas);
-                fs2.vel.set(2.0*fs1.vel.x-fs0.vel.x, 2.0*fs1.vel.y-fs0.vel.y, 2.0*fs1.vel.z-fs0.vel.z) ;
-                version(MHD) {
-                    fs2.B.set(2.0*fs1.B.x-fs0.B.x, 2.0*fs1.B.y-fs0.B.y, 2.0*fs1.B.z-fs0.B.z);
-                }
-                version(turbulence) {
-                    size_t nturb = blk.myConfig.turb_model.nturb;
-                    foreach(it; 0 .. nturb) { fs2.turb[it] = 2.0*fs1.turb[it] - fs0.turb[it]; }
-                }
-                fs2.mu_t = 2.0*fs1.mu_t - fs0.mu_t;
-                fs2.k_t = 2.0*fs1.k_t - fs0.k_t;
-            } // end linearly_extrapolate_flow()
-            //
-            // Apply the helper function.
             foreach (i, f; bc.faces) {
-                if (blk.n_ghost_cell_layers > 0) {
-                    FVCell c0, c1, c2;
-                    if (bc.outsigns[i] == 1) {
-                        c0 = f.left_cells[1]; c1 = f.left_cells[0]; c2 = f.right_cells[0];
-                    } else {
-                        c0 = f.right_cells[1]; c1 = f.right_cells[0]; c2 = f.left_cells[0];
-                    }
+                if (blk.n_ghost_cell_layers == 0) continue;
+                FVCell c0, c1, c2;
+                if (bc.outsigns[i] == 1) {
+                    c0 = f.left_cells[1]; c1 = f.left_cells[0]; c2 = f.right_cells[0];
+                } else {
+                    c0 = f.right_cells[1]; c1 = f.right_cells[0]; c2 = f.left_cells[0];
+                }
+                linearly_extrapolate_flowstate(c0.fs, c1.fs, c2.fs);
+                foreach (n; 1 .. blk.n_ghost_cell_layers) {
+                    // Shuffle along and do next cell.
+                    c0 = c1; c1 = c2;
+                    c2 = (bc.outsigns[i] == 1) ? f.right_cells[n] : f.left_cells[n];
                     linearly_extrapolate_flowstate(c0.fs, c1.fs, c2.fs);
-                    foreach (n; 1 .. blk.n_ghost_cell_layers) {
-                        // Shuffle along and do next cell.
-                        c0 = c1; c1 = c2;
-                        c2 = (bc.outsigns[i] == 1) ? f.right_cells[n] : f.left_cells[n];
-                        linearly_extrapolate_flowstate(c0.fs, c1.fs, c2.fs);
-                    }
-                } // end if blk.n_ghost_cell_layers > 0
-            } // end foreach
+                }
+            } // end foreach face
         } // end else
-
     } // end apply_structured_grid()
+
+private:
+    @nogc
+    void linearly_extrapolate_flowstate(ref FlowState fs0, ref FlowState fs1, ref FlowState fs2)
+    {
+        auto gmodel = blk.myConfig.gmodel;
+        // Extrapolate on primitive variables
+        fs2.gas.rho = 2.0*fs1.gas.rho - fs0.gas.rho;
+        fs2.gas.u = 2.0*fs1.gas.u - fs0.gas.u;
+        version(multi_T_gas) {
+            size_t nmodes = blk.myConfig.n_modes;
+            foreach (i; 0 .. nmodes) { fs2.gas.u_modes[i] = 2.0*fs1.gas.u_modes[i] - fs0.gas.u_modes[i]; }
+        }
+        version(multi_species_gas) {
+            size_t nsp = blk.myConfig.n_species;
+            if (nsp > 1) {
+                foreach (i; 0 .. nsp) { fs2.gas.massf[i] = 2.0*fs1.gas.massf[i] - fs0.gas.massf[i]; }
+                scale_mass_fractions(fs2.gas.massf);
+            } else {
+                fs2.gas.massf[0] = 1.0;
+            }
+        }
+        gmodel.update_thermo_from_rhou(fs2.gas);
+        fs2.vel.set(2.0*fs1.vel.x-fs0.vel.x, 2.0*fs1.vel.y-fs0.vel.y, 2.0*fs1.vel.z-fs0.vel.z) ;
+        version(MHD) {
+            fs2.B.set(2.0*fs1.B.x-fs0.B.x, 2.0*fs1.B.y-fs0.B.y, 2.0*fs1.B.z-fs0.B.z);
+        }
+        version(turbulence) {
+            size_t nturb = blk.myConfig.turb_model.nturb;
+            foreach(it; 0 .. nturb) { fs2.turb[it] = 2.0*fs1.turb[it] - fs0.turb[it]; }
+        }
+        fs2.mu_t = 2.0*fs1.mu_t - fs0.mu_t;
+        fs2.k_t = 2.0*fs1.k_t - fs0.k_t;
+    } // end linearly_extrapolate_flow()
+
 } // end class GhostCellExtrapolateCopy
