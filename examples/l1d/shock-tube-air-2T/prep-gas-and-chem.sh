3320abe998be07822096da0d24d9b861e558e2f5
Peter Jacobs <peterj@mech.uq.edu.au>
2020-12-08 10:45:03 +1000

    l1d 2T example now gets its gas files from examples/kinetics/.

--- examples/l1d/shock-tube-air-2T/prep-gas-and-chem.sh
+++ examples/l1d/shock-tube-air-2T/prep-gas-and-chem.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # prep-gas-and-chem.sh
 cp ${DGD_REPO}/src/gas/sample-data/cea-air11species-gas-model.lua .
+cp ${DGD_REPO}/examples/kinetics/air-chemistry-2T/air-11sp-2T.inp .
+cp ${DGD_REPO}/examples/kinetics/air-chemistry-2T/GuptaEtAl-air-2T.lua .
+sed -i '/^NO_SPECIES =/s/5/11/' GuptaEtAl-air-2T.lua
+cp ${DGD_REPO}/examples/kinetics/air-chemistry-2T/air-energy-exchange.lua .
 prep-gas ideal-helium.inp ideal-helium-gas-model.lua
 prep-gas air-11sp-2T.inp air-11sp-2T.lua
 prep-chem air-11sp-2T.lua GuptaEtAl-air-2T.lua air-11sp-2T-chem.lua
