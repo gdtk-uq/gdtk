a60b89a8dff6d70e5fd29545e3144a10f26bb3b5
Whyborn <lachlan.whyborn@uqconnect.edu.au>
2021-02-26 17:16:30 +1000

    Eilmer: Fixed minor error in commit 0cd4a9c

--- src/eilmer/globalconfig.d
+++ src/eilmer/globalconfig.d
@@ -2101,12 +2101,12 @@ void configCheckPoint4()
     version(shape_sensitivity) {
         GlobalConfig.n_grid_time_levels = 3;
     }
-    return;
 
     if (GlobalConfig.do_temporal_DFT &&
             (GlobalConfig.DFT_n_modes * GlobalConfig.DFT_step_interval != GlobalConfig.max_step)) {
         writeln("Warning: config.DFT_n_modes * config.DFT_step_interval should equal config.max_step");
     }
+    return;
 }
 
 void checkGlobalConfig()
