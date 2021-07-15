57d690ae6ae53d0330c43e02bafe6c9c3302551e
Peter Jacobs <peterj@mech.uq.edu.au>
2021-07-15 19:22:04 +1000

    Eilmer: change to using implicit updates for the cylinder-with-axial-flow example.
    Putting the cfl value up to 50 gives us (nearly) a factor of 100 decrease
    in the number of steps needed.

--- examples/eilmer/2D/cylinder-axial-flow/sg/cyl50.lua
+++ examples/eilmer/2D/cylinder-axial-flow/sg/cyl50.lua
@@ -2,6 +2,7 @@
 -- PJ, updated from Tcl script, 14-Aug-2006
 --     Eilmer3 port, July 2008
 --     Eilmer4 port, May 2015
+--     Change to using implicit update, July 2021
 
 config.title = "Mach 2 flow along the axis of a 5mm cylinder."
 print(config.title)
@@ -27,20 +28,22 @@ grd = StructuredGrid:new{psurface=AOPatch:new{p00=a, p10=b, p11=c, p01=d},
 			 cfList=myCFList, niv=51, njv=51}
 
 -- Assemble the blocks from the grid and boundary data.
-blks = FBArray:new{grid=grd, nib=2, njb=2, 
-		       initialState=inflow,
-		       bcList={north=InFlowBC_Supersonic:new{flowState=inflow},
-			       east=OutFlowBC_Simple:new{},
-			       south=WallBC_NoSlip_FixedT:new{Twall=222.0},
-			       west=InFlowBC_Supersonic:new{flowState=inflow}}}
+blks = FBArray:new{grid=grd, nib=2, njb=2,
+                   initialState=inflow,
+                   bcList={north=InFlowBC_Supersonic:new{flowState=inflow},
+                           east=OutFlowBC_Simple:new{},
+                           south=WallBC_NoSlip_FixedT:new{Twall=222.0},
+                           west=InFlowBC_Supersonic:new{flowState=inflow}}}
 
 -- Do a little more setting of the simulation configuration data.
 config.viscous = true
 config.spatial_deriv_locn = "vertices"
 config.spatial_deriv_calc = "divergence"
 config.flux_calculator = "ausmdv"
-config.gasdynamic_update_scheme = "euler"
+config.gasdynamic_update_scheme = "backward_euler"
 config.max_time = 8.0e-3  -- seconds
 config.max_step = 230000
 config.dt_init = 3.0e-8
+config.cfl_schedule_times = {0.0, 10.0e-6, 100.0e-6}
+config.cfl_schedule_values = {0.5, 5.0, 50.0}
 config.dt_plot = 4.0e-3
