629fded008512574aa0233976a017b0b3cd46d28
Peter Jacobs <peterj@mech.uq.edu.au>
2021-08-16 11:44:28 +1000

    Eilmer sphere-with-shock-fitting example: add stagnation-line plot of temperatures.

--- examples/eilmer/2D/sphere-co2/post.sh
+++ examples/eilmer/2D/sphere-co2/post.sh
@@ -6,3 +6,8 @@ e4shared --post --job=sco2 --tindx-plot=all --vtk-xml --add-vars="mach"
 #
 # 2. shock location
 e4shared --custom-script --script-file=shock-position.lua
+#
+# 3. stagnation-line properties (first line of cell centres)
+e4shared --post --job=sco2 --tindx-plot=last --output-file="stag-profile.data" \
+         --slice-list="0,:,0,0;2,:,0,0" --add-vars="mach,pitot"
+gnuplot plot-stag-line-properties.gplot
