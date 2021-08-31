2c99413fce6a3bad575f6a9db9673efaa780c175
Peter Jacobs <peterj@mech.uq.edu.au>
2021-08-31 22:40:32 +1000

    Eilmer: rework the FBArray constructor to accept a single grid or a gridArray.
    The classic form is to accept a single grid which gets divided.
    The new arrangement can accept an array of subgrids.
    It is not yet complete but this commit is to let Nick's CI bot check
    that the old way continues to work.

--- src/eilmer/fluidblock.lua
+++ src/eilmer/fluidblock.lua
@@ -404,7 +404,7 @@ function FBArray:new(o)
       error("Make sure that you are using FBArray:new{} and not FBArray.new{}", 2)
    end
    o = o or {}
-   local flag = checkAllowedNames(o, {"grid", "initialState", "fillCondition",
+   local flag = checkAllowedNames(o, {"grid", "gridArray", "initialState", "fillCondition",
 				      "active", "label", "omegaz", "may_be_turbulent",
                                       "bcList", "nib", "njb", "nkb"})
    if not flag then
@@ -421,12 +421,6 @@ function FBArray:new(o)
       error('Have previously defined a FBArray with label "' .. o.label .. '"', 2)
    end
    fluidBlockArraysDict[o.label] = o.id
-   if not o.grid then
-      error("You need to supply a grid to FBArray constructor.", 2)
-   end
-   if (not o.grid.get_type) or o.grid:get_type() ~= "structured_grid" then
-      error("You need to supply a structured_grid to FBArray constructor.", 2)
-   end
    if not o.initialState then
       -- try old name
       o.initialState = o.fillCondition
@@ -449,90 +443,173 @@ function FBArray:new(o)
    end
    o.shock_fitting = (o.bcList["west"].type == "inflow_shock_fitting")
    o.xforceList = o.xforceList or {}
-   -- Numbers of subblocks in each coordinate direction
-   o.nib = o.nib or 1
-   o.njb = o.njb or 1
-   o.nkb = o.nkb or 1
-   if config.dimensions == 2 then
-      o.nkb = 1
+   if (not o.grid) and (not o.gridArray) then
+      error("You need to supply a grid or gridArray to FBArray constructor.", 2)
    end
-   -- Extract some information from the StructuredGrid
-   o.niv = o.grid:get_niv()
-   o.njv = o.grid:get_njv()
-   o.nkv = o.grid:get_nkv()
-   -- Subdivide the block based on numbers of cells.
-   -- Note 0-based indexing for vertices and cells in the D-domain.
-   local nic_total = o.niv - 1
-   local dnic = math.floor(nic_total/o.nib)
-   local njc_total = o.njv - 1
-   local dnjc = math.floor(njc_total/o.njb)
-   local nkc_total = o.nkv - 1
-   local dnkc = math.floor(nkc_total/o.nkb)
-   if config.dimensions == 2 then
-      nkc_total = 1
-      dnkc = 1
+   if o.grid then
+      -- We will take a single grid and divide it into an array of subgrids.
+      o.gridArray = {} -- will be a multi-dimensional array, indexed as [ib][jb][kb]
+      if (not o.grid.get_type) or o.grid:get_type() ~= "structured_grid" then
+         error("You need to supply a structured_grid to FBArray constructor.", 2)
+      end
+      -- Numbers of subblocks in each coordinate direction
+      o.nib = o.nib or 1
+      o.njb = o.njb or 1
+      o.nkb = o.nkb or 1
+      if config.dimensions == 2 then
+         o.nkb = 1
+      end
+      -- Extract some information from the StructuredGrid
+      o.niv = o.grid:get_niv()
+      o.njv = o.grid:get_njv()
+      o.nkv = o.grid:get_nkv()
+      -- Subdivide the single grid based on numbers of cells.
+      -- Note 0-based indexing for vertices and cells in the D-domain.
+      local nic_total = o.niv - 1
+      local dnic = math.floor(nic_total/o.nib)
+      local njc_total = o.njv - 1
+      local dnjc = math.floor(njc_total/o.njb)
+      local nkc_total = o.nkv - 1
+      local dnkc = math.floor(nkc_total/o.nkb)
+      if config.dimensions == 2 then
+         nkc_total = 1
+         dnkc = 1
+      end
+      -- Work along each index direction and work out numbers of cells in subgrid.
+      o.nics = {} -- numbers of cells in each subgrid
+      local nic_remaining = nic_total
+      for ib = 1, o.nib do
+         local nic = math.floor(nic_remaining/(o.nib-ib+1))
+         if (ib == o.nib) then
+            -- On last subgrid, just use what's left
+            nic = nic_remaining
+         end
+         o.nics[#o.nics+1] = nic
+         nic_remaining = nic_remaining - nic
+      end
+      o.njcs = {}
+      local njc_remaining = njc_total
+      for jb = 1, o.njb do
+         local njc = math.floor(njc_remaining/(o.njb-jb+1))
+         if (jb == o.njb) then
+            njc = njc_remaining
+         end
+         o.njcs[#o.njcs+1] = njc
+         njc_remaining = njc_remaining - njc
+      end
+      o.nkcs = {}
+      if config.dimensions == 2 then
+         o.nkcs[1] = 1
+      else
+         local nkc_remaining = nkc_total
+         for kb = 1, o.nkb do
+            local nkc = math.floor(nkc_remaining/(o.nkb-kb+1))
+            if (kb == o.nkb) then
+               nkc = nkc_remaining
+            end
+            o.nkcs[#o.nkcs+1] = nkc
+            nkc_remaining = nkc_remaining - nkc
+         end
+      end
+      -- Now, generate the actual subgrids.
+      local i0 = 0
+      for ib = 1, o.nib do
+         o.gridArray[ib] = {}
+         local nic = o.nics[ib]
+         local j0 = 0
+         for jb = 1, o.njb do
+            local njc = o.njcs[jb]
+            if config.dimensions == 2 then
+               -- 2D flow
+               if false then
+                  -- May activate print statements for debug.
+                  print("ib=", ib, "jb= ", jb)
+                  print("i0= ", i0, " nic= ", nic, " j0= ", j0, " njc= ", njc)
+               end
+               if nic < 1 then
+                  error(string.format("Invalid nic=%d while making subgrid ib=%d, jb=%d", nic, ib, jb), 2)
+               end
+               if njc < 1 then
+                  error(string.format("Invalid njc=%d while making subgrid ib=%d, jb=%d", njc, ib, jb), 2)
+               end
+               o.gridArray[ib][jb] = o.grid:subgrid(i0,nic+1,j0,njc+1)
+            else
+               -- 3D flow, need one more level in the array
+               o.gridArray[ib][jb] = {}
+               local k0 = 0
+               for kb = 1, o.nkb do
+                  local nkc = o.nkcs[kb]
+                  if nic < 1 then
+                     error(string.format("Invalid nic=%d while making subgrid ib=%d, jb=%d, kb=%d", nic, ib, jb, kb), 2)
+                  end
+                  if njc < 1 then
+                     error(string.format("Invalid njc=%d while making subgrid ib=%d, jb=%d, kb=%d", njc, ib, jb, kb), 2)
+                  end
+                  if nkc < 1 then
+                     error(string.format("Invalid nkc=%d while making subgrid ib=%d, jb=%d, kb=%d", nkc, ib, jb, kb), 2)
+                  end
+                  o.gridArray[ib][jb][kb] = o.grid:subgrid(i0,nic+1,j0,njc+1,k0,nkc+1)
+                  -- Prepare k0 at end of loop, ready for next iteration
+                  k0 = k0 + nkc
+               end -- kb loop
+            end -- dimensions
+            -- Prepare j0 at end of loop, ready for next iteration
+            j0 = j0 + njc
+         end -- jb loop
+         -- Prepare i0 at end of loop, ready for next iteration
+         i0 = i0 + nic
+      end -- ib loop
+      -- Finished generating subgrids
+   else
+      -- We were not given a single grid,
+      -- so we assume that we were given the array of subgrids.
+      --
+      -- [TODO] 2021-08-31 PJ
+      -- We need to check that the gridArray tables are regular and the individual grids align.
+      -- Maybe we should do that while assembling a single overall grid.
+      --
+      o.nib = #(o.gridArray)
+      o.njb = #(o.gridArray[1])
+      if config.dimensions == 2 then
+         -- Make stacks of the original subgrids.
+         local stacksOfGrids = {}
+         for ib = 1, o.nib do
+            stacksOfGrids[ib] = StructuredGrid:clone(o.gridArray[ib][1]) -- [TODO] need this method
+            for jb = 2, o.njb do
+               stacksOfGrids[ib].joinGrid(o.gridArray[ib][jb], "north")
+            end
+         end
+         o.grid = stacksOfGrids[1]
+         for ib = 2, o.nib do
+            o.grid.joinGrid(stacksOfGrids[ib], "east")
+         end
+      else
+         -- For 3D
+         o.nkb = #(o.gridArray[1][1])
+         error("[TODO] In 3D, we need to assemble a single grid from the gridArray")
+         -- Make stacks of the original subgrids, starting in the k-index direction,
+         -- then building slabs spanning the jk directions from those stacks and,
+         -- finally, joining the slabs in the i-direction.
+      end
+      -- Extract some information from the assembled StructuredGrid
+      o.niv = o.grid:get_niv()
+      o.njv = o.grid:get_njv()
+      o.nkv = o.grid:get_nkv()
    end
+   --
+   -- At this point, we have an array of grids so generate the array of FluidBlocks.
+   --
    o.blockArray = {} -- will be a multi-dimensional array, indexed as [ib][jb][kb],
                      -- with 1<=ib<=nib, 1<=jb<=njb, 1<=kb<=nkb
    o.blockCollection = {} -- will be a single-dimensional array, also starting at 1
-   -- Work along each index direction and work out numbers of cells in sub-blocks.
-   o.nics = {} -- numbers of cells in each sub-block
-   local nic_remaining = nic_total
-   for ib = 1, o.nib do
-      local nic = math.floor(nic_remaining/(o.nib-ib+1))
-      if (ib == o.nib) then
-         -- On last block, just use what's left
-         nic = nic_remaining
-      end
-      o.nics[#o.nics+1] = nic
-      nic_remaining = nic_remaining - nic
-   end
-   o.njcs = {}
-   local njc_remaining = njc_total
-   for jb = 1, o.njb do
-      local njc = math.floor(njc_remaining/(o.njb-jb+1))
-      if (jb == o.njb) then
-         njc = njc_remaining
-      end
-      o.njcs[#o.njcs+1] = njc
-      njc_remaining = njc_remaining - njc
-   end
-   o.nkcs = {}
-   if config.dimensions == 2 then
-      o.nkcs[1] = 1
-   else
-      local nkc_remaining = nkc_total
-      for kb = 1, o.nkb do
-         local nkc = math.floor(nkc_remaining/(o.nkb-kb+1))
-         if (kb == o.nkb) then
-            nkc = nkc_remaining
-         end
-         o.nkcs[#o.nkcs+1] = nkc
-         nkc_remaining = nkc_remaining - nkc
-      end
-   end
-   -- Now, generate the sub-blocks.
    local i0 = 0
    for ib = 1, o.nib do
       o.blockArray[ib] = {}
-      local nic = o.nics[ib]
       local j0 = 0
       for jb = 1, o.njb do
-         local njc = o.njcs[jb]
 	 if config.dimensions == 2 then
 	    -- 2D flow
-            if false then
-               -- May activate print statements for debug.
-               print("ib=", ib, "jb= ", jb)
-               print("i0= ", i0, " nic= ", nic, " j0= ", j0, " njc= ", njc)
-            end
-            if nic < 1 then
-               error(string.format("Invalid nic=%d while making subgrid ib=%d, jb=%d", nic, ib, jb), 2)
-            end
-            if njc < 1 then
-               error(string.format("Invalid njc=%d while making subgrid ib=%d, jb=%d", njc, ib, jb), 2)
-            end
-	    local subgrid = o.grid:subgrid(i0,nic+1,j0,njc+1)
+	    local subgrid = o.gridArray[ib][jb]
 	    local bcList = {north=WallBC_WithSlip:new(), east=WallBC_WithSlip:new(),
 			    south=WallBC_WithSlip:new(), west=WallBC_WithSlip:new()}
 	    if ib == 1 then
@@ -559,17 +636,7 @@ function FBArray:new(o)
 	    o.blockArray[ib][jb] = {}
             local k0 = 0
 	    for kb = 1, o.nkb do
-               local nkc = o.nkcs[kb]
-               if nic < 1 then
-                  error(string.format("Invalid nic=%d while making subgrid ib=%d, jb=%d, kb=%d", nic, ib, jb, kb), 2)
-               end
-               if njc < 1 then
-                  error(string.format("Invalid njc=%d while making subgrid ib=%d, jb=%d, kb=%d", njc, ib, jb, kb), 2)
-               end
-               if nkc < 1 then
-                  error(string.format("Invalid nkc=%d while making subgrid ib=%d, jb=%d, kb=%d", nkc, ib, jb, kb), 2)
-               end
-	       local subgrid = o.grid:subgrid(i0,nic+1,j0,njc+1,k0,nkc+1)
+	       local subgrid = o.gridArray[ib][jb][kb]
 	       local bcList = {north=WallBC_WithSlip:new(), east=WallBC_WithSlip:new(),
 			       south=WallBC_WithSlip:new(), west=WallBC_WithSlip:new(),
 			       top=WallBC_WithSlip:new(), bottom=WallBC_WithSlip:new()}
@@ -598,15 +665,9 @@ function FBArray:new(o)
                                                 fluidBlockArrayId=o.id,}
 	       o.blockArray[ib][jb][kb] = new_block
 	       o.blockCollection[#o.blockCollection+1] = new_block
-               -- Prepare k0 at end of loop, ready for next iteration
-               k0 = k0 + nkc
 	    end -- kb loop
 	 end -- dimensions
-         -- Prepare j0 at end of loop, ready for next iteration
-         j0 = j0 + njc
       end -- jb loop
-      -- Prepare i0 at end of loop, ready for next iteration
-      i0 = i0 + nic
    end -- ib loop
    -- Make the inter-subblock connections
    if #o.blockCollection > 1 then
