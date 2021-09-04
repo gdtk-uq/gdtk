a2a90e2a78e9429e7fd604ce7d778024adf91d3e
Chris James <c.james4@uq.edu.au>
2021-09-04 17:24:14 +1000

    examples/pitot3: added Drummond RST example
    Now that I have the RST nozzle working, I can finally add the
    Drummond RST example to the repository.

---
+++ examples/pitot3/drummond_rst/drummond_rst.sh
@@ -0,0 +1,6 @@
+# drummond_rst.sh: 
+# Shell script to run the Drummond RST
+# air theoretical example for PITOT3.
+# Chris James (c.james4@uq.edu.au) - 30/08/21
+
+pitot3.py --config_file drummond_rst.yaml
