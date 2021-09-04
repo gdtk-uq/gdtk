19b9724c0ad03f8d8a6b22d1e6389f0e7668345a
Peter Jacobs <peterj@mech.uq.edu.au>
2021-09-04 21:18:36 +1000

    Eilmer 3D sphere example: it helps to apply the BC correctly.
    This low-resolution simulation of Mach 1.5 flow over a sphere runs in
    about a minute on my workstation.

--- examples/eilmer/3D/sphere/run.sh
+++ examples/eilmer/3D/sphere/run.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/bash
+#! /bin/bash
 # run.sh
 e4shared --job=sphere --run
 e4shared --job=sphere --post --vtk-xml --tindx-plot=all
--- examples/eilmer/3D/sphere/sphere.lua
+++ examples/eilmer/3D/sphere/sphere.lua
@@ -2,7 +2,9 @@
 --
 -- Author: Ingo Jahn & Peter J.
 -- Created: 22/3/2021
--- Last Modified: 2021-04-03 simplified to use the SpherePatch and TwoSurfaceVolume classes.
+-- Modified:
+--   2021-04-03 simplified to use the SpherePatch and TwoSurfaceVolume classes.
+--   2021-09-04 get the orientation correct for half of the blocks.
 --
 config.dimensions = 3
 nsp, nmodes, gm = setGasModel('ideal-air-gas-model.lua')
@@ -12,7 +14,7 @@ inflow = FlowState:new{p=95.84e3, T=1103.0, velx=1000.0}
 -- Domain is constructed by 6 blocks attached to the outside of the sphere.
 -- The blocks are labeled in accordance with the face they are attached to.
 R_sphere = 1.0
-R_mesh = 2.0
+R_mesh = 4.0
 vols = {}
 faces = {"east", "west", "north", "south", "top", "bottom"}
 for _,f in ipairs(faces) do
@@ -28,10 +30,14 @@ end
 -- Define the 6 grids.
 N_edge = 20  -- cells along edge of cube faces that are mapped onto the sphere
 N_normal = 20  -- cells in sphere normal direction
-cf = RobertsFunction:new{end0=true, end1=false, beta=1.05}
-cfList = {edge04=cf, edge15=cf, edge26=cf, edge37=cf}
 grids = {}
 for _,f in ipairs(faces) do
+   if f == "east" or f == "south" or f == "top" then
+      cf = RobertsFunction:new{end0=true, end1=false, beta=1.05}
+   else
+      cf = RobertsFunction:new{end0=false, end1=true, beta=1.05}
+   end
+   cfList = {edge04=cf, edge15=cf, edge26=cf, edge37=cf}
    grids[f] = StructuredGrid:new{pvolume=vols[f], cfList=cfList,
                                  niv=N_edge, njv=N_edge, nkv=N_normal}
 end
@@ -40,8 +46,12 @@ blks = {}
 nib = 1; njb = 1; nkb = 1
 wall_bc = WallBC_WithSlip:new{}
 inflow_bc = InOutFlowBC_Ambient:new{flowState=inflow}
-bcList = {top=inflow_BC, bottom=wall_bc}
 for _,f in ipairs(faces) do
+   if f == "east" or f == "south" or f == "top" then
+      bcList = {top=inflow_bc, bottom=wall_bc}
+   else
+      bcList = {bottom=inflow_bc, top=wall_bc}
+   end
    blks[f] = FBArray:new{grid=grids[f], initialState=initial,
                          bcList=bcList, nib=nib, njb=njb, nkb=nkb}
 end
