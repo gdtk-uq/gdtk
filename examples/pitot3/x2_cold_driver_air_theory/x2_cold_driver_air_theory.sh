a31720ad24bfe70d164e7e5ca84192d7ec3430b5
Chris James <c.james4@uq.edu.au>
2021-09-06 16:38:58 +1000

    pitot3/examples: added a cold driven air example
    added a cold driven air X2 example from Matt Thompson's work

---
+++ examples/pitot3/x2_cold_driver_air_theory/x2_cold_driver_air_theory.sh
@@ -0,0 +1,6 @@
+# x2_cold_driver_air_theory.sh: 
+# Shell script to run a cold driven X2
+# air theoretical example for PITOT3.
+# Chris James (c.james4@uq.edu.au) - 06/09/21
+
+pitot3.py --config_file x2_cold_driver_air_theory.yaml
