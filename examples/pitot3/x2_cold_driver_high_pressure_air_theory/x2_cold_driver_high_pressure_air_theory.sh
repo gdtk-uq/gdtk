8b042f58de9ae4ce3f677bf20b42c699dc3c22de
Chris James <c.james4@uq.edu.au>
2021-09-07 15:14:56 +1000

    examples/pitot3: added new high pressure cold driver and Uranus entry examples
    These are two exampels which now work with changes I made earlier today.

---
+++ examples/pitot3/x2_cold_driver_high_pressure_air_theory/x2_cold_driver_high_pressure_air_theory.sh
@@ -0,0 +1,6 @@
+# x2_cold_driver_high_pressure_air_theory.sh: 
+# Shell script to run a high pressure, cold driven X2
+# air theoretical example for PITOT3.
+# Chris James (c.james4@uq.edu.au) - 07/09/21
+
+pitot3.py --config_file x2_cold_driver_high_pressure_air_theory.yaml
---
+++ examples/pitot3/x2_uranus_entry_with_secondary_driver_theory/x2_uranus_entry_with_secondary_driver_theory.sh
@@ -0,0 +1,6 @@
+# x2_uranus_entry_with_secondary_driver_theory.sh: 
+# Shell script to run the PITOT3
+# theoretical Uranus entry with secondary driver example.
+# Chris James (c.james4@uq.edu.au) - 30/08/21
+
+pitot3.py --config_file x2_uranus_entry_with_secondary_driver_theory.yaml
