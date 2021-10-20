c8a3d2ef0287f84c6ad0dd4a872f0e4286364f8c
Peter Jacobs <peterj@mech.uq.edu.au>
2021-10-21 09:46:37 +1000

    nenzf1d: introduce a test script for 1T and 2T air in the T4 shock tunnel.

---
+++ examples/nenzf1d/nenzf1d-test.rb
@@ -0,0 +1,136 @@
+#! /usr/bin/env ruby
+# nenzf1d-test.rb
+# Exercise the NENZF1d program for a number of cases.
+# PJ, 2021-10-21
+#
+require 'test/unit'
+require 'open3'
+
+class TestNENZF1D < Test::Unit::TestCase
+  def test_0_prep_gas_files_air
+    cmd = "cp #{ENV['DGD_REPO']}/src/gas/sample-data/cea-air5species-gas-model.lua ."
+    o, e, s = Open3.capture3(*cmd.split)
+    assert_equal(s.success?, true)
+    cmd = "cp #{ENV['DGD_REPO']}/src/gas/sample-data/air-5sp-eq.lua ."
+    o, e, s = Open3.capture3(*cmd.split)
+    assert_equal(s.success?, true)
+    cmd = "cp #{ENV['DGD_REPO']}/examples/kinetics/air-chemistry-1T/air-5sp-1T.inp ."
+    o, e, s = Open3.capture3(*cmd.split)
+    assert_equal(s.success?, true)
+    cmd = "cp #{ENV['DGD_REPO']}/examples/kinetics/air-chemistry-1T/GuptaEtAl-air-reactions.lua ."
+    o, e, s = Open3.capture3(*cmd.split)
+    assert_equal(s.success?, true)
+    cmd = "prep-gas air-5sp-1T.inp air-5sp-1T.lua"
+    o, e, s = Open3.capture3(*cmd.split)
+    assert_equal(s.success?, true)
+    cmd = "prep-chem air-5sp-1T.lua GuptaEtAl-air-reactions.lua air-5sp-1T-reactions.lua"
+    o, e, s = Open3.capture3(*cmd.split)
+    assert_equal(s.success?, true)
+    cmd = "cp #{ENV['DGD_REPO']}/examples/kinetics/air-chemistry-2T/air-5sp-2T.inp ."
+    o, e, s = Open3.capture3(*cmd.split)
+    assert_equal(s.success?, true)
+    cmd = "cp #{ENV['DGD_REPO']}/examples/kinetics/air-chemistry-2T/GuptaEtAl-air-2T.lua ."
+    o, e, s = Open3.capture3(*cmd.split)
+    assert_equal(s.success?, true)
+    cmd = "cp #{ENV['DGD_REPO']}/examples/kinetics/air-chemistry-2T/air-energy-exchange.lua ."
+    o, e, s = Open3.capture3(*cmd.split)
+    assert_equal(s.success?, true)
+    cmd = "prep-gas air-5sp-2T.inp air-5sp-2T.lua"
+    o, e, s = Open3.capture3(*cmd.split)
+    assert_equal(s.success?, true)
+    cmd = "prep-chem air-5sp-2T.lua GuptaEtAl-air-2T.lua air-5sp-6r-2T.lua"
+    o, e, s = Open3.capture3(*cmd.split)
+    assert_equal(s.success?, true)
+  end
+
+  def test_1_t4m8_11742
+    cmd = "nenzf1d t4m8-11742.yaml"
+    o, e, s = Open3.capture3(*cmd.split)
+    assert_equal(s.success?, true)
+    found_exit_condition = false
+    enthalpy = 0.0; mach = 0.0; temperature = 0.0; pressure = 0.0
+    massf_NO = 0.0
+    lines = o.split("\n")
+    lines.each do |txt|
+      if txt.match('Exit condition') then
+        found_exit_condition = true
+      end
+      if txt.match('H5s-H1') then
+        items = txt.split(' ')
+        enthalpy = items[1].to_f
+      end
+      if found_exit_condition then
+        if txt.match('Mach') then
+          items = txt.split(' ')
+          mach = items[1].to_f
+        end
+        if txt.match('temperature') then
+          items = txt.split(' ')
+          temperature = items[1].to_f
+        end
+        if txt.match('pressure') then
+          items = txt.split(' ')
+          pressure = items[1].to_f
+        end
+        if txt.match('massf\[NO\]') then
+          items = txt.split(' ')
+          massf_NO = items[1].to_f
+        end
+      end
+    end
+    assert((enthalpy - 4.79689).abs < 0.01, "Failed to compute correct enthalpy.")
+    assert(found_exit_condition, "Failed to find exit condition in output.")
+    assert((mach - 7.15261).abs < 0.01, "Failed to compute correct Mach number.")
+    assert((temperature - 432.096).abs < 0.1, "Failed to compute correct temperature.")
+    assert((pressure - 4.69785).abs < 0.01, "Failed to compute correct pressure.")
+    assert((massf_NO - 0.068366).abs < 0.001, "Failed to compute correct mass-fraction of NO.")
+  end
+
+  def test_1_t4m7_air_2T
+    cmd = "nenzf1d t4m7-air-2T.yaml"
+    o, e, s = Open3.capture3(*cmd.split)
+    assert_equal(s.success?, true)
+    found_exit_condition = false
+    enthalpy = 0.0; mach = 0.0; temperature = 0.0; pressure = 0.0
+    massf_NO = 0.0; tvib = 0.0
+    lines = o.split("\n")
+    lines.each do |txt|
+      if txt.match('Exit condition') then
+        found_exit_condition = true
+      end
+      if txt.match('H5s-H1') then
+        items = txt.split(' ')
+        enthalpy = items[1].to_f
+      end
+      if found_exit_condition then
+        if txt.match('Mach') then
+          items = txt.split(' ')
+          mach = items[1].to_f
+        end
+        if txt.match('temperature') then
+          items = txt.split(' ')
+          temperature = items[1].to_f
+        end
+        if txt.match('pressure') then
+          items = txt.split(' ')
+          pressure = items[1].to_f
+        end
+        if txt.match('massf\[NO\]') then
+          items = txt.split(' ')
+          massf_NO = items[1].to_f
+        end
+        if txt.match('T_modes\[0\]') then
+          items = txt.split(' ')
+          tvib = items[1].to_f
+        end
+      end
+    end
+    assert((enthalpy - 2.47955).abs < 0.01, "Failed to compute correct enthalpy.")
+    assert(found_exit_condition, "Failed to find exit condition in output.")
+    assert((mach - 7.60648).abs < 0.01, "Failed to compute correct Mach number.")
+    assert((temperature - 221.798).abs < 0.1, "Failed to compute correct temperature.")
+    assert((pressure - 2.83504).abs < 0.01, "Failed to compute correct pressure.")
+    assert((massf_NO - 0.00972494).abs < 0.001, "Failed to compute correct mass-fraction of NO.")
+    assert((tvib - 1316.84).abs < 0.1, "Failed to compute correct vibrational temperature.")
+  end
+end
