a5a954fcc090c8bf5a9395238fa7f98580c569c4
Nick N. Gibbons <n.gibbons@uq.edu.au>
2021-11-04 16:24:22 +1000

    field: Added a constant conductivity model

--- src/eilmer/field/fieldconductivity.d
+++ src/eilmer/field/fieldconductivity.d
@@ -27,6 +27,23 @@ class TestConductivity : ConductivityModel{
     }
 }
 
+class ConstantConductivity : ConductivityModel{
+/*
+    Interestingly, as far as the electric potential cares its the gradient of the
+    conductivity that matters, not the conductivity value itself. What this means
+    is that if we want to solve a constant conductivity problem, we can just say
+    that it is equal to one, and get the same answer as any other value.
+
+    For the *current* though, things are different. At that stage we really do need
+    the actual values; but for the moment calculating currents is a post processing
+    thing only.
+*/
+    this() {}
+    final const double opCall(const FlowState fs, const Vector3 pos){
+        return 1.0;
+    }
+}
+
 
 ConductivityModel create_conductivity_model(string name){
     ConductivityModel conductivity_model;
@@ -34,6 +51,11 @@ ConductivityModel create_conductivity_model(string name){
     case "test":
         conductivity_model = new TestConductivity();
         break;
+    case "constant":
+        conductivity_model = new ConstantConductivity();
+        break;
+    case "none":
+        throw new Error("User has asked for solve_electric_field but failed to specify a conductivity model.");
     default:
         string errMsg = format("The conductivity model '%s' is not available.", name);
         throw new Error(errMsg);
