0b1eb63ef17fa7f09d40fa894742ba837726c38a
Peter Jacobs <peterj@mech.uq.edu.au>
2021-11-28 14:49:29 +1000

    Eilmer examples: get the Lobb sphere case working better with the no-ghost-cell wall BC.
    It seems that the no-ghost-cell slip wall handles the strong shock
    reflection not as well as its ghost-cell counterpart.

--- examples/eilmer/2D/sphere-lobb/smoke-test/lobb.lua
+++ examples/eilmer/2D/sphere-lobb/smoke-test/lobb.lua
@@ -31,24 +31,28 @@ u_inf = 4825.0 -- m/s
 inflow = FlowState:new{p=p_inf, T=T_inf, velx=u_inf, massf={N2=0.78,O2=0.22}}
 
 body_flow_time = Db/u_inf
+print("body_flow_time=", body_flow_time)
 config.reacting = true
 config.reactions_file = 'air-5sp-6r.lua'
 config.reaction_time_delay = 0.6*no_flow_times*body_flow_time
+print("reaction_time_delay=", config.reaction_time_delay)
 config.dimensions = 2
 config.axisymmetric = true
 config.grid_motion = "shock_fitting"
 config.flux_calculator = "ausmdv"
 config.interpolation_order = 2
-config.gasdynamic_update_scheme = "moving_grid_1_stage"
+config.gasdynamic_update_scheme = "backward_euler"
 config.max_time = no_flow_times*body_flow_time
+print("max_time=", config.max_time)
 if useOldSoln then
    config.shock_fitting_delay = 0.0
 else
    config.shock_fitting_delay = 2.0*body_flow_time
 end
+print("shock_ftting_delay=", config.shock_fitting_delay)
 config.max_step = 80000
-config.dt_init = 1.0e-11
-config.cfl_value = 0.2
+config.dt_init = 1.0e-9
+config.cfl_schedule = {{0.0, 0.2}, {3.0*body_flow_time, 0.2}, {5.0*body_flow_time, 0.4}}
 config.adjust_invalid_cell_data = true
 config.report_invalid_cells = false
 config.max_invalid_cells = 10
@@ -103,9 +107,9 @@ print "Done building grid."
 
 print "Construct block."
 blk = FBArray:new{grid=grid, initialState=initial, label='blk',
-		      bcList={west=InFlowBC_ShockFitting:new{flowState=inflow},
-			      north=OutFlowBC_Simple:new{}},
-		      nib=1, njb=njb}
+                  bcList={west=InFlowBC_ShockFitting:new{flowState=inflow},
+                          north=OutFlowBC_Simple:new{}},
+                  nib=1, njb=njb}
 print "Done constructing block."
 
 -- Set some recording points to capture flow data
@@ -113,5 +117,3 @@ setHistoryPoint{x=0.0, y=0.0}
 setHistoryPoint{x=Rc, y=Rc}
 setHistoryPoint{ib=0, i=0, j=0} -- at shock boundary (Which we expect to move)
 config.dt_history = 1.0e-7
-
-
