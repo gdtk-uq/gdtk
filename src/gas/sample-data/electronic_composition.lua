d2522072ceff38f094b245333a018c3ec44ebb35
Peter Jacobs <peterj@mech.uq.edu.au>
2021-12-14 15:56:38 +1000

    Gas: update the electronically-specific gas model input to suit Lua 5.3.

--- src/gas/sample-data/electronic_composition.lua
+++ src/gas/sample-data/electronic_composition.lua
@@ -10,162 +10,161 @@ electronic_species = {}
 
 electronic_species[0] = {
 	name = 'NI - 1',
-	lower_level = 1.0,
-	upper_level = 1.0,
+	lower_level = 1,
+	upper_level = 1,
 	M = 0.0140067,
 	group_energy = 0.0,
-	group_degeneracy = 4.0,
+	group_degeneracy = 4,
 	dof = 3,
 }
 
 electronic_species[1] = {
 	name = 'NI - 2',
-	lower_level = 2.0,
-	upper_level = 2.0,
+	lower_level = 2,
+	upper_level = 2,
 	M = 0.0140067,
 	group_energy = 2.384,
-	group_degeneracy = 10.0,
+	group_degeneracy = 10,
 	dof = 3,
 }
 
 electronic_species[2] = {
 	name = 'NI - 3',
-	lower_level = 3.0,
-	upper_level = 3.0,
+	lower_level = 3,
+	upper_level = 3,
 	M = 0.0140067,
 	group_energy = 3.576,
-	group_degeneracy = 6.0,
+	group_degeneracy = 6,
 	dof = 3,
 }
 
 electronic_species[3] = {
 	name = 'NI - 4',
-	lower_level = 4.0,
-	upper_level = 6.0,
+	lower_level = 4,
+	upper_level = 6,
 	M = 0.0140067,
 	group_energy = 10.641,
-	group_degeneracy = 30.0,
+	group_degeneracy = 30,
 	dof = 3,
 }
 
 electronic_species[4] = {
 	name = 'NI - 5',
-	lower_level = 7.0,
-	upper_level = 13.0,
+	lower_level = 7,
+	upper_level = 13,
 	M = 0.0140067,
 	group_energy = 11.95084375,
-	group_degeneracy = 64.0,
+	group_degeneracy = 64,
 	dof = 3,
 }
 
 electronic_species[5] = {
 	name = 'NI - 6',
-	lower_level = 14.0,
-	upper_level = 21.0,
+	lower_level = 14,
+	upper_level = 21,
 	M = 0.0140067,
 	group_energy = 12.9847636364,
-	group_degeneracy = 110.0,
+	group_degeneracy = 110,
 	dof = 3,
 }
 
 electronic_species[6] = {
 	name = 'NI - 7',
-	lower_level = 22.0,
-	upper_level = 27.0,
+	lower_level = 22,
+	upper_level = 27,
 	M = 0.0140067,
 	group_energy = 13.34203125,
-	group_degeneracy = 64.0,
+	group_degeneracy = 64,
 	dof = 3,
 }
 
 electronic_species[7] = {
 	name = 'NI - 8',
-	lower_level = 28.0,
-	upper_level = 46.0,
+	lower_level = 28,
+	upper_level = 46,
 	M = 0.0140067,
 	group_energy = 13.9876399132,
-	group_degeneracy = 922.0,
+	group_degeneracy = 922,
 	dof = 3,
 }
 
 
 electronic_species[8] = {
 	name = 'OI - 1',
-	lower_level = 1.0,
-	upper_level = 1.0,
+	lower_level = 1,
+	upper_level = 1,
 	M = 0.0159994,
 	group_energy = 0.0,
-	group_degeneracy = 9.0,
+	group_degeneracy = 9,
 	dof = 3,
 }
 
 electronic_species[9] = {
 	name = 'OI - 2',
-	lower_level = 2.0,
-	upper_level = 2.0,
+	lower_level = 2,
+	upper_level = 2,
 	M = 0.0159994,
 	group_energy = 1.97,
-	group_degeneracy = 5.0,
+	group_degeneracy = 5,
 	dof = 3,
 }
 
 electronic_species[10] = {
 	name = 'OI - 3',
-	lower_level = 3.0,
-	upper_level = 3.0,
+	lower_level = 3,
+	upper_level = 3,
 	M = 0.0159994,
 	group_energy = 4.19,
-	group_degeneracy = 1.0,
+	group_degeneracy = 1,
 	dof = 3,
 }
 
 electronic_species[11] = {
 	name = 'OI - 4',
-	lower_level = 4.0,
-	upper_level = 6.0,
+	lower_level = 4,
+	upper_level = 6,
 	M = 0.0159994,
 	group_energy = 10.2344782609,
-	group_degeneracy = 23.0,
+	group_degeneracy = 23,
 	dof = 3,
 }
 
 electronic_species[12] = {
 	name = 'OI - 5',
-	lower_level = 7.0,
-	upper_level = 13.0,
+	lower_level = 7,
+	upper_level = 13,
 	M = 0.0159994,
 	group_energy = 12.0181481481,
-	group_degeneracy = 81.0,
+	group_degeneracy = 81,
 	dof = 3,
 }
 
 electronic_species[13] = {
 	name = 'OI - 6',
-	lower_level = 14.0,
-	upper_level = 21.0,
+	lower_level = 14,
+	upper_level = 21,
 	M = 0.0159994,
 	group_energy = 12.7522302158,
-	group_degeneracy = 139.0,
+	group_degeneracy = 139,
 	dof = 3,
 }
 
 electronic_species[14] = {
 	name = 'OI - 7',
-	lower_level = 22.0,
-	upper_level = 27.0,
+	lower_level = 22,
+	upper_level = 27,
 	M = 0.0159994,
 	group_energy = 13.07290625,
-	group_degeneracy = 128.0,
+	group_degeneracy = 128,
 	dof = 3,
 }
 
 electronic_species[15] = {
 	name = 'OI - 8',
-	lower_level = 28.0,
-	upper_level = 40.0,
+	lower_level = 28,
+	upper_level = 40,
 	M = 0.0159994,
 	group_energy = 13.3391939252,
-	group_degeneracy = 428.0,
+	group_degeneracy = 428,
 	dof = 3,
 }
-
