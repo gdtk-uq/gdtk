e939bfb0f1a748514871400a569a21ee0c1ee954
Chris James <c.james4@uq.edu.au>
2022-03-16 10:45:31 +1000

    pitot3: adding a related CO2 example for PITOT3.

---
+++ examples/pitot3/x2_cold_driver_co2_theory/x2_cold_driver_co2_theory.sh
@@ -0,0 +1,6 @@
+# x2_cold_driver_co2_theory.sh: 
+# Shell script to run a cold driven X2
+# CO2 theoretical example for PITOT3.
+# Chris James (c.james4@uq.edu.au) - 16/03/22
+
+pitot3.py --config_file x2_cold_driver_co2_theory.yaml
