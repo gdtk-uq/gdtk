cd29ed23c13893249b4f24eb206a741a6a8da19d
Peter Jacobs <peterj@mech.uq.edu.au>
2022-05-24 22:04:50 +1000

    Eilmer: make the value of UDF source term for energy available.
    It will be appear as the  "Qudf_totEnergy" entry in the table
    that is returned by sampleFluidCell.

--- src/eilmer/lua_helper.d
+++ src/eilmer/lua_helper.d
@@ -272,6 +272,10 @@ void pushFluidCellToTable(lua_State* L, int tblIdx, ref const(FVCell) cell,
     lua_pushnumber(L, cell.jLength); lua_setfield(L, tblIdx, "jLength");
     lua_pushnumber(L, cell.kLength); lua_setfield(L, tblIdx, "kLength");
     pushFlowStateToTable(L, tblIdx, cell.fs, gmodel);
+    // For Carrie Xie 2022-05-24, we want access to the user-defined energy source term
+    // when we sample the Fluid cell during the UDF evaluation for the corresponding solid cell.
+    auto cqi = GlobalConfig.cqi;
+    lua_pushnumber(L, cell.Qudf.vec[cqi.totEnergy]); lua_setfield(L, tblIdx, "Qudf_totEnergy");
 } // end pushFluidCellToTable()
 
 void pushFluidFaceToTable(lua_State* L, int tblIdx, ref const(FVInterface) face,
