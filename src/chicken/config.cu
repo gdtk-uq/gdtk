b5ed4bccd1faac16cfe1a7ee349153770e5d8ae1
Peter Jacobs <peterj@mech.uq.edu.au>
2022-09-17 08:39:37 +1000

    Chicken: move the CUDA workshop experiment code aside.
    The top-level functions for the simulation are introduced but are empty.

---
+++ src/chicken/config.cu
@@ -0,0 +1,19 @@
+// config.cu
+// Include file for chicken: configuration variables.
+// PJ 2022-09-17
+
+#ifndef CONFIG_INCLUDED
+#define CONFIG_INCLUDED
+
+#include "number.cu"
+
+using namespace std;
+
+namespace Config {
+    string job = "job";
+    int nib = 1;
+    int njb = 1;
+    int nkb = 1;
+}
+
+#endif
