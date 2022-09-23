a35ae2d44052c44630b1ea97b5031fcf65658422
Peter Jacobs <peterj@mech.uq.edu.au>
2022-09-24 08:31:38 +1000

    Chicken: post-processing writes the top-level .pvts file
    This allows all blocks for a particular tindx to be loaded as a single
    solution by Paraview.

--- src/chicken/chkn_post.py
+++ src/chicken/chkn_post.py
@@ -119,26 +119,60 @@ def write_vtk_files(jobDir, tindx):
     global config, grids, flows
     plotDir = jobDir + '/plot'
     if not os.path.exists(plotDir): os.mkdir(plotDir)
+    whole_niv = sum(config['nics']) + 1
+    whole_njv = sum(config['njcs']) + 1
+    whole_nkv = sum(config['nkcs']) + 1
+    # The coordinating .pvts file is written as we write the individual .vts files.
+    fileName = plotDir + ('/flow-t%04d.pvts' % (tindx,))
+    fp = open(fileName, mode='w')
+    fp.write('<VTKFile type="PStructuredGrid" version="0.1" byte_order="BigEndian">\n')
+    fp.write('<PStructuredGrid WholeExtent="%d %d %d %d %d %d" GhostLevel="0">\n' %
+             (0, whole_niv-1, 0, whole_njv-1, 0, whole_nkv-1))
+    fp.write('<PCellData>\n')
+    for var in config["flow_var_names"]:
+        fp.write('<DataArray Name="%s" type="Float64" NumberOfComponents="1" format="ascii" />\n' % (var,))
+    fp.write('</PCellData>\n')
+    fp.write('<PPoints>\n')
+    fp.write('<DataArray type="Float64" NumberOfComponents="3" format="ascii" />\n')
+    fp.write('</PPoints>\n')
+    start_nkv = 0
     for k in range(config['nkb']):
+        start_njv = 0
         for j in range(config['njb']):
+            start_niv = 0
             for i in range(config['nib']):
-                fileName = plotDir + ('/flow-t%04d-%04d-%04d-%04d.vts' % (tindx, i, j, k))
+                fileName = 'flow-t%04d-%04d-%04d-%04d.vts' % (tindx, i, j, k)
                 key = '%d,%d,%d'%(i,j,k)
-                write_vtk_structured_grid_file(fileName, grids[key], flows[key])
+                grid = grids[key]
+                fp.write('<Piece Extent="%d %d %d %d %d %d" Source="%s" />\n' %
+                         (start_niv, start_niv+grid.niv-1,
+                          start_njv, start_njv+grid.njv-1,
+                          start_nkv, start_nkv+grid.nkv-1, fileName))
+                write_vtk_structured_grid_file(plotDir+'/'+fileName, grid, flows[key],
+                                               whole_niv, whole_njv, whole_nkv,
+                                               start_niv, start_njv, start_nkv)
+                start_niv += config['nics'][i]
+            start_njv += config['njcs'][j]
+        start_nkv += config['nkcs'][k]
+    fp.write('</PStructuredGrid>\n')
+    fp.write('</VTKFile>\n')
+    fp.close()
     return
 
-def write_vtk_structured_grid_file(fileName, grid, flowData):
+def write_vtk_structured_grid_file(fileName, grid, flowData,
+                                   whole_niv, whole_njv, whole_nkv,
+                                   start_niv, start_njv, start_nkv):
     """
-    Combine the grid and flow data into a VTK StructuredGrid file.
-
-    For the moment, we treat each block independently, not as a piece of a larger grid.
+    Combine the grid and flow data for one block into a VTK StructuredGrid file.
     """
     with open(fileName, mode='w') as fp:
         fp.write('<VTKFile type="StructuredGrid" version="0.1" byte_order="BigEndian">\n')
         fp.write('<StructuredGrid WholeExtent="%d %d %d %d %d %d">\n' %
-                 (0, grid.niv-1, 0, grid.njv-1, 0, grid.nkv-1))
+                 (0, whole_niv-1, 0, whole_njv-1, 0, whole_nkv-1))
         fp.write('<Piece Extent="%d %d %d %d %d %d">\n' %
-                 (0, grid.niv-1, 0, grid.njv-1, 0, grid.nkv-1))
+                 (start_niv, start_niv+grid.niv-1,
+                  start_njv, start_njv+grid.njv-1,
+                  start_nkv, start_nkv+grid.nkv-1))
         fp.write('<CellData>\n')
         for var in config["flow_var_names"]:
             fp.write('<DataArray Name="%s" type="Float64" NumberOfComponents="1" format="ascii">\n' % (var,))
