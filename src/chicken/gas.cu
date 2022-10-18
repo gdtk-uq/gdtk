aa665885100c34fe986640c1bfd340213d2f4446
Peter Jacobs <peterj@mech.uq.edu.au>
2022-10-18 14:08:00 +1000

    Chicken: add molecular-transport coefficients.
    Assuming air with a Sutherland viscosity model and
    constant Prandtl number.

--- src/chicken/gas.cu
+++ src/chicken/gas.cu
@@ -12,9 +12,17 @@
 using namespace std;
 
 namespace GasModel {
+    // Ideal gas, air.
     constexpr number g = 1.4;
     constexpr number R = 287.1;
     constexpr number Cv = R / (g-1.0);
+    constexpr number Cp = Cv + R;
+    // Sutherland viscosity model, air.
+    constexpr number mu_ref = 1.716e-5;
+    constexpr number T_ref = 273.0;
+    constexpr number S = 111.0;
+    //
+    constexpr number Prandtl = 0.72;
 };
 
 
@@ -30,7 +38,9 @@ struct GasState {
             ", T=" + to_string(T) + ", e=" + to_string(e) + ", a=" + to_string(a) + ")";
     }
 
-    __host__ __device__ void update_from_pT()
+
+    __host__ __device__
+    void update_from_pT()
     {
         using namespace GasModel;
         e = T*Cv;
@@ -38,13 +48,23 @@ struct GasState {
         a = sqrt(g*R*T);
     }
 
-    __host__ __device__ void update_from_rhoe()
+    __host__ __device__
+    void update_from_rhoe()
     {
         using namespace GasModel;
         T = e/Cv;
         p = rho*R*T;
         a = sqrt(g*R*T);
     }
+
+    __host__ __device__
+    void trans_coeffs(number& mu, number& k)
+    {
+        using namespace GasModel;
+        mu = mu_ref*sqrt(T/T_ref)*(T/T_ref)*(T_ref + S)/(T + S);
+        k = Cp*mu/Prandtl;
+    }
+
 }; // end GasState
 
 #endif
