983c6b7212efec507e139419109ea7d1ca7c9db6
Peter Jacobs <peterj@mech.uq.edu.au>
2022-12-21 08:23:23 +1000

    Generalize the offset to the Python CoonsPatch and use it in a Lorikeet example.

---
+++ examples/lorikeet/shear-layer-periodic/psl2.py
@@ -0,0 +1,48 @@
+# psl.py
+# Periodic shear layer.
+# PJ 2022-10-09 Adapted from Eilmer4 example.
+#    2022-12-15 Lorikeet variant.
+#    2022-12-21 Extra blocks to test if glitches in vorticity at domain edges are a Paraview issue.
+#
+config.title = "Periodic shear layer."
+print(config.title)
+init_gas_model("ideal-air-gas-model.lua")
+#
+H = 0.010 # y-direction layer thickness in metres
+L = 0.100 # x-direction wavelength in metres
+ymin = -20.0*H; ymax = 20.0*H
+xmin = -L; xmax = L
+#
+box0 = CoonsPatch(p00=Vector3(xmin,ymin), p10=Vector3(xmax,ymin),
+                  p11=Vector3(xmax,ymax), p01=Vector3(xmin,ymax))
+boxes = [box0, box0+Vector3(2*L,0,0), box0-Vector3(2*L,0,0)]
+factor = 1
+grds = [StructuredGrid(psurf=b, niv=int(60*factor)+1, njv=int(120*factor)+1) for b in boxes]
+#
+def initial_flow(x, y):
+    """
+    User-defined function for the initial flow state works in physical space.
+    """
+    p = 100.0e3 # Pa
+    T = 300.0 # K
+    velx0 = 200.0 # m/s subsonic
+    # The lower half of the domain is flowing left and the upper half, right.
+    velx = -velx0
+    if y > H:
+        velx = velx0
+    elif y > -H:
+        velx = y/H * velx0
+    # Add perturbation that is periodic west to east
+    # but gets smaller toward the north and south boundaries.
+    vely = 10.0 * math.exp(-abs(y)/H) * \
+        (math.cos(x/L*math.pi) + math.sin(2*x/L*math.pi))
+    # We use the FlowState object to conveniently set all of
+    # the relevant properties.
+    return FlowState(p=p, velx=velx, vely=vely, T=T)
+#
+bcs = {'iminus':ExchangeBC(),'iplus':ExchangeBC()}
+blks = [FluidBlock(grid=g, i=i, initialState=initial_flow, bcs=bcs) for i,g in enumerate(grds)]
+#
+config.max_time = 10.0e-3  # seconds
+config.max_step = 150000
+config.plot_dt = 0.1e-3
--- src/lib/gdtk/geom/surface.py
+++ src/lib/gdtk/geom/surface.py
@@ -8,7 +8,7 @@ PJ, 2020-07-05
 """
 import numpy as np
 from abc import ABC, abstractmethod
-from copy import copy
+from copy import deepcopy
 from gdtk.geom.vector3 import Vector3, approxEqualVectors
 from gdtk.geom.path import Line
 
@@ -35,16 +35,15 @@ class CoonsPatch(ParametricSurface):
                'offset']
 
     def __init__(self, north=None, east=None, south=None, west=None,
-                 p00=None, p10=None, p11=None, p01=None):
+                 p00=None, p10=None, p11=None, p01=None, offset=Vector3(0,0,0)):
         """
         Initialise from edges or corner points.
         """
-        self.offset = Vector3(0,0,0)
         if all([north, east, south, west]):
-            self.north = copy(north)
-            self.east = copy(east)
-            self.south = copy(south)
-            self.west = copy(west)
+            self.north = deepcopy(north)
+            self.east = deepcopy(east)
+            self.south = deepcopy(south)
+            self.west = deepcopy(west)
             self.p00 = self.south(0.0)
             self.p10 = self.south(1.0)
             self.p01 = self.north(0.0)
@@ -67,13 +66,14 @@ class CoonsPatch(ParametricSurface):
             self.east = Line(p10, p11)
             self.south = Line(p00, p10)
             self.west = Line(p00, p01)
-            self.p00 = copy(p00)
-            self.p10 = copy(p10)
-            self.p11 = copy(p11)
-            self.p01 = copy(p01)
+            self.p00 = deepcopy(p00)
+            self.p10 = deepcopy(p10)
+            self.p11 = deepcopy(p11)
+            self.p01 = deepcopy(p01)
             self.defined_by_corners = True
         else:
             raise Exception("CoonsPatch should be defined by four edges or four corners.")
+        self.offset = offset
         return
 
     def __repr__(self):
@@ -82,7 +82,7 @@ class CoonsPatch(ParametricSurface):
             str += f"p00={self.p00}, p10={self.p10}, p11={self.p10}, p01={self.p10}"
         else:
             str += f"north={self.north}, east={self.east}, south={self.south}, west={self.west}"
-        str += ")"
+        str += f", offset={self.offset})"
         return str
 
     def __call__(self, r, s):
@@ -97,42 +97,60 @@ class CoonsPatch(ParametricSurface):
             (self.p00*(1.0-r)*(1.0-s) + self.p01*(1.0-r)*s +
              self.p10*r*(1.0-s) + self.p11*r*s) + self.offset
         return p
-    
-    def __add__(self, other):
-        """Displace the Coons Patch using a Vector3 object.
-        """
-        if not isinstance(other, Vector3):
-            raise Exception(f"Cannot add a {type(other)} to a CoonsPatch.")
-        
-        offset_patch = copy(self)
-        offset_patch.offset = other
 
-        return offset_patch
+    def __add__(self, offset):
+        """
+        Returns a copy of the original, displaced by a Vector3 object.
+        """
+        if not isinstance(offset, Vector3):
+            raise Exception(f"Cannot add a {type(offset)} to a CoonsPatch.")
+        new_patch = deepcopy(self)
+        new_patch.offset += offset
+        return new_patch
+
+    def __sub__(self, offset):
+        """
+        Returns a copy of the original, displaced by a Vector3 object.
+        """
+        if not isinstance(offset, Vector3):
+            raise Exception(f"Cannot subtract a {type(offset)} from a CoonsPatch.")
+        new_patch = deepcopy(self)
+        new_patch.offset -= offset
+        return new_patch
 
 
 if __name__=='__main__':
-
     p0 = Vector3(x=0.0, y=0.0)
     p1 = Vector3(x=1.0, y=0.0)
     p2 = Vector3(x=1.0, y=1.0)
     p3 = Vector3(x=0.0, y=1.0)
     patch = CoonsPatch(p00=p0, p10=p1, p11=p2, p01=p3)
-
     x = patch(0.5, 0.5)
     xx= patch(np.array([0.5, 0.5]), np.array([0.5, 0.5]))
-
     assert(np.isclose(x.x, xx.x[0]))
     assert(np.isclose(x.y, xx.y[0]))
-
+    #
     n = Line(p3, p2)
     s = Line(p0, p1)
     e = Line(p1, p2)
     w = Line(p0, p3)
     patch = CoonsPatch(north=n, south=s, east=e, west=w)
-
     x = patch(0.5, 0.5)
     xx= patch(np.array([0.5, 0.5]), np.array([0.5, 0.5]))
     print(xx)
-
     assert(np.isclose(x.x, xx.x[0]))
     assert(np.isclose(x.y, xx.y[0]))
+    #
+    patch2 = patch + Vector3(1.0, 0.0, 0.0)
+    x2 = patch2(0.5, 0.5)
+    print(x2)
+    assert(np.isclose(x2.x, x.x+1.0))
+    assert(np.isclose(x2.y, x.y))
+    assert(np.isclose(x2.z, x.z))
+    #
+    patch3 = patch - Vector3(0.0, 1.0, 0.0)
+    x3 = patch3(0.5, 0.5)
+    print(x3)
+    assert(np.isclose(x3.x, x.x))
+    assert(np.isclose(x3.y, x.y-1.0))
+    assert(np.isclose(x3.z, x.z))
