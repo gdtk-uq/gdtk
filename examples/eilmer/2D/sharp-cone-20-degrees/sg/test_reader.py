7cbd04918a7add1a2d39c946dee5bcd0d9047722
Peter Jacobs <peterj@mech.uq.edu.au>
2023-04-08 12:37:49 +1000

    Allow the Python flow.Field class to read classic Eilmer4 files as well.
    The example script is in the cone20 structured-grid example.

---
+++ examples/eilmer/2D/sharp-cone-20-degrees/sg/test_reader.py
@@ -0,0 +1,25 @@
+# test_reader.py
+# Try out the Python service functions for reading grids and flow fields.
+# $ python3 test_reader.py
+# PJ 2023-04-07
+
+from gdtk.geom.sgrid import StructuredGrid
+from gdtk.flow.field import Field
+
+g = StructuredGrid(gzfile="grid/t0000/cone20.grid.b0000.t0000.gz")
+
+print("g.label=", g.label)
+print("g.niv=", g.niv)
+print("g.njv=", g.njv)
+print("g.nkv=", g.nkv)
+# print("g=", g)
+g.write_to_gzip_file("test-grid.gz", format_version="1.1")
+
+f = Field(gzfile="flow/t0000/cone20.flow.b0000.t0000.gz")
+print("f=", f)
+for i in range(f.nic):
+    x = f.data['pos.x'][i,0]
+    y = f.data['pos.y'][i,0]
+    p = f.data['p'][i,0]
+    print(f"i={i} pos=({x},{y}) pressure={p}")
+
--- src/lib/gdtk/flow/field.py
+++ src/lib/gdtk/flow/field.py
@@ -4,66 +4,139 @@ Field class for holding Eilmer field data for a FluidBlock.
 
 PJ, 2023-04-08
 """
-
+import os
+import re
 import zipfile
+import gzip
 import json
 import numpy
 
 class Field():
     """
-    Pick up a block of field-data from an Eilmer simulation.
+    Pick up a block of flow-field data from an Eilmer simulation.
 
     Once in memory, the data are available in a dictionary of numpy arrays.
     """
-    __slots__ = ['ziparchive',
-                 'variables', 'data', 'lengths',
+    __slots__ = ['new_format', 'ziparchive', 'gzfile',
+                 'sim_time', 'variables', 'data', 'lengths',
                  'nic', 'njc', 'nkc', 'structured']
 
-    def __init__(self, ziparchive):
+    def __init__(self, ziparchive=None, gzfile=None):
         """
-        Read a block's field data from one of Daryl's zip archives.
+        Read a block's field data from either a zip-archives or a gzipped file.
 
-        ziparchive: file name of the zip archive.
+        ziparchive: file name of Daryl's zip archive.
+        gzfile: file name of the "classic" Eilmer flow file.
         """
-        if not zipfile.is_zipfile(ziparchive):
-            raise RuntimeError(f"Not a zip arvhive: {ziparchive}")
-        self.ziparchive = ziparchive
-        zf = zipfile.ZipFile(ziparchive, mode='r')
-        with zf.open('header.txt', 'r') as fp:
-            text = fp.read()
-        header = json.loads(text)
-        self.variables = list(header['variables'].keys())
-        self.nic = header['nic']
-        self.njc = header['njc']
-        self.nkc = header['nkc']
-        self.data = {}
-        self.lengths = {} # a length of 1 indicates a scalar quantity
-        for var in self.variables:
-            metaData = header['variables'][var]
-            dataFile = metaData['data']
-            n_cells = metaData['dimension'][0]
-            length = metaData['dimension'][1]
-            with zf.open(dataFile, 'r') as fp:
-                dataBlob = numpy.loadtxt(fp)
-            if length == 1:
-                # Scalar quantity
-                if self.nkc > 1:
-                    dataBlob = dataBlob.reshape(self.nkc, self.njc, self.nic)
+        if ziparchive and not gzfile:
+            self.new_format = True
+        elif not ziparchive and gzfile:
+            self.new_format = False
+        else:
+            raise RuntimeError("Request either a ziparchive or a gzfile as input.")
+        #
+        if self.new_format:
+            # Daryl's zip archive format.
+            if not zipfile.is_zipfile(ziparchive):
+                raise RuntimeError(f"Not a zip archive: {ziparchive}")
+            self.ziparchive = ziparchive
+            za = zipfile.ZipFile(ziparchive, mode='r')
+            text = za.read('header.txt')
+            header = json.loads(text)
+            self.sim_time = header['sim_time']
+            self.variables = list(header['variables'].keys())
+            self.nic = header['nic']
+            self.njc = header['njc']
+            self.nkc = header['nkc']
+            self.data = {}
+            self.lengths = {} # a length of 1 indicates a scalar quantity
+            for var in self.variables:
+                metaData = header['variables'][var]
+                dataFile = metaData['data']
+                n_cells = metaData['dimension'][0]
+                length = metaData['dimension'][1]
+                with za.open(dataFile, 'r') as fp:
+                    dataBlob = numpy.loadtxt(fp)
+                if length == 1:
+                    # Scalar quantity
+                    if self.nkc > 1:
+                        dataBlob = dataBlob.reshape(self.nkc, self.njc, self.nic)
+                    else:
+                        dataBlob = dataBlob.reshape(self.njc, self.nic)
+                    self.data[var] = dataBlob.transpose() # index as [i,j] or [i,j,k]
                 else:
-                    dataBlob = dataBlob.reshape(self.njc, self.nic)
-                self.data[var] = dataBlob.transpose() # index as [i,j] or [i,j,k]
+                    # Array quantity
+                    if self.nkc > 1:
+                        dataBlob = dataBlob.reshape(length, self.nkc, self.njc, self.nic)
+                    else:
+                        dataBlob = dataBlob.reshape(length, self.njc, self.nic)
+                    self.data[var] = dataBlob.transpose() # index as [i,j,n] or [i,j,k,n]
+                self.lengths[var] = length
+        else:
+            # Classic (gzipped text) format for Eilmer flow file.
+            if not os.path.exists(gzfile):
+                raise RuntimeError(f"Could not find gzfile: {gzfile}")
+            self.gzfile = gzfile
+            fp = gzip.open(gzfile, 'rt')
+            text = fp.readline()
+            if text.strip() != "structured_grid_flow 1.0":
+                raise RuntimeError(f"Did not see correct first line: {text}")
+            text = fp.readline() # label
+            text = fp.readline() # sim_time
+            items = re.split(":", text)
+            self.sim_time = float(items[1])
+            text = fp.readline() # variables:
+            items = re.split(":", text)
+            nvariables = int(items[1])
+            print(f"nvariables={nvariables}")
+            text = fp.readline().strip() # The variable names
+            # Assume no spaces within each name.
+            items = text.split(' ')
+            self.variables = [item.replace('"', '') for item in items]
+            text = fp.readline() # dimensions
+            items = re.split(":", text)
+            dimensions = int(items[1])
+            text = fp.readline() # nicell
+            items = re.split(":", text)
+            self.nic = int(items[1])
+            text = fp.readline() # njcell
+            items = re.split(":", text)
+            self.njc = int(items[1])
+            text = fp.readline() # nkcell
+            items = re.split(":", text)
+            self.nkc = int(items[1])
+            self.data = {}
+            if dimensions==2:
+                if not self.nkc==1:
+                    raise RuntimeError(f"For a 2D grid, expect nkc==1 but got {self.nkc}")
+                for var in self.variables:
+                    self.data[var] = numpy.zeros((self.nic,self.njc), float)
+                for j in range(self.njc):
+                    for i in range(self.nic):
+                        text = fp.readline().strip()
+                        items = text.split(' ')
+                        assert(nvariables == len(items), "Wrong number of items on data line.")
+                        for n in range(nvariables):
+                            self.data[self.variables[n]][i,j] = float(items[n])
             else:
-                # Array quantity
-                if self.nkc > 1:
-                    dataBlob = dataBlob.reshape(length, self.nkc, self.njc, self.nic)
-                else:
-                    dataBlob = dataBlob.reshape(length, self.njc, self.nic)
-                self.data[var] = dataBlob.transpose() # index as [i,j,n] or [i,j,k,n]
-            self.lengths[var] = length
+                # 3D grid
+                for var in self.variables:
+                    self.data[var] = numpy.zeros((self.nic,self.njc,self.nkc), float)
+                for k in range(self.nkc):
+                    for j in range(self.njc):
+                        for i in range(self.nic):
+                            text = fp.readline().strip()
+                            items = text.split(' ')
+                            assert(nvariables == len(items), "Wrong number of items on data line.")
+                            for n in range(nvariables):
+                                self.data[self.variables[n]][i,j,k] = float(items[n])
+            fp.close()
         return
 
     def __repr__(self):
-        str = f"Field(ziparchive={self.ziparchive}"
+        str = "Field("
+        str += f"ziparchive={self.ziparchive}" if self.new_format else f"gzfile={self.gzfile}"
+        str += f", sim_time={self.sim_time}"
         str += f", variables={self.variables}"
         str += f", nic={self.nic}, njc={self.njc}, nkc={self.nkc}"
         str += ")"
