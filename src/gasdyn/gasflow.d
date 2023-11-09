e4ed9a1fdcb9290760357b5bd04fa8dc1cbcdd80
Peter Jacobs <peterj@mech.uq.edu.au>
2023-11-10 09:25:42 +1000

    gasdyn: remove the unnecessary call to update thermo for the initial state2
    ...in the normal_shock functions.

--- src/gasdyn/gasflow.d
+++ src/gasdyn/gasflow.d
@@ -101,8 +101,7 @@ number[] shock_ideal(ref const(GasState) state1, number Vs, ref GasState state2,
 } // end shock_ideal()
 
 number[] normal_shock(ref const(GasState) state1, number Vs, ref GasState state2,
-                      GasModel gm, double rho_tol=1.0e-6, double T_tol = 0.1,
-		      bool update_initial_state2=true)
+                      GasModel gm, double rho_tol=1.0e-6, double T_tol = 0.1)
 /**
  * Computes post-shock conditions in a shock-stationary frame.
  * (This version iterates on rho2, T2.)
@@ -114,8 +113,6 @@ number[] normal_shock(ref const(GasState) state1, number Vs, ref GasState state2
  *   gm: the gas model in use
  *   rho_tol: tolerance in kg/m^3
  *   T_tol: temperature tolerance in degrees K
- *   update_initial_state2: by default do this initial update.
- *      Chris James has a case where he does not want to do this update.
  *
  * Returns: the dynamic array [V2, Vg], containing the post-shock gas speeds,
  *   V2 in the shock-reference frame,
@@ -125,7 +122,6 @@ number[] normal_shock(ref const(GasState) state1, number Vs, ref GasState state2
 {
     // Assume a chemically-frozen gas, so copy mass-fractions and the like from state1.
     state2.copy_values_from(state1);
-    if (update_initial_state2) { gm.update_thermo_from_pT(state2); }
     // Initial guess via ideal gas relations.
     number[] velocities = shock_ideal(state1, Vs, state2, gm);
     number V2 = velocities[0]; number Vg = velocities[1];
@@ -215,7 +211,6 @@ number[] normal_shock_1(ref const(GasState) state1, number Vs, ref GasState stat
 {
     // Assume a chemically-frozen gas, so copy mass-fractions and the like from state1.
     state2.copy_values_from(state1);
-    gm.update_thermo_from_pT(state2);
     // Initial guess via ideal gas relations.
     number[] velocities = shock_ideal(state1, Vs, state2, gm);
     number V2 = velocities[0]; number Vg = velocities[1];
