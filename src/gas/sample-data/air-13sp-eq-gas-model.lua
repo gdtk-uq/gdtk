6b807919d0d4510a33c4e61d9989ee69ac02aba1
Peter Jacobs <peterj@mech.uq.edu.au>
2023-11-15 09:45:31 +1000

    Gas: add a couple of input files for generating 1T gas models.
    This location is a convenient place to find these files.
    The corresponding equilibrium gas models will need the fully-specified
    1T gas model files that can be generated from these input files.

--- src/gas/sample-data/air-13sp-eq-gas-model.lua
+++ src/gas/sample-data/air-13sp-eq-gas-model.lua
@@ -1,5 +1,10 @@
 model = "EquilibriumGas"
 
+-- Note that this equilibrium-chemistry model requires
+-- an fully-specified thermally-perfect 1T gas model.
+-- This may be generated from a list of species with the command:
+-- $ prep-gas air-13sp-1T-input.lua air-13sp-gas-model.lua
+
 EquilibriumGas = {
   mixtureName = 'air5species',
   tpGasEqFile = 'air-13sp-gas-model.lua',
--- src/gas/sample-data/air-5sp-eq.lua
+++ src/gas/sample-data/air-5sp-eq.lua
@@ -1,5 +1,10 @@
 model = "EquilibriumGas"
 
+-- Note that this equilibrium-chemistry model requires
+-- an fully-specified thermally-perfect 1T gas model.
+-- This may be generated from a list of species with the command:
+-- $ prep-gas air-5sp-1T-input.lua air-5sp-1T.lua
+
 EquilibriumGas = {
   mixtureName = 'air5species',
   tpGasEqFile = 'air-5sp-1T.lua',
