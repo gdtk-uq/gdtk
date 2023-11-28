685e5350960aabd2737424ecffd0af146b3d84cf
Rowan J. Gollan <r.gollan@uq.edu.au>
2023-11-28 21:24:34 +1000

    examples/lmr: A very minimal gridpro import example.
    This example shows how to import a gridpro grid for the
    prep-grid stage.

---
+++ examples/lmr/3D/gridpro-import/job.lua
@@ -0,0 +1,28 @@
+-- Author: Rowan J. Gollan
+-- Date: 2016-04-22
+--
+-- History:
+--   2018-02-14 : Use Fabian Zander's example with BC labels.
+--   2023-11-28 : Update for use in staged preparation
+
+gproGrid = "blk.tmp"
+gproConn = "blk.tmp.conn"
+gproPty = "blk.tmp.pty"
+
+config.title = "Gridpro importer test"
+config.dimensions = 3
+
+-- Read in GridPro grids.
+-- The GridPro grid file contains all the blocks in one file.
+-- We'll separate these into individual pieces, and store
+-- a list of grids.
+scale = 1.0
+grids = importGridproGrid(gproGrid, scale)
+
+for i,g in ipairs(grids) do
+   registerGrid{grid=g, fsTag="initial"}
+end
+
+importGridproConnectivity(gproConn)
+importGridproBCs(gproPty)
+
