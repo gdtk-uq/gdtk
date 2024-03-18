gdtk-uq/gdtk

commit 9850470e0c16a3bf4b94f8120318038f2918d526

lmr: make loads header lines compatible with pandas and gnuplot labels for columns
Also, added loads request to convex-ramp steady solve example.

src/lmr/loads.d
@@ -7,7 +7,7 @@
  *
  * 2018-01-20 Some code clean up by PJ;
  * [TODO] more is needed for MPI context.
- * 
+ *
  * History:
  *   2024-03-11 RJG update for eilmer 5
  */
@@ -167,12 +167,11 @@ string generate_boundary_load_file(int blkid, int bcid, int current_loads_tindx,
     string fname = loadsFilename(current_loads_tindx, blkid, bcid, group);
     // create file
     auto f = File(fname, "w");
-    f.writeln("# t = ", sim_time);
-    f.writeln("# 1:pos.x 2:pos.y 3:pos.z 4:n.x 5:n.y 6:n.z 7:area 8:cellWidthNormalToSurface 9:outsign "~
-              "10:p 11:rho 12:T 13:velx 14:vely 15:velz 16:mu 17:a "~
-              "18:Re 19:y+ "~
-              "20:tau_wall_x 21:tau_wall_y 22:tau_wall_z "~
-              "23:q_total 24:q_cond 25:q_diff");
+    f.writeln("pos.x pos.y pos.z n.x n.y n.z area cellWidthNormalToSurface outsign"~
+              " p rho T vel.x vel.y vel.z mu a"~
+              " Re y+"~
+              " tau_wall.x tau_wall.y tau_wall.z"~
+              " q_total q_cond q_diff");
     f.close();
     return fname;
 } // end generate_boundary_load_file()
@@ -212,7 +211,7 @@ void wait_for_current_indx_dir(int current_loads_tindx)
 void init_loads_metadata_file()
 {
     string fname = lmrCfg.loadsDir ~ "/" ~ lmrCfg.loadsPrefix ~ "-metadata";
-    std.file.write(fname, "# 1:loads_index    2:sim_time    3:step\n");
+    std.file.write(fname, "loads_index    sim_time    step\n");
 }
 
 void update_loads_metadata_file(double sim_time, int step, int current_loads_tindx)

examples/lmr/2D/convex-ramp/steady.lua
@@ -21,13 +21,14 @@ flowDict = {
 bcDict = {
    inflow=InFlowBC_Supersonic:new{flowState=inflow},
    outflow=OutFlowBC_FixedPT:new{p_outside=p_inf/5, T_outside=T_inf},
-   noslipwall=WallBC_NoSlip_FixedT:new{Twall=T_wall},
+   noslipwall=WallBC_NoSlip_FixedT:new{Twall=T_wall, group='loads'},
 }
 --
 makeFluidBlocks(bcDict, flowDict)
 mpiDistributeBlocks{ntasks=8}
 --
-config.flux_calculator= "ausmdv"
+config.write_loads = true
+config.flux_calculator = "ausmdv"
 config.interpolation_order = 2
 config.viscous = true
 config.thermo_interpolator = "rhop"

commit 9230dde8de356cb8da62a700247e4ef08742f353

lmr: fixed bug in count_written_loads().
The count_written_loads() routine was failing to return 0 upon restarting
a simulation which had yet to write out any loads data to disk.

This bug was introduced in commit 9850470e.

src/lmr/loads.d
@@ -181,12 +181,13 @@ int count_written_loads()
     int nPrevLoadsWritten;
     string fname = lmrCfg.loadsDir ~ "/" ~ lmrCfg.loadsPrefix ~ "-metadata";
     if (exists(fname)) {
-        auto finalLine = readText(fname).splitLines()[$-1];
-        if (finalLine[0] == '#') {
-            // looks like we found a single comment line.
+        auto lines = readText(fname).splitLines();
+        if (lines.length == 1) {
+            // looks like we found a single line.
             nPrevLoadsWritten = 0;
-            } else {
+        } else {
             // assume we have a valid line to work with
+            auto finalLine = lines[$-1];
             nPrevLoadsWritten = to!int(finalLine.split[0]) + 1;
         }
     } else {