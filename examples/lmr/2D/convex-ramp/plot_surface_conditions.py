36aa80a2f07d48c98e115ef5221421889d87363c
Rowan J. Gollan <r.gollan@uq.edu.au>
2024-03-19 09:07:55 +1000

    examples/lmr: convex-ramp plotting now uses pyplot...
    ... and I added a --save-figure option to the script
    so I could generate a figure for the VKI paper.

--- examples/lmr/2D/convex-ramp/plot_surface_conditions.py
+++ examples/lmr/2D/convex-ramp/plot_surface_conditions.py
@@ -1,8 +1,22 @@
 # plot_surface_conditions.py
 # Assemble the ramp loads by picking data out of the loads files.
 # Peter J. 2024-03-13
+# RJG, 2024-03-19
+#   + change to pyplot usage
+#   + add option to save figure
 #
+# Usage:
+# 
+# For plotting to screen:
+# $ python[3] plot_surface_conditions.py
+#
+# For saving figure to disk:
+# $ python[3] plot_surface_conditions.py --save-figure
+# 
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
 #
 # We know that the ramp surface consists of the southern boundaries
 # of the lower blocks.  These will be the even-number blocks.
@@ -19,24 +33,26 @@ for i in blk_ids:
     blk_dfs.append(pd.read_table(f, sep='\s+'))
 ramp_df = pd.concat(blk_dfs)
 
-import numpy as np
 xp = np.loadtxt('mohammadian-figure-12-p_p_inf.data')
 xq = np.loadtxt('mohammadian-figure-13-heat-flux.data')
 
-import pylab as pl
-pl.subplot(2,1,1)
-pl.plot(ramp_df['pos.x']*1000, ramp_df['q_total']/1000, '-b', label='Eilmer')
-pl.plot(xq[:,0]*25.4, xq[:,1]*11.5, '+k', label='Mohammadian (1972)')
-pl.xlabel(''); pl.ylabel('q, kW/m^2')
-pl.xlim(0,250); pl.ylim(0,120)
-pl.legend(loc='upper right')
-pl.title('Surface conditions on convex ramp')
-pl.subplot(2,1,2)
-pl.plot(ramp_df['pos.x']*1000, ramp_df['p']/1000, '-b', label='Eilmer')
-pl.plot(xp[:,0]*25.4, xp[:,1]*66.43/1000, '+k', label='Mohammadian (1972)')
-pl.xlabel('x, mm'); pl.ylabel('p, kPa')
-pl.xlim(0,240); pl.ylim(0,2.5)
-pl.legend(loc='upper right')
-pl.title('')
-pl.show()
-
+plt.rcParams.update({'font.size': 14,
+                     'font.family': 'monospace',
+                     'font.monospace': ['Courier New']})
+fig, (ax0, ax1) = plt.subplots(2,1)
+ax0.plot(ramp_df['pos.x']*1000, ramp_df['q_total']/1000, '-b', label='Eilmer')
+ax0.plot(xq[:,0]*25.4, xq[:,1]*11.5, '+k', label='Mohammadian (1972)')
+ax0.set_xlabel(''); ax0.set_ylabel('q, kW/m^2')
+ax0.set_xlim(0,250); ax0.set_ylim(0,120)
+ax0.legend(loc='upper right')
+ax0.set_title('Surface conditions on convex ramp')
+ax1.plot(ramp_df['pos.x']*1000, ramp_df['p']/1000, '-b', label='Eilmer')
+ax1.plot(xp[:,0]*25.4, xp[:,1]*66.43/1000, '+k', label='Mohammadian (1972)')
+ax1.set_xlabel('x, mm'); ax1.set_ylabel('p, kPa')
+ax1.set_xlim(0,250); ax1.set_ylim(0,2.5)
+ax1.legend(loc='upper right')
+ax1.set_title('')
+if len(sys.argv) == 2 and sys.argv[1] == "--save-figure":
+    fig.savefig("convex-ramp-surf-conds.png", transparent=True, dpi=300)
+else:
+    plt.show()
