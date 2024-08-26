39153d5d7dc2d354db7df0524b63f37d7fb83cfd
Peter Jacobs <peterj@mech.uq.edu.au>
2024-08-26 20:23:29 +1000

    lmr: make better use of the YAML probe-flow output in sod-shock-tube tests

--- examples/lmr/3D/sod-shock-tube/sg/test_sod_sg.py
+++ examples/lmr/3D/sod-shock-tube/sg/test_sod_sg.py
@@ -9,6 +9,7 @@ import pytest
 import subprocess
 import re
 import os
+import yaml
 
 # This is used to change to local directory so that subprocess runs nicely.
 @pytest.fixture(autouse=True)
@@ -60,32 +61,28 @@ def test_snapshot():
     assert os.path.exists('lmrsim/vtk')
 
 def test_probe_post_shock_region():
-    refs = {'rho':0.2647, 'p':30.2e3, 'T':398.0, 'vel.x':293.0}
-    vals = {}
+    expected_result = {'rho':0.2647, 'p':30.2e3, 'T':398.0, 'vel.x':293.0}
     cmd = 'lmr probe-flow --names=rho,p,T,vel.x --location=0.78,0.025,0.025'
     proc = subprocess.run(cmd.split(), capture_output=True, text=True)
     assert proc.returncode == 0, "Failed during: " + cmd
-    lines = proc.stdout.split("\n")
-    for name in refs.keys():
-        for line in lines:
-            if line.find(name) >= 0:
-                vals[name] = float(line.strip().split()[-1])
-        assert abs(vals[name] - refs[name])/refs[name] < 0.01, f"Failed to match {name}"
-    # assert False, f"refs={refs}, vals={vals}"
+    probe_result = yaml.safe_load(proc.stdout)
+    for key in expected_result.keys():
+        val = float(probe_result['pointdata'][0][key])
+        v = expected_result[key]
+        assert abs(val - v)/(abs(v)+1.0) < 0.01, "Failed to see correct "+key
+    return
 
 def test_probe_expanded_driver_region():
-    refs = {'rho':0.4271, 'p':30.2e3, 'T':247.0, 'vel.x':293.0}
-    vals = {}
+    expected_result = {'rho':0.4271, 'p':30.2e3, 'T':247.0, 'vel.x':293.0}
     cmd = 'lmr probe-flow --names=rho,p,T,vel.x --location=0.6,0.025,0.025'
     proc = subprocess.run(cmd.split(), capture_output=True, text=True)
     assert proc.returncode == 0, "Failed during: " + cmd
-    lines = proc.stdout.split("\n")
-    for name in refs.keys():
-        for line in lines:
-            if line.find(name) >= 0:
-                vals[name] = float(line.strip().split()[-1])
-        assert abs(vals[name] - refs[name])/refs[name] < 0.01, f"Failed to match {name}"
-    # assert False, f"refs={refs}, vals={vals}"
+    probe_result = yaml.safe_load(proc.stdout)
+    for key in expected_result.keys():
+        val = float(probe_result['pointdata'][0][key])
+        v = expected_result[key]
+        assert abs(val - v)/(abs(v)+1.0) < 0.01, "Failed to see correct "+key
+    return
 
 def test_cleanup():
     cmd = "rm -rf ./lmrsim ideal-air.gas"
