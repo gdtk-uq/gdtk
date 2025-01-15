gdtk-uq/gdtk

commit 664378abc53dbcaf21988a49e136df9bd95a013f

examples/lmr/diffuser-busemann: Update test values for macos...
... and make the invocation of Python scripts more portable,
according to PEP 394.

Also moved the generate-contour.py script into a common/ area
rather than have duplicates in each grid-specific directory.

examples/lmr/2D/diffuser-busemann/coarse-grid/makefile
@@ -3,7 +3,9 @@ gas:
 	lmr prep-gas -i ideal-air.inp -o ideal-air.lua
 
 grid:
-	./generate-contour.py
+	cd ../common; ./generate-contour.py
+	mv ../common/bd-contour.dat .
+	mv ../common/bd-props.txt .
 	sed -i '1 s/./# &/' bd-contour.dat
 	lmr prep-grid
 

examples/lmr/2D/diffuser-busemann/common/generate-contour.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 """
 Calls the busemann.py module to generate a contour file.
 
 Usage:
 
-  > ./generate-contour.py
+  > python generate-contour.py
 
 .. Author: RJG
 .. Date: 2024-07-06
@@ -21,7 +21,7 @@ bd = BusemannDiffuser(M2, theta_23)
 r = 1.0
 dtheta = 0.001
 bd.generate_contour(r, dtheta)
-npts = 50
+npts = 200
 bd.write_contour('bd-contour.dat', npts)
 
 props = bd.properties()

examples/lmr/2D/diffuser-busemann/fine-grid/makefile
@@ -7,7 +7,9 @@ gas:
 	lmr prep-gas -i ideal-air.inp -o ideal-air.lua
 
 grid:
-	./generate-contour.py
+	cd ../common; ./generate-contour.py
+	mv ../common/bd-contour.dat .
+	mv ../common/bd-props.txt .
 	sed -i '1 s/./# &/' bd-contour.dat
 	lmr prep-grid
 

examples/lmr/2D/diffuser-busemann/fine-grid/generate-contour.py
@@ -1,35 +1,0 @@
-#!/usr/bin/python3
-"""
-Calls the busemann.py module to generate a contour file.
-
-Usage:
-
-  > ./generate-contour.py
-
-.. Author: RJG
-.. Date: 2024-07-06
-"""
-
-from gdtk.busemann import BusemannDiffuser
-from math import asin
-
-M2 = 3.0
-k = 1.4
-theta_23 = asin(k/M2)
-bd = BusemannDiffuser(M2, theta_23)
-
-r = 1.0
-dtheta = 0.001
-bd.generate_contour(r, dtheta)
-npts = 200
-bd.write_contour('bd-contour.dat', npts)
-
-props = bd.properties()
-
-with open('bd-props.txt', "w") as f:
-    for k, v in zip(props._fields, props):
-        f.write(f"{k} = {v:.6f}\n")
-
-  
-  
-

examples/lmr/2D/diffuser-busemann/fine-grid/test_diffuser_busemann.py
@@ -13,6 +13,7 @@ import subprocess
 import re
 import os
 import shlex
+import sys
 
 # This is used to change to local directory so that subprocess runs nicely.
 @pytest.fixture(autouse=True)
@@ -20,8 +21,20 @@ def change_test_dir(request, monkeypatch):
     monkeypatch.chdir(request.fspath.dirname)
 
 expected_reason_for_stop = "relative-global-residual-target"
-expected_number_steps = 39
 expected_final_cfl = 1.000e+03
+# RJG, 2025-01-14
+# The expected number of steps differs on linux and macos due to
+# floating-point precision subtleties. This affects growth of
+# CFL and the test for convergence based on relative residuals.
+#
+# Values for MacOs
+# ----------------
+# Compiler: LLVM D Compiler v1.40.0 with LLVM 19.1.6
+# CPU: Apple M2 Pro
+if (sys.platform == 'linux'):
+    expected_number_steps = 39
+else:
+    expected_number_steps = 46
 
 def expected_output(proc, check_start_step=False):
     steps = 0

commit d09e5fdd270a97c56a1078467d09b8d1299e501d

examples/lmr/diffuser-busemann: Fix test failure in introduced in commit 664378ab
Recently, I consolidated the generate-contour.py script in one place.
However, I failed to realise that I had different hard-coded values
for how many points to use in the contour for the coarse-grid and
fine-grid versions. The difference mattered in terms of convergence
behaviour (but not in terms of functional output).

The solution is to generalise the generate-contour.py script so
that the number of points is passed as a command-line argument.
Using 50 points for the coarse grid and 200 points for the fine grid,
the original behaviour is recovered (as far as the tests are concerned).

examples/lmr/2D/diffuser-busemann/coarse-grid/makefile
@@ -3,7 +3,7 @@ gas:
 	lmr prep-gas -i ideal-air.inp -o ideal-air.lua
 
 grid:
-	cd ../common; ./generate-contour.py
+	cd ../common; ./generate-contour.py 50
 	mv ../common/bd-contour.dat .
 	mv ../common/bd-props.txt .
 	sed -i '1 s/./# &/' bd-contour.dat

examples/lmr/2D/diffuser-busemann/common/generate-contour.py
@@ -4,7 +4,10 @@ Calls the busemann.py module to generate a contour file.
 
 Usage:
 
-  > python generate-contour.py
+  > ./generate-contour.py npts
+
+  where 'npts' is an integer number of points used to
+  represent the contour.
 
 .. Author: RJG
 .. Date: 2024-07-06
@@ -12,6 +15,16 @@ Usage:
 
 from gdtk.busemann import BusemannDiffuser
 from math import asin
+import sys
+
+if len(sys.argv) != 2:
+    print("Incorrect number of arguments; exactly 1 expected.")
+    print("Usage: ./generate-contour.py npts")
+    print("")
+    print("   where 'npts' is an integer")
+    sys.exit(1)
+
+npts = int(sys.argv[1])
 
 M2 = 3.0
 k = 1.4
@@ -21,7 +34,6 @@ bd = BusemannDiffuser(M2, theta_23)
 r = 1.0
 dtheta = 0.001
 bd.generate_contour(r, dtheta)
-npts = 200
 bd.write_contour('bd-contour.dat', npts)
 
 props = bd.properties()

examples/lmr/2D/diffuser-busemann/fine-grid/makefile
@@ -7,7 +7,7 @@ gas:
 	lmr prep-gas -i ideal-air.inp -o ideal-air.lua
 
 grid:
-	cd ../common; ./generate-contour.py
+	cd ../common; ./generate-contour.py 200
 	mv ../common/bd-contour.dat .
 	mv ../common/bd-props.txt .
 	sed -i '1 s/./# &/' bd-contour.dat
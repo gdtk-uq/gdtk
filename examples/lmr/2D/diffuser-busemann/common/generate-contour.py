d09e5fdd270a97c56a1078467d09b8d1299e501d
Rowan J. Gollan <r.gollan@uq.edu.au>
2025-01-15 20:10:54 +1000

    examples/lmr/diffuser-busemann: Fix test failure in introduced in commit 664378ab
    Recently, I consolidated the generate-contour.py script in one place.
    However, I failed to realise that I had different hard-coded values
    for how many points to use in the contour for the coarse-grid and
    fine-grid versions. The difference mattered in terms of convergence
    behaviour (but not in terms of functional output).
    
    The solution is to generalise the generate-contour.py script so
    that the number of points is passed as a command-line argument.
    Using 50 points for the coarse grid and 200 points for the fine grid,
    the original behaviour is recovered (as far as the tests are concerned).

--- examples/lmr/2D/diffuser-busemann/common/generate-contour.py
+++ examples/lmr/2D/diffuser-busemann/common/generate-contour.py
@@ -4,7 +4,10 @@ Calls the busemann.py module to generate a contour file.
 
 Usage:
 
-  > python generate-contour.py
+  > ./generate-contour.py npts
+
+  where 'npts' is an integer number of points used to
+  represent the contour.
 
 .. Author: RJG
 .. Date: 2024-07-06
@@ -12,6 +15,16 @@ Usage:
 
 from gdtk.busemann import BusemannDiffuser
 from math import asin
+import sys
+
+if len(sys.argv) != 2:
+    print("Incorrect number of arguments; exactly 1 expected.")
+    print("Usage: ./generate-contour.py npts")
+    print("")
+    print("   where 'npts' is an integer")
+    sys.exit(1)
+
+npts = int(sys.argv[1])
 
 M2 = 3.0
 k = 1.4
@@ -21,7 +34,6 @@ bd = BusemannDiffuser(M2, theta_23)
 r = 1.0
 dtheta = 0.001
 bd.generate_contour(r, dtheta)
-npts = 200
 bd.write_contour('bd-contour.dat', npts)
 
 props = bd.properties()
