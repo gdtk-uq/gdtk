bd7874649f37344ee8ee2b36ed14a9a55b3002a3
Peter Jacobs <peterj@mech.uq.edu.au>
2025-01-21 13:05:39 +1000

    lmr: Updated driven shock tube test to include an exercise for TransientProfile.

--- examples/lmr/2D/shock-tube-driven-inflow/test_shock_tube_driven_inflow.py
+++ examples/lmr/2D/shock-tube-driven-inflow/test_shock_tube_driven_inflow.py
@@ -1,8 +1,8 @@
 # Author: Peter J. and Rowan Gollan
-# Date: 2024-03-03
+# Date: 2025-01-18, 2025-01-21
 #
 # Integration test for the simulation of a shock tube with driven inflow,
-# exercising InFlowBC_StaticProfile.
+# exercising InFlowBC_StaticProfile and InFlowBC_TransientProfile.
 # Note that the tests are not independent and must be run in order of appearance.
 
 import pytest
@@ -27,17 +27,17 @@ def test_prep_static_profile():
     proc = subprocess.run(cmd.split(), capture_output=True, text=True)
     assert proc.returncode == 0, "Failed during: " + cmd
 
-def test_prep_grid():
+def test_prep_grid_A():
     cmd = "lmr prep-grid --job=grid.lua"
     proc = subprocess.run(cmd.split(), capture_output=True, text=True)
     assert proc.returncode == 0, "Failed during: " + cmd
 
-def test_prep_sim():
+def test_prep_sim_A():
     cmd = "lmr prep-sim --job=transient-A.lua"
     proc = subprocess.run(cmd.split(), capture_output=True, text=True)
     assert proc.returncode == 0, "Failed during: " + cmd
 
-def test_run():
+def test_run_A():
     cmd = "lmr run"
     proc = subprocess.run(cmd.split(), capture_output=True, text=True)
     assert proc.returncode == 0, "Failed during: " + cmd
@@ -59,13 +59,13 @@ def test_run():
       "Failed to arrive at expected time on final step."
     print("reason=", reason)
 
-def test_snapshot():
+def test_snapshot_A():
     cmd = "lmr snapshot2vtk --all"
     proc = subprocess.run(cmd.split())
     assert proc.returncode == 0, "Failed during: " + cmd
     assert os.path.exists('lmrsim/vtk')
 
-def test_probe_post_shock_region():
+def test_probe_post_shock_region_A():
     # Expected values come from the analytic solution.
     expected_result = {'rho':0.0417124, 'p':7152.19, 'T':597.22, 'vel.x':587.33}
     cmd = 'lmr probe-flow --names=rho,p,T,vel.x --location=0.90,0.025,0.0'
@@ -78,7 +78,7 @@ def test_probe_post_shock_region():
         assert abs(val - v)/(abs(v)+1.0) < 0.01, "Failed to see correct "+key
     return
 
-def test_probe_before_shock_region():
+def test_probe_before_shock_region_A():
     expected_result = {'rho':0.0124931, 'p':1.0e3, 'T':278.8, 'vel.x':0.0}
     cmd = 'lmr probe-flow --names=rho,p,T,vel.x --location=0.95,0.025,0.0'
     proc = subprocess.run(cmd.split(), capture_output=True, text=True)
@@ -90,7 +90,87 @@ def test_probe_before_shock_region():
         assert abs(val - v)/(abs(v)+1.0) < 0.01, "Failed to see correct "+key
     return
 
-def test_cleanup():
+def test_cleanup_A():
     cmd = "rm -rf ./lmrsim ideal-air.gas static-profile.data"
+    proc = subprocess.run(cmd.split())
+    assert proc.returncode == 0, "Failed during: " + cmd
+
+# Tests for TransientProfile follow.
+
+def test_prep_gas_B():
+    cmd = "lmr prep-gas -i ideal-air.lua -o ideal-air.gas"
+    proc = subprocess.run(cmd.split())
+    assert proc.returncode == 0, "Failed during: " + cmd
+
+def test_prep_transient_profile():
+    cmd = "python3 prepare-transient-profile.py"
+    proc = subprocess.run(cmd.split(), capture_output=True, text=True)
+    assert proc.returncode == 0, "Failed during: " + cmd
+
+def test_prep_grid_B():
+    cmd = "lmr prep-grid --job=grid.lua"
+    proc = subprocess.run(cmd.split(), capture_output=True, text=True)
+    assert proc.returncode == 0, "Failed during: " + cmd
+
+def test_prep_sim_B():
+    cmd = "lmr prep-sim --job=transient-B.lua"
+    proc = subprocess.run(cmd.split(), capture_output=True, text=True)
+    assert proc.returncode == 0, "Failed during: " + cmd
+
+def test_run_B():
+    cmd = "lmr run"
+    proc = subprocess.run(cmd.split(), capture_output=True, text=True)
+    assert proc.returncode == 0, "Failed during: " + cmd
+    reason = ""
+    steps = 0
+    t = 0.0
+    lines = proc.stdout.split("\n")
+    for line in lines:
+        if line.find("STOP-REASON") != -1:
+            reason = ' '.join(line.split()[1:]).strip()
+        if line.find("FINAL-STEP") != -1:
+            steps = int(line.split()[1])
+        if line.find("FINAL-TIME") != -1:
+            t = float(line.split()[1])
+    assert reason.startswith("maximum-time"), \
+      "Failed to stop for the expected reason."
+    assert abs(steps-401) < 5, "Failed to take correct number of steps."
+    assert abs(t - 0.0005)/0.0005 < 0.01, \
+      "Failed to arrive at expected time on final step."
+    print("reason=", reason)
+
+def test_snapshot_B():
+    cmd = "lmr snapshot2vtk --all"
+    proc = subprocess.run(cmd.split())
+    assert proc.returncode == 0, "Failed during: " + cmd
+    assert os.path.exists('lmrsim/vtk')
+
+def test_probe_post_shock_region_B():
+    # Expected values come from the analytic solution.
+    expected_result = {'rho':0.0417124, 'p':7152.19, 'T':597.22, 'vel.x':587.33}
+    cmd = 'lmr probe-flow --names=rho,p,T,vel.x --location=0.80,0.025,0.0'
+    proc = subprocess.run(cmd.split(), capture_output=True, text=True)
+    assert proc.returncode == 0, "Failed during: " + cmd
+    probe_result = yaml.safe_load(proc.stdout)
+    for key in expected_result.keys():
+        val = float(probe_result['pointdata'][0][key])
+        v = expected_result[key]
+        assert abs(val - v)/(abs(v)+1.0) < 0.01, "Failed to see correct "+key
+    return
+
+def test_probe_before_shock_region_B():
+    expected_result = {'rho':0.0124931, 'p':1.0e3, 'T':278.8, 'vel.x':0.0}
+    cmd = 'lmr probe-flow --names=rho,p,T,vel.x --location=0.87,0.025,0.0'
+    proc = subprocess.run(cmd.split(), capture_output=True, text=True)
+    assert proc.returncode == 0, "Failed during: " + cmd
+    probe_result = yaml.safe_load(proc.stdout)
+    for key in expected_result.keys():
+        val = float(probe_result['pointdata'][0][key])
+        v = expected_result[key]
+        assert abs(val - v)/(abs(v)+1.0) < 0.01, "Failed to see correct "+key
+    return
+
+def test_cleanup_B():
+    cmd = "rm -rf ./lmrsim ideal-air.gas transient-profile.zip"
     proc = subprocess.run(cmd.split())
     assert proc.returncode == 0, "Failed during: " + cmd
