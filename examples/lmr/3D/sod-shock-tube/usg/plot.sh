0dc909a0ddd6bac530e1e00cb9e16b773d765dee
Peter Jacobs <peterj@mech.uq.edu.au>
2025-02-06 15:36:58 +1000

    lmr: make use of the column headers for the sod shock tube example

--- examples/lmr/3D/sod-shock-tube/usg/plot.sh
+++ examples/lmr/3D/sod-shock-tube/usg/plot.sh
@@ -10,7 +10,7 @@ set xlabel "x, m"
 set ylabel "Pressure, Pa"
 set xrange [0.0:1.0]
 set yrange [0.0:120.0e3]
-plot "sod_new.dat" using 1:5 title "lmr (usg)" with points ps 1 pt 1, \
+plot "sod_new.dat" using "pos.x":"p" title "lmr (usg)" with points ps 1 pt 1, \
      "../analytic/analytic.data" using 1:3 title "analytic" with lines lt 1
 EOF
 
@@ -22,7 +22,7 @@ set xlabel "x, m"
 set ylabel "Density, kg/m**3"
 set xrange [0.0:1.0]
 set yrange [0.0:1.2]
-plot "sod_new.dat" using 1:4 title "lmr (usg)" with points ps 1 pt 1, \
+plot "sod_new.dat" using "pos.x":"rho" title "lmr (usg)" with points ps 1 pt 1, \
      "../analytic/analytic.data" using 1:2 title "analytic" with lines lt 1
 EOF
 
@@ -34,7 +34,7 @@ set xlabel "x, m"
 set ylabel "Velocity, m/s"
 set xrange [0.0:1.0]
 set yrange [0.0:500.0]
-plot "sod_new.dat" using 1:7 title "lmr (usg)" with points ps 1 pt 1, \
+plot "sod_new.dat" using "pos.x":"vel.x" title "lmr (usg)" with points ps 1 pt 1, \
      "../analytic/analytic.data" using 1:5 title "analytic" with lines lt 1
 EOF
 
@@ -46,6 +46,6 @@ set xlabel "x, m"
 set ylabel "Temperature, K"
 set xrange [0.0:1.0]
 set yrange [0.0:500.0]
-plot "sod_new.dat" using 1:6 title "lmr (usg)" with points ps 1 pt 1, \
+plot "sod_new.dat" using "pos.x":"T" title "lmr (usg)" with points ps 1 pt 1, \
      "../analytic/analytic.data" using 1:4 title "analytic" with lines lt 1
 EOF
