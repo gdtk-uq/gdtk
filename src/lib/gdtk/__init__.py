6d8c609b7553429566351853a542b697a453f46d
Rowan J. Gollan <r.gollan@uq.edu.au>
2025-02-10 21:54:19 +1000

    python/gdtk: One character change to correct typo in __init__ list.

--- src/lib/gdtk/__init__.py
+++ src/lib/gdtk/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ['imoc', 'geom', 'flow', 'numeric',
            'gas', 'ideal_gas_flow', 'reflected_shock_tunnel',
            'sutherland', 'oblique_detonation',
-           'billig', 'billig_patch', 'busemann', 'prandt_meyer_inlet']
+           'billig', 'billig_patch', 'busemann', 'prandtl_meyer_inlet']
