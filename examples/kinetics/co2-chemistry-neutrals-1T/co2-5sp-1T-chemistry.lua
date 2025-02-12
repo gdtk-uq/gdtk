3b8d1d8d8f0b7965e2302f80ef9b358aa638130c
Peter Jacobs <peterj@mech.uq.edu.au>
2025-02-12 21:46:07 +1000

    kinetics: added 1T flavour of carbon-dioxide chemical rate equations.
    Adapted from Rowan's file of 2T rates.

---
+++ examples/kinetics/co2-chemistry-neutrals-1T/co2-5sp-1T-chemistry.lua
@@ -0,0 +1,79 @@
+-- Author: Rowan J. Gollan
+-- Date: 2021-06-12
+-- Modified for 1T calculations, Peter J., 2025-02-12
+--
+-- Reference:
+--   Ebrahim and Hornung (1973)
+--   Nonequilibrium Nozzle Expansions of Carbon Dioxide from a
+--   High-Enthalpy Reservoir
+--   AIAA Journal, 11(10), pp. 1369--1370
+--
+-- From Table 1.
+
+R_univ = 1.987e-3 -- kcal/(mol.K) [from Wikipedia]
+
+-- Dissociation --
+
+Reaction{
+   'CO2 + M <=> CO + O + M',
+   fr={'Arrhenius', A=2.88e11,  n=0.5,  C=74.8/R_univ},
+   label='r1'
+}
+
+Reaction{
+   'CO + M <=> C + O + M',
+   fr={'Arrhenius', A=8.79e29, n=-3.52, C=255.76/R_univ},
+   efficiencies={CO=0, O=0}, -- because these are excluded as M, they have separate rates.
+   label='r2'
+}
+
+Reaction{
+   'CO + CO <=> C + O + CO',
+   fr={'Arrhenius', A=1.76e30, n=-3.52, C=255.76/R_univ},
+   label='r3'
+}
+
+Reaction{
+   'CO + O <=> C + O + O',
+   fr={'Arrhenius', A=1.29e31, n=-3.52, C=255.76/R_univ},
+   label='r4'
+}
+
+Reaction{
+   'O2 + M <=> 2O + M',
+   fr={'Arrhenius', A=2.55e18, n=-1.0, C=118.7/R_univ},
+   efficiencies={O=0, O2=0}, -- because these are excluded as M, they have separate rates.
+   label='r5'
+}
+
+Reaction{
+   'O2 + O2 <=> 2O + O2',
+   fr={'Arrhenius', A=2.75e19, n=-1.0, C=118.7/R_univ},
+   label='r6'
+}
+
+Reaction{
+   'O2 + O <=> 3O',
+   fr={'Arrhenius', A=2.1e18, n=-1.0, C=117.96/R_univ},
+   label='r7'
+}
+
+-- Carbon exchange --
+
+Reaction{
+   'CO + CO <=> CO2 + C',
+   fr={'Arrhenius', A=2.33e9, n=0.5, C=130.5/R_univ},
+   label='r8'
+}
+
+Reaction{
+   'O + CO <=> C + O2',
+   fr={'Arrhenius', A=2.73e11, n=0.5, C=138.1/R_univ},
+   label='r9'
+}
+
+Reaction{
+   'CO + O2 <=> CO2 + O',
+   fr={'Arrhenius', A=1.6e13, n=0, C=41.0/R_univ},
+   label='r10'
+}
