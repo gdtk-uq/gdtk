d74eb2bd87945a47cf7b5e7fc66decd3bbce1fb2
Peter Jacobs <peterj@mech.uq.edu.au>
2025-02-13 22:44:43 +1000

    lmr HET simulation: have the simulation running with a 1T reaction scheme for CO2.

--- examples/kinetics/co2-chemistry-neutrals-1T/park-co2-neutrals-5sp-1T-chemistry.lua
+++ examples/kinetics/co2-chemistry-neutrals-1T/park-co2-neutrals-5sp-1T-chemistry.lua
@@ -1,79 +1,24 @@
 --
 -- This chemical kinetic system provides
--- a full reaction scheme for the Martian atmosphere.
+-- a HACKED,PARTIAL reaction scheme for the Martian atmosphere.
 --
 -- Reference: Park, Howe, Jaffe and Candler - 
 -- Review of Chemical-Kinetic Problems of Future NASA Missions, II: Mars Entries 
 -- https://arc.aiaa.org/doi/pdf/10.2514/3.496
 --
--- Author: Jens Kunze
+-- Author of the original unhacked file: Jens Kunze
 -- Date: 2020-Jan-16
--- Place: BRisbane, Australia
+-- Place: Brisbane, Australia
+-- This reduced version: Rowan G. and Peter J.
 --
--- Species are CO2, N2, Ar, O2, CO, O, C, N, CN, C2, NO, NCO, 
---              NO+, O2+, CO+, O+, C+, e-
---
--- Ar+, N+, C2+, N2+ and CN+ are possible but their concentrations
--- are insignificantly small according to Park et al
+-- Species are CO2, O2, CO, O, C 
 --
 -- Reaction rates in table 2 in the reference paper are
 -- given in the format 3.7^14. In the text, however,
 -- they are used as 3.7e+14!
 --
--- Comment NO_SPECIES=12 if you want to use the full reaction scheme
--- with ions
-----------------------------------------------------------------
-
--- RJG, 2025-02-13
--- Mechanism reduced to neutrals only
-
 Config{
    tempLimits = {lower=300.0, upper=15000.0}
-}
-
-
-Reaction{
-    'CO2 + C <=> CO + O + C',
-    fr={'Arrhenius', A=1.4e+22, n=-1.50, C=63275},
-    label='r49'
-}
-
-Reaction{
-    'CO2 + O <=> CO + O + O',
-    fr={'Arrhenius', A=1.4e+22, n=-1.50, C=63275},
-    label='r51'
-}
-
-
-Reaction{
-    'CO + C <=> C + O + C',
-    fr={'Arrhenius', A=3.4e+20, n=-1.00, C=129000},
-    label='r27'
-}
-
-Reaction{
-    'CO + O <=> C + O + O',
-    fr={'Arrhenius', A=3.4e+20, n=-1.00, C=129000},
-    label='r29'
-}
-
-Reaction{
-    'CO + O2 <=> C + O + O2',
-    fr={'Arrhenius', A=2.3e+20, n=-1.00, C=129000},
-    label='r32'
-}
-
-Reaction{
-    'CO + CO <=> C + O + CO',
-    fr={'Arrhenius', A=2.3e+20, n=-1.00, C=129000},
-    label='r34'
-}
-
-
-Reaction{
-    'CO + CO <=> C + O + CO',
-    fr={'Arrhenius', A=2.3e+20, n=-1.00, C=129000},
-    label='r34'
 }
 
 Reaction{
@@ -104,6 +49,72 @@ Reaction{
     'O2 + CO2 <=> O + O + CO2',
     fr={'Arrhenius', A=2.0e+21, n=-1.50, C=59750},
     label='r24'
+}
+
+Reaction{
+    'CO + C <=> C + O + C',
+    fr={'Arrhenius', A=3.4e+20, n=-1.00, C=129000},
+    label='r27'
+}
+
+Reaction{
+    'CO + O <=> C + O + O',
+    fr={'Arrhenius', A=3.4e+20, n=-1.00, C=129000},
+    label='r29'
+}
+
+Reaction{
+    'CO + O2 <=> C + O + O2',
+    fr={'Arrhenius', A=2.3e+20, n=-1.00, C=129000},
+    label='r32'
+}
+
+Reaction{
+    'CO + CO <=> C + O + CO',
+    fr={'Arrhenius', A=2.3e+20, n=-1.00, C=129000},
+    label='r34'
+}
+
+Reaction{
+    'CO + CO2 <=> C + O + CO2',
+    fr={'Arrhenius', A=2.3e+20, n=-1.00, C=129000},
+    label='r36'
+}
+
+Reaction{
+    'CO2 + C <=> CO + O + C',
+    fr={'Arrhenius', A=1.4e+22, n=-1.50, C=63275},
+    label='r49'
+}
+
+Reaction{
+    'CO2 + O <=> CO + O + O',
+    fr={'Arrhenius', A=1.4e+22, n=-1.50, C=63275},
+    label='r51'
+}
+
+Reaction{
+    'CO2 + O2 <=> CO + O + O2',
+    fr={'Arrhenius', A=6.9e+21, n=-1.50, C=63275},
+    label='r54'
+}
+
+Reaction{
+    'CO2 + CO <=> CO + O + CO',
+    fr={'Arrhenius', A=6.9e+21, n=-1.50, C=63275},
+    label='r56'
+}
+
+Reaction{
+    'CO2 + CO2 <=> CO + O + CO2',
+    fr={'Arrhenius', A=6.9e+21, n=-1.50, C=63275},
+    label='r58'
+}
+
+Reaction{
+    'CO + O <=> C + O2',
+    fr={'Arrhenius', A=3.9e+13, n=-0.18, C=69200},
+    label='r62'
 }
 
 Reaction{
--- examples/lmr/2D/expansion-tube-het/condition-3/contour.gnuplot
+++ examples/lmr/2D/expansion-tube-het/condition-3/contour.gnuplot
@@ -17,7 +17,7 @@ unset key
 unset clabel
 set cntrparam linear
 set cntrparam levels incremental 3.0,0.10,5.9
-set yrange [0:6.0]
-set xrange [0:10.0]
+set yrange [0:5.0]
+set xrange [0:9.2]
 splot "xtlogp.data" using ($1):($2*1000):($3) with pm3d at b, \
       "xtlogp.data" using ($1):($2*1000):($3) nosurface with lines lc rgb 'black'
