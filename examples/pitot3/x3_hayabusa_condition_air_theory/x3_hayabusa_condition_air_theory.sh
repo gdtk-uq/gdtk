22e51fbce1befb4877605d8f6031a20f807553dd
Chris James <c.james4@uq.edu.au>
2025-02-14 15:42:30 +1000

    pitot3: finally added some X3/X3R examples for Tamara

---
+++ examples/pitot3/x3_hayabusa_condition_air_theory/x3_hayabusa_condition_air_theory.sh
@@ -0,0 +1,6 @@
+# x3_hayabusa_condition_air_theory.sh: 
+# Shell script to run the X2 Hayabusa condition
+# air theoretical example for PITOT3.
+# Chris James (c.james4@uq.edu.au) - 14/02/25
+
+pitot3.py --config_file x3_hayabusa_condition_air_theory.yaml
---
+++ examples/pitot3/x3r_560kg_full_pressure_design_condition/x3r_560kg_full_pressure_design_condition.sh
@@ -0,0 +1,6 @@
+# x3r_560kg_full_pressure_design_condition.sh: 
+# Shell script to run the X3R 560kg RST
+# air theoretical example for PITOT3.
+# Chris James (c.james4@uq.edu.au) - 01/07/24
+
+pitot3.py --config_file x3r_560kg_full_pressure_design_condition.yaml
