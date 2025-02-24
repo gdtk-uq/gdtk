ee4b80923da9f809bf983dd1d0d584d4ef0d108c
Nick N. Gibbons <n.gibbons@uq.edu.au>
2025-02-24 12:36:23 +1000

    slf: Python interface has (read-only) properties for most of the intereting things in slf

--- src/slf/cwrap.d
+++ src/slf/cwrap.d
@@ -65,21 +65,60 @@ extern (C) int run()
     return exitCode;
 }
 
-extern (C) int get_size_of_solution()
-{
-    return to!int(flame.n);
-}
+extern (C) int get_nsp() { return to!int(flame.nsp); }
+extern (C) int get_neq() { return to!int(flame.neq); }
+extern (C) int get_N()   { return to!int(flame.N); }
+extern (C) int get_n()   { return to!int(flame.n); }
+
+extern (C) double get_D()    { return to!double(flame.pm.D.re); }
+extern (C) double get_p()    { return to!double(flame.pm.p.re); }
+extern (C) double get_dZ()   { return to!double(flame.pm.dZ.re); }
+extern (C) double get_T0()   { return to!double(flame.pm.T0.re); }
+extern (C) double get_T1()   { return to!double(flame.pm.T1.re); }
 
 
-extern (C) int get_solution(double* U)
-{
-    // Return the flame field as a numpy array
+immutable string get_array_function_def = "
+extern (C) int get_%s(double* %s) {
+    try { foreach(i; 0 .. flame.%s.length) %s[i] = flame.%s[i].re; return 0; }
+    catch (Exception e) { stderr.writeln(\"Failed to get %s because reason: \", e.msg); return -1; }
+}";
+
+mixin(format(get_array_function_def, "Z", "Z",   "pm.Z",  "Z",  "pm.Z",  "Z"));
+mixin(format(get_array_function_def, "Y0", "Y0", "pm.Y0", "Y0", "pm.Y0", "Y0"));
+mixin(format(get_array_function_def, "Y1", "Y1", "pm.Y1", "Y1", "pm.Y1", "Y1"));
+mixin(format(get_array_function_def, "U0", "U0", "pm.U0", "U0", "pm.U0", "U0"));
+mixin(format(get_array_function_def, "U1", "U1", "pm.U1", "U1", "pm.U1", "U1"));
+mixin(format(get_array_function_def, "U", "U", "U", "U", "U", "U"));
+mixin(format(get_array_function_def, "R", "R", "R", "R", "R", "R"));
+
+extern (C) int get_T(double* T) {
+    size_t nsp = flame.nsp;
+    size_t neq = flame.neq;
     try {
-        foreach(i; 0 .. flame.U.length) U[i] = flame.U[i].re;
+        foreach(i; 0 .. flame.N) T[i] = flame.U[i*neq + nsp].re;
         return 0;
-    } catch (Exception e) {
-        stderr.writeln("Exception message: ", e.msg);
+    }
+    catch (Exception e) {
+        stderr.writeln("Failed to get temperature because reason: ", e.msg);
         return -1;
     }
 }
 
+extern (C) int get_Y(double* Y) {
+    size_t nsp = flame.nsp;
+    size_t neq = flame.neq;
+    size_t idx = 0;
+    try {
+        foreach(i; 0 .. flame.N) {
+            foreach(j; 0 .. flame.nsp) {
+                Y[idx] = flame.U[i*neq + j].re;
+                idx += 1;
+            }
+        }
+        return 0;
+    }
+    catch (Exception e) {
+        stderr.writeln("Failed to get temperature because reason: ", e.msg);
+        return -1;
+    }
+}
--- src/slf/slf.py
+++ src/slf/slf.py
@@ -8,15 +8,37 @@ from cffi import FFI
 from numpy import array, zeros
 from os import environ, path
 
-ffi = FFI()
-ffi.cdef("""
+header = """
     int cwrap_init();
     int init_slf(const char* file_name);
     int set_initial_condition();
     int run();
-    int get_size_of_solution();
-    int get_solution(double* U);
-""")
+
+    int get_nsp();
+    int get_neq();
+    int get_N();
+    int get_n();
+
+    double get_D();
+    double get_p();
+    double get_dZ();
+    double get_T0();
+    double get_T1();
+
+    int get_Z(double*);
+    int get_Y0(double*);
+    int get_Y1(double*);
+    int get_U0(double*);
+    int get_U1(double*);
+    int get_U(double*);
+    int get_R(double*);
+    int get_T(double*);
+    int get_Y(double*);
+"""
+
+ffi = FFI()
+ffi.cdef(header)
+
 #so = ffi.dlopen("libslf.so")
 #so.cwrap_init()
 
@@ -42,12 +64,113 @@ class Flame(object):
         result = self.lib.run()
         return result
 
-    def get_solution(self):
-        n = self.lib.get_size_of_solution()
-        U = zeros(n)
-        Up = ffi.from_buffer("double[]", U)
-        self.lib.get_solution(Up)
+    @property
+    def nsp(self):
+        return self.lib.get_nsp()
+
+    @property
+    def neq(self):
+        return self.lib.get_neq()
+
+    @property
+    def N(self):
+        return self.lib.get_N()
+
+    @property
+    def n(self):
+        return self.lib.get_n()
+
+    # Put some setters for these, I think
+    @property
+    def D(self):
+        return self.lib.get_D()
+
+    @property
+    def p(self):
+        return self.lib.get_p()
+
+    @property
+    def dZ(self):
+        return self.lib.get_dZ()
+
+    @property
+    def T0(self):
+        return self.lib.get_T0()
+
+    @property
+    def T1(self):
+        return self.lib.get_T1()
+
+    @property
+    def Z(self):
+        Z = zeros(self.N)
+        code = self.lib.get_Z(Z)
+        if code!=0: raise Exception("Failed to get array Z from slf")
+        return Z
+
+    @property
+    def Y0(self):
+        Y0 = zeros(self.nsp)
+        Y0p = ffi.cast("double *", ffi.from_buffer(Y0))
+        code = self.lib.get_Y0(Y0p)
+        if code!=0: raise Exception("Failed to get array Y0 from slf")
+        return Y0
+
+    @property
+    def Y1(self):
+        Y1 = zeros(self.nsp)
+        Y1p = ffi.cast("double *", ffi.from_buffer(Y1))
+        code = self.lib.get_Y1(Y1p)
+        if code!=0: raise Exception("Failed to get array Y1 from slf")
+        return Y1
+
+    @property
+    def U0(self):
+        U0 = zeros(self.nsp+1)
+        U0p = ffi.cast("double *", ffi.from_buffer(U0))
+        code = self.lib.get_U0(U0p)
+        if code!=0: raise Exception("Failed to get array U0 from slf")
+        return U0
+
+    @property
+    def U1(self):
+        U1 = zeros(self.nsp+1)
+        U1p = ffi.cast("double *", ffi.from_buffer(U1))
+        code = self.lib.get_U1(U1p)
+        if code!=0: raise Exception("Failed to get array U1 from slf")
+        return U1
+
+    @property
+    def U(self):
+        U = zeros(self.n)
+        Up = ffi.cast("double *", ffi.from_buffer(U))
+        code = self.lib.get_U(Up)
+        if code!=0: raise Exception("Failed to get array U from slf")
         return U
+
+    @property
+    def R(self):
+        R = zeros(self.n)
+        Rp = ffi.cast("double *", ffi.from_buffer(R))
+        code = self.lib.get_R(Rp)
+        if code!=0: raise Exception("Failed to get array R from slf")
+        return R
+
+    @property
+    def T(self):
+        T = zeros(self.N)
+        Tp = ffi.cast("double *", ffi.from_buffer(T))
+        code = self.lib.get_T(Tp)
+        if code!=0: raise Exception("Failed to get array T from slf")
+        return T
+
+    @property
+    def Y(self):
+        Y = zeros(self.N*self.nsp)
+        Yp = ffi.cast("double *", ffi.from_buffer(Y))
+        code = self.lib.get_Y(Yp)
+        if code!=0: raise Exception("Failed to get array Y from slf")
+        return Y
 
 if __name__=='__main__':
     print("Put a test here, I guess...")
