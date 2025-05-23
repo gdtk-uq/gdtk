ffc799e8875434c5955f5333d918ae7785185e86
Peter Jacobs <peterj@mech.uq.edu.au>
2025-05-23 18:14:29 +1000

    lmr.py module: stored metadata is tidier and easier to access.
    Also, bring Rowan's pyvista loader into the SimInfo class.

--- src/lib/gdtk/lmr.py
+++ src/lib/gdtk/lmr.py
@@ -19,10 +19,6 @@ except ModuleNotFoundError:
 from gdtk.geom.sgrid import StructuredGrid
 
 
-class DomainType(Enum):
-    FLUID = 'fluid'
-    SOLID = 'solid'
-
 class LmrConfig:
     """
     A place to store the lmr code configuration.
@@ -52,14 +48,33 @@ class LmrConfig:
         return self.data[indx]
 
 
-BlockInfo = NamedTuple("BlockInfo", [('id', int), ('type', str), ('label', str), ('ncells', int)])
+DomainType = Enum('DomainType', ['FLUID', 'SOLID'])
+
+GridType = Enum('GridType', ['STRUCTURED', 'UNSTRUCTURED'])
+
+GridInfo = NamedTuple(
+    'Gridinfo', [('tag', str),
+                 ('type', GridType),
+                 ('field_type', DomainType),
+                 ('dimensions', int),
+                 ('niv', int), ('njv', int), ('nkv', int),
+                 ('nic', int), ('njc', int), ('nkc', int),
+                 ('json', dict)])
+
+BlockInfo = NamedTuple(
+    'BlockInfo', [('id', int),
+                  ('grid_type', GridType),
+                  ('label', str),
+                  ('ncells', int)])
 
 class SimInfo:
     """
     A place to store the information about an lmr simulation.
     """
-    __slots__ = ['lmr_cfg', 'sim_cfg', 'blocks', 'grids', 'grid_metadata',
-                 'snapshots_metadata', 'snapshots', 'times', 'fluid_variables']
+    __slots__ = ['lmr_cfg', 'sim_cfg',
+                 'sim_dir', 'grid_dir', 'snaps_dir', 'vtk_dir',
+                 'blocks', 'grids', 'snapshots', 'times',
+                 'fluid_variables']
 
     def __init__(self, lmr_cfg):
         """
@@ -67,14 +82,18 @@ class SimInfo:
         so that we can find the relevant files, etc.
         """
         self.lmr_cfg = lmr_cfg
-        sim_dir = self.lmr_cfg["simulation-directory"]
-        fname = os.path.join(sim_dir, self.lmr_cfg["config-filename"])
+        self.sim_dir = self.lmr_cfg["simulation-directory"]
+        self.grid_dir = os.path.join(os.path.join(self.sim_dir, self.lmr_cfg["grid-directory"]))
+        self.snaps_dir = os.path.join(self.sim_dir, self.lmr_cfg["snapshot-directory"])
+        self.vtk_dir = os.path.join(self.sim_dir, self.lmr_cfg['vtk-output-directory'])
+        #
+        fname = os.path.join(self.sim_dir, self.lmr_cfg["config-filename"])
         with open(fname, 'r') as fp:
             self.sim_cfg = json.load(fp)
         #
         # Read the metadata for the list of blocks.
         #
-        fname = os.path.join(sim_dir, self.lmr_cfg["block-list-filename"])
+        fname = os.path.join(self.sim_dir, self.lmr_cfg["block-list-filename"])
         self.blocks = []
         if os.path.exists(fname):
             with open(fname, 'r') as fp:
@@ -82,17 +101,23 @@ class SimInfo:
                 for line in content:
                     if line.strip().startswith('#'): continue
                     items = line.strip().split()
-                    self.blocks.append(BlockInfo(int(items[0]), items[1], items[2], int(items[3])))
+                    gt = GridType.STRUCTURED if items[1] == "structured_grid" else GridType.UNSTRUCTURED
+                    bi = BlockInfo(id=int(items[0]), grid_type=gt, label=items[2], ncells=int(items[3]))
+                    self.blocks.append(bi)
         # Check that the blocks are in the expected order.
         ids_in_order = [i == self.blocks[i].id for i in range(len(self.blocks))]
         if not all(ids_in_order):
             print("Warning: not all block ids are in order.")
         #
         # Read the metadata for the snapshots.
+        # The primary source of information is the collection of directory names,
+        # because both the steady-state and the transient code do the same thing
+        # with these directories.  The transient simulation also writes metadata
+        # for each snapshot that includes the time instants at which snapshots
+        # are written.
         #
-        snaps_dirname = os.path.join(sim_dir, self.lmr_cfg["snapshot-directory"])
         self.snapshots = []
-        names = os.listdir(snaps_dirname)
+        names = os.listdir(self.snaps_dir)
         names = [n for n in names if re.match(r'\d\d\d\d', n)]
         names.sort()
         names_in_order = [i == int(names[i]) for i in range(len(names))]
@@ -101,7 +126,7 @@ class SimInfo:
         self.snapshots = names
         self.times = []
         if self.sim_cfg['solver_mode'] == 'transient':
-            fname = os.path.join(snaps_dirname, "snapshot-times-metadata")
+            fname = os.path.join(self.snaps_dir, "snapshot-times-metadata")
             with open(fname, 'r') as fp:
                 snapshots_metadata = yaml.safe_load(fp)
             snaps = list(snapshots_metadata.keys())
@@ -114,7 +139,7 @@ class SimInfo:
         # List of names of the fluid variables.
         #
         self.fluid_variables = []
-        fname = os.path.join(snaps_dirname, "fluid"+self.lmr_cfg["metadata-extension"])
+        fname = os.path.join(self.snaps_dir, "fluid"+self.lmr_cfg["metadata-extension"])
         with open(fname, 'r') as fp:
             fluid_metadata = yaml.safe_load(fp)
             self.fluid_variables = fluid_metadata["variables"]
@@ -122,16 +147,21 @@ class SimInfo:
         # Grid metadata
         #
         self.grids = []
-        grid_dir = os.path.join(os.path.join(sim_dir, self.lmr_cfg["grid-directory"]))
-        fname = os.path.join(os.path.join(grid_dir, "grid"+self.lmr_cfg["metadata-extension"]))
+        fname = os.path.join(os.path.join(self.grid_dir, "grid"+self.lmr_cfg["metadata-extension"]))
         with open(fname, 'r') as fp:
-            self.grid_metadata = json.load(fp)
-        ngrids = self.grid_metadata['ngrids']
-        for i in range(ngrids):
+            grid_metadata = json.load(fp)
+        for i in range(grid_metadata['ngrids']):
             fname = ("grid-%04d" % i)+self.lmr_cfg["metadata-extension"]
-            fname = os.path.join(os.path.join(grid_dir, fname))
+            fname = os.path.join(os.path.join(self.grid_dir, fname))
             with open(fname, 'r') as fp:
-                self.grids.append(json.load(fp))
+                mdata = json.load(fp)
+                gt = GridType.STRUCTURED if mdata['type'] == "structured_grid" else GridType.UNSTRUCTURED
+                gi = GridInfo(tag=mdata['tag'], type=gt, field_type=mdata['fieldType'],
+                              dimensions=mdata['dimensions'],
+                              niv=mdata['niv'], njv=mdata['njv'], nkv=mdata['njv'],
+                              nic=mdata['nic'], njc=mdata['njc'], nkc=mdata['nkc'],
+                              json=mdata)
+                self.grids.append(gi)
         #
         return
 
@@ -140,55 +170,61 @@ class SimInfo:
         Return a list containing all of the grids associated the simulation.
         """
         grids = []
-        sim_dir = self.lmr_cfg["simulation-directory"]
-        grid_dir = os.path.join(os.path.join(sim_dir, self.lmr_cfg["grid-directory"]))
-        ngrids = self.grid_metadata['ngrids']
         grid_format = self.sim_cfg["grid_format"]
-        for i in range(ngrids):
-            grid_type = self.grids[i]["type"]
-            if grid_type == "structured_grid":
+        for i in range(len(self.grids)):
+            grid_type = self.grids[i].type
+            if grid_type == GridType.STRUCTURED:
                 if grid_format == "gziptext":
                     fname = ("grid-%04d.gz" % i)
-                    fname = os.path.join(os.path.join(grid_dir, fname))
+                    fname = os.path.join(os.path.join(self.grid_dir, fname))
                     grids.append(StructuredGrid(gzfile=fname))
                 elif grid_format == "rawbinary":
                     fname = "grid-%04d.bin" % (i)
-                    fname = os.path.join(os.path.join(grid_dir, fname))
+                    fname = os.path.join(os.path.join(self.grid_dir, fname))
                     grids.append(StructuredGrid(binaryfile=fname))
                 else:
                     raise "Unknown format for grid[%d] %s" % (i, grid_format)
-            elif grid_type == "unstructured_grid":
+            elif grid_type == GridType.UNSTRUCTURED:
                 raise "unstructured_grid reading not implemtented"
+            else:
+                raise "unknown grid type"
         return grids
 
+    def read_snapshot(self, snap_name):
+        """
+        Read the solution data for a particular snapshot.
+        """
+        # [TODO] PJ 2025-05-23
+        fields = []
+        grids = []
+        return fields, grids
 
-# Service functions for picking up data from an Eilmer simulation
+    def load_pvd_into_pyvista(self, domain_type=DomainType.FLUID, merged=False, as_point_data=False):
+        """
+        This loads the top-level PVD file for VTK output from Eilmer.
 
-def load_pvd_into_pyvista(lmr_cfg, sim_info, domain_type=DomainType.FLUID, merged=False, as_point_data=False):
-    """This loads the top-level PVD file for VTK output from Eilmer.
+        The VTK output is ordinarily produced as the result of running 'snapshot2vtk'.
 
-    The VTK output is ordinarily produced as the result of running 'snapshot2vtk'.
+        Args:
+            domain_type (DomainType) : indicates if domain contains fluid or solid data
+            merged (bool)            : set to True to combine blocks; default is to leave partitioned
+            as_point_data (bool)     : set to True to convert to point data; default is to leave as cell data
 
-    Args:
-        lmr_cfg (LmrConfig)      : config object for Eilmer program itself
-        sim_info (SimInfo)       : information for specific simulation (in current working directory)
-        domain_type (DomainType) : indicates if domain contains fluid or solid data
-        merged (bool)            : set to True to combine blocks; default is to leave partitioned
-        as_point_data (bool)     : set to True to convert to point data; default is to leave as cell data
+        Returns:
+            A pyvista.DataSet object.
+        """
+        if not HAVE_PYVISTA:
+            raise "pyvista module is not loaded"
+        # locate the PVD file
+        fname = os.path.join(self.vtk_dir, domain_type.value+".pvd")
+        if not os.path.exists(fname):
+            raise "Cannot find .pvd file: %s" % (fname,)
+        reader = pv.get_reader(fname)
+        pv_data = reader.read()
+        if merged:
+            pv_data = pv_data.combine()
+        if as_point_data:
+            pv_data = pv_data.cell_data_to_point_data()
+        return pv_data
 
-    Returns:
-        A pyvista.DataSet object.
-    """
-    if not HAVE_PYVISTA:
-        raise "pyvista module is not loaded"
-    # locate the PVD file
-    vtk_dir = os.path.join(lmr_cfg['simulation-directory'], lmr_cfg['vtk-output-directory'])
-    fname = os.path.join(vtk_dir, domain_type.value+".pvd")
-    # now read
-    reader = pv.get_reader(fname)
-    pv_data = reader.read()
-    if merged:
-        pv_data = pv_data.combine()
-    if as_point_data:
-        pv_data = pv_data.cell_data_to_point_data()
-    return pv_data
+    # end of class SimInfo
