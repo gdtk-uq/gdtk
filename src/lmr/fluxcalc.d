c716ddfd1e1b566d075a0a20307d10798d9288ab
Robert Watt <wattrg@hotmail.com>
2025-08-08 14:50:03 +1000

    lmr: Include electron pressure term in convective flux in the electron energy conservation equation

--- src/lmr/fluxcalc.d
+++ src/lmr/fluxcalc.d
@@ -621,6 +621,10 @@ void ausmdv(in FlowState Lft, in FlowState Rght, in FlowState fs, ref ConservedQ
         }
         version(multi_T_gas) {
             foreach (i; 0 .. cqi.n_modes) { F[cqi.modes+i] += factor*ru_half*Lft.gas.u_modes[i]; }
+
+            if (gmodel.is_plasma && gmodel.n_modes > 0) {
+                F[cqi.modes+cqi.n_modes-1] += factor * ru_half * Lft.gas.p_e / Lft.gas.rho;
+            }
         }
         // NOTE: - the following relies on the free-electron mode being the last mode
         //       - for single temp models F_renergies isn't used
@@ -643,6 +647,10 @@ void ausmdv(in FlowState Lft, in FlowState Rght, in FlowState fs, ref ConservedQ
         }
         version(multi_T_gas) {
             foreach (i; 0 .. cqi.n_modes) { F[cqi.modes+i] += factor*ru_half*Rght.gas.u_modes[i]; }
+
+            if (gmodel.is_plasma && gmodel.n_modes > 0) {
+                F[cqi.modes+cqi.n_modes-1] += factor * ru_half * Rght.gas.p_e / Rght.gas.rho;
+            }
         }
     }
     //
@@ -676,7 +684,13 @@ void ausmdv(in FlowState Lft, in FlowState Rght, in FlowState fs, ref ConservedQ
             }
             version(multi_T_gas) {
                 foreach (i; 0 .. cqi.n_modes) {
-                    F[cqi.modes+i] -= factor*d_ua*(rR*Rght.gas.u_modes[i] - rL*Lft.gas.u_modes[i]);
+                    number hli = to!number(0.0);
+                    number hri = to!number(0.0);
+                    foreach (isp; 0 .. gmodel.n_species) {
+                        hli += Lft.gas.massf[isp]*gmodel.enthalpyPerSpeciesInMode(Lft.gas, isp, cast(int) i);
+                        hri += Rght.gas.massf[isp]*gmodel.enthalpyPerSpeciesInMode(Rght.gas, isp, cast(int) i);
+                    }
+                    F[cqi.modes+i] -= factor*d_ua*(rR*hri - rL*hli);
                 }
             }
         } // end of entropy fix (d_ua != 0)
@@ -945,6 +959,10 @@ void ldfss0(in FlowState Lft, in FlowState Rght, in FlowState fs, ref ConservedQ
         foreach (i; 0 .. cqi.n_modes) {
             F[cqi.modes+i] +=  factor*(aL*rL*CL*Lft.gas.u_modes[i] + aR*rR*CR*Rght.gas.u_modes[i]);
         }
+
+        if (gmodel.is_plasma && gmodel.n_modes > 0) {
+            F[cqi.modes+cqi.n_modes-1] += factor*(aL*CL*Lft.gas.p_e + aR*CR*Rght.gas.p_e);
+        }
     }
 } // end ldfss0()
 
@@ -1055,6 +1073,10 @@ void ldfss2(in FlowState Lft, in FlowState Rght, in FlowState fs, ref ConservedQ
         foreach (i; 0 .. cqi.n_modes) {
             F[cqi.modes+i] +=  factor*(am*rL*CL*Lft.gas.u_modes[i] + am*rR*CR*Rght.gas.u_modes[i]);
         }
+
+        if (gmodel.is_plasma && gmodel.n_modes > 0) {
+            F[cqi.modes+cqi.n_modes-1] += factor*(am*CL*Lft.gas.p_e + am*CR*Rght.gas.p_e);
+        }
     }
 } // end ldfss2()
 
@@ -1156,6 +1178,10 @@ void hanel(in FlowState Lft, in FlowState Rght, in FlowState fs, ref ConservedQu
     version(multi_T_gas) {
         foreach (i; 0 .. cqi.n_modes) {
             F[cqi.modes+i] += factor*(uLplus*rL*Lft.gas.u_modes[i] + uRminus*rR*Rght.gas.u_modes[i]);
+        }
+
+        if (gmodel.is_plasma && gmodel.n_modes > 0) {
+            F[cqi.modes+cqi.n_modes-1] += factor*(uLplus*Lft.gas.p_e + uRminus*Rght.gas.p_e);
         }
     }
 } // end hanel()
