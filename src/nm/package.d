8c65ea82b799847505db781cbec70fc3dbea3e08
Alex Muirhead <a.muirhead@uq.net.au>
2025-10-04 22:33:46 +1000

    nm: Complete package.d

--- src/nm/package.d
+++ src/nm/package.d
@@ -3,3 +3,29 @@ module nm;
 public import nm.nm_exception;
 public import nm.number;
 
+// Method modules
+public import nm.bbla;
+public import nm.bdfLU;
+public import nm.bracketing;
+public import nm.brent;
+public import nm.gaussquad;
+public import nm.limiters;
+public import nm.linesearch;
+public import nm.nelmin;
+public import nm.newton;
+public import nm.newtoncotes;
+public import nm.ridder;
+public import nm.rsla;
+public import nm.rungekutta;
+public import nm.schedule;
+public import nm.secant;
+public import nm.smla;
+public import nm.spline;
+public import nm.splinelsq;
+public import nm.stmatrix;
+public import nm.tree_patch;
+public import nm.univariate_lut;
+
+// Lua modules
+public import nm.luabbla;
+
