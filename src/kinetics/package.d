d790dcc4b22fc2dd8398c00a4573646ac4075f3d
Alex Muirhead <a.muirhead@uq.net.au>
2025-10-04 22:32:01 +1000

    kinetics: Complete package.d

--- src/kinetics/package.d
+++ src/kinetics/package.d
@@ -3,3 +3,33 @@ module kinetics;
 public import kinetics.thermochemical_reactor;
 public import kinetics.init_thermochemical_reactor;
 
+public import kinetics.chemistry_update;
+public import kinetics.energy_exchange_mechanism;
+public import kinetics.energy_exchange_system;
+public import kinetics.equilibrium_update;
+public import kinetics.exchange_chemistry_coupling;
+public import kinetics.exchange_cross_section;
+public import kinetics.fuel_air_mix_kinetics;
+public import kinetics.ideal_dissociating_gas_kinetics;
+public import kinetics.multi_temperature_thermochemical_reactor;
+public import kinetics.powers_aslam_kinetics;
+public import kinetics.rate_constant;
+public import kinetics.reaction;
+public import kinetics.relaxation_time;
+public import kinetics.two_temperature_air_kinetics;
+public import kinetics.two_temperature_argon_kinetics;
+public import kinetics.two_temperature_argon_with_ideal_gas;
+public import kinetics.two_temperature_dissociating_nitrogen_kinetics;
+public import kinetics.two_temperature_gasgiant_kinetics;
+public import kinetics.two_temperature_nitrogen_kinetics;
+public import kinetics.vib_specific_co_kinetics;
+public import kinetics.vib_specific_nitrogen_kinetics;
+public import kinetics.yee_kotov_kinetics;
+
+// Lua modules
+public import kinetics.luachemistry_update;
+public import kinetics.luaequilibrium_calculator;
+public import kinetics.luareaction_mechanism;
+public import kinetics.luathermochemical_reactor;
+public import kinetics.luatwo_temperature_air_kinetics;
+public import kinetics.luavib_specific_nitrogen_kinetics;
