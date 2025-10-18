40b67525d61dc96eed81de75bdc0c495c0e129f5
Alex Muirhead <a.muirhead@uq.net.au>
2025-10-18 22:20:38 +1000

    src: Moving python wrapper to gdtk-py

--- src/gdtk-py/gdtk/__init__.py
+++
@@ -1,4 +0,0 @@
-__all__ = ['lmr', 'imoc', 'geom', 'flow', 'numeric',
-           'gas', 'ideal_gas_flow', 'reflected_shock_tunnel',
-           'sutherland', 'oblique_detonation',
-           'billig', 'billig_patch', 'busemann', 'prandtl_meyer_inlet']
